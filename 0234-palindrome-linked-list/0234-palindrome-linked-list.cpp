a5afd8efd4c184464b55d5d0a33ac1b1ae0553f6
AkshatSinghNayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-05-29 03:12:18 +0530

    Time: 0 ms (100%), Space: 118 MB (70.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,55 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if (!head || !head->next) return true;
-        
-        // 1. Count the number of nodes
-        int count = 0;
-        ListNode* temp = head;
-        while (temp) {
-            count++;
-            temp = temp->next;
+
+        // 1. Find the middle (slow will point to middle)
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        // 2. Move to the middle
-        int mid = count/2;
-        ListNode* firstHalf = head;
-        ListNode* secondHalf = head;
-        for (int i = 0; i < mid; ++i) {
-            secondHalf = secondHalf->next;
-        }
-        // If odd, skip the middle node
-        if (count % 2 != 0) {
-            secondHalf = secondHalf->next;
-        }
-        
-        // 3. Reverse the second half
+
+        // 2. Reverse the second half
         ListNode* prev = nullptr;
-        while (secondHalf) {
-            ListNode* nextTemp = secondHalf->next;
-            secondHalf->next = prev;
-            prev = secondHalf;
-            secondHalf = nextTemp;
+        while (slow) {
+            ListNode* next = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = next;
         }
-        
-        // 4. Compare values in both halves
-        ListNode* p1 = head;
-        ListNode* p2 = prev;
-        for (int i = 0; i < mid; ++i) {
-            if (p1->val != p2->val) return false;
-            p1 = p1->next;
-            p2 = p2->next;
+
+        // 3. Compare first and second half
+        ListNode* left = head;
+        ListNode* right = prev;
+        while (right) {
+            if (left->val != right->val) return false;
+            left = left->next;
+            right = right->next;
         }
+
         return true;
     }
 };
