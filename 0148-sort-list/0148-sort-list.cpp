e146265d90d9511375f21a77c237cc733d6d18ce
AkshatSinghNayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-06-01 20:12:45 +0530

    Time: 53 ms (23.58%), Space: 75.8 MB (22.06%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,33 +1,52 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+
+    ListNode* mergeLL(ListNode* l1 , ListNode* l2){
+        ListNode* dummy = new ListNode (-1);
+        ListNode* temp = dummy ; 
+        while( l1 != nullptr && l2 != nullptr ){
+            if( l1->val <= l2->val){
+                temp->next = l1 ; 
+                l1= l1->next ;
+            }
+            else{
+                temp->next = l2 ;
+                l2 = l2->next ;
+            }
+            temp=temp->next ;
+        }
+        if(l1 != nullptr){
+            temp->next = l1;
+        }
+       else{
+        temp->next = l2 ;
+       }
+        return dummy->next ;
+    }
+
+    ListNode* middle (ListNode* head){
+        ListNode* slow = head ; 
+        ListNode* fast = head->next ; 
+    while( fast!=nullptr && fast->next!=nullptr){
+        slow = slow->next ; 
+        fast = fast->next->next;
+    }
+    return slow ;
+    }
+
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head ; 
-        if ( head == nullptr || head->next==nullptr) return head ; 
-        vector<int> arr ;
-       
-        while(temp!=nullptr){
-           arr.push_back(temp->val);
-            temp= temp->next;  
-            
-        }
-        sort(arr.begin(),arr.end()); 
-        temp = head ; 
-        int i = 0 ;
-        while(temp != nullptr){
-            temp->val = arr[i];
-            temp=temp->next; 
-            i++;
-        }
-        return head; 
+        if( head == nullptr || head->next== nullptr ) return head ; 
+
+        ListNode* mid = middle(head) ; 
+
+        ListNode* right = mid->next ; 
+        mid->next = nullptr ; 
+        ListNode* left = head;
+
+     left=   sortList(left);
+      right =  sortList(right);
+
+        return mergeLL(left , right);
+
     }
 };
