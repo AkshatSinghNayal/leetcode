c0dead645a77e6aab1e699b62ca34c5e8ea1fa49
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-06-08 19:53:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // Check if n is positive and satisfies the condition for power of two
-        if (n > 0 && (n & (n - 1)) == 0) {
-            return true;
-        }
-        return false;
+        // Check if n is greater than 0 and satisfies the power of two condition
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
