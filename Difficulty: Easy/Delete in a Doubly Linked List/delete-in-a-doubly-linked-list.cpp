de1663f778c9f10fafd182ce4313b9e66226f23b
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-06-08 23:13:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete in a Doubly Linked List/delete-in-a-doubly-linked-list.cpp
+++ Difficulty: Easy/Delete in a Doubly Linked List/delete-in-a-doubly-linked-list.cpp
@@ -1,36 +1,46 @@
+/* Structure of Node
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val) {
+        data = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+*/
+
 class Solution {
   public:
     // Function to delete a node at given position.
     Node* deleteNode(Node* head, int x) {
         
-        // Case 1: Deleting the head
-        if(x == 1){
-            Node* del = head;          // Save the head node to delete
-            head = head->next;         // Move head forward
-            if(head) head->prev = NULL; // Fix back link if head is not null
-            delete del;                // Free memory
-            return head;
+        Node* temp = head ; 
+        int i = 0; 
+        if( x == 1 ){
+            Node* del = head ; 
+            head= head->next ; 
+            if(head) head->prev= nullptr ; 
+            delete(del);
+            return head ; 
+        }
+        while(temp && i<(x-1)){
+            i++; 
+            temp = temp->next ; 
         }
         
-        // Case 2: Deleting at position x (not head)
-        Node* temp = head;
-        int i = 0;
-        while(temp && i < (x-1)){
-            temp = temp->next;
-            i++;
-        }
+        if( temp == nullptr) return head ; 
         
-        // Check if node exists
-        if(temp == NULL) return head;
-
-        Node* before = temp->prev;
-        Node* after = temp->next;
-
-        if(before) before->next = after;
-        if(after) after->prev = before;
-
-        delete temp;
-
-        return head;
+        Node* after = temp->next ; 
+        Node* before = temp->prev ; 
+        
+        if(after) after->prev = before ; 
+        if(before) before->next = after ; 
+        
+        return head; 
+        
     }
 };
