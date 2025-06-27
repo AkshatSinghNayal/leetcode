a53a6bec3782f818f8689f00b6111cd3f02f9bb1
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-06-28 01:09:20 +0530

    Time: 2 ms (49.67%), Space: 11 MB (44.38%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    void reverse(string &s, int left, int right) {
-        while (left < right) {
-            swap(s[left++], s[right--]);
-        }
-    }
-
     string reverseWords(string s) {
-        // 1. Reverse the whole string
-        reverse(s, 0, s.size() - 1);
+        vector<string> words;
+        string word;
 
-        int n = s.size();
-        int i = 0, j = 0;
-    int start = 0;
-        // 2. Reverse each word and remove extra spaces in-place
-        while (j < n) {
-            // Skip spaces
-            while (j < n && s[j] == ' ') j++;
-            if (j >= n) break;
-
-            // Start of word
-            if (i != 0) s[i++] = ' '; // add one space if it's not the first word
-             start = i;
-
-            // Copy word
-            while (j < n && s[j] != ' ')
-                s[i++] = s[j++];
-
-            // Reverse the copied word
-            reverse(s, start, i - 1);
+       for(int i = 0; i < s.size(); i++){
+            if (word.empty() && s[i] != ' ') {
+                word += s[i];
+            } else if (word.empty() && s[i] == ' ') {
+                continue;
+            } else if (!word.empty() && s[i] == ' ') {
+                words.push_back(word);
+                word.erase();
+            }
+            else{
+                word+=s[i];
+            }
         }
-
-        // 3. Resize to new length without trailing spaces
-        s.resize(i);
-        return s;
+            if (!word.empty()) {
+                words.push_back(word);
+            }
+            reverse(words.begin(),words.end());
+        string result;
+        for(int i = 0; i < words.size(); i++) {
+            result += words[i];
+            if (i < words.size() - 1) {
+                result += ' ';
+            }
+        }
+        return result;
     }
 };
