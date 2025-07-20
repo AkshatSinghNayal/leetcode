707b8aa8310b98680ba4a372fb1d29b8dbdf4258
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-07-20 14:00:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap two numbers/swap-two-numbers.cpp
+++ Difficulty: Basic/Swap two numbers/swap-two-numbers.cpp
@@ -1,11 +1,10 @@
-// User function Template for C++
-
 class Solution {
   public:
     pair<int, int> get(int a, int b) {
-        a=a^b;
-        b=a^b; 
-        a=a^b;
-        return { a, b } ; 
+        // code here
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        return {a,b};
     }
 };
