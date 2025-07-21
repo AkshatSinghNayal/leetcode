f838ba879119fdc330c7ee60620fe039af1e5a61
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-07-21 16:48:59 +0530

    Time: 3 ms (32.58%), Space: 13.4 MB (36.54%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -6,9 +6,9 @@ public:
             int countBits = 0; 
             
             for( auto it : nums ) {
-                if(it&(1<<i)){
+               if((it>>i)&1){
                     countBits++;
-                }
+               }
             }
             if(countBits%3!=0){
                 result = result | (1<<i);
