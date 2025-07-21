7fb15c098c21df1d09a145b1f0f12fb7cd7dcffc
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-07-21 16:50:20 +0530

    Time: 0 ms (100%), Space: 13.5 MB (36.54%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0 ; 
-        for( int i = 0 ; i <=31 ; i++){
-            int countBits = 0; 
-            
-            for( auto it : nums ) {
-               if((it>>i)&1){
-                    countBits++;
-               }
-            }
-            if(countBits%3!=0){
-                result = result | (1<<i);
-            }
+        int ones = 0, twos = 0;
+
+        for (auto num : nums) {
+            // Update `ones` to include current number's bits that are not in `twos`
+            ones = (ones ^ num) & ~twos;
+
+            // Update `twos` to include current number's bits that are not in `ones`
+            twos = (twos ^ num) & ~ones;
         }
-        return result ; 
+
+        // `ones` contains the single number that appears only once
+        return ones;
     }
 };
