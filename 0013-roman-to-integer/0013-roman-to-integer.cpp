3f5366462e4f21fc5859a637bc2b8f86f0198303
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-07-28 16:06:32 +0530

    Time: 15 ms (10.05%), Space: 13.4 MB (19.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> mp = {
-            {'I', 1},   {'V', 5},   {'X', 10},
-            {'L', 50},  {'C', 100}, {'D', 500},
-            {'M', 1000}
-        };
+        unordered_map<char, int> mp = {{'I', 1},   {'V', 5},   {'X', 10},
+                                       {'L', 50},  {'C', 100}, {'D', 500},
+                                       {'M', 1000}};
 
         int result = 0;
         int i = 0;
@@ -13,18 +11,17 @@ public:
         while (i < s.size()) {
             if ((i + 1 < s.size()) && mp[s[i]] >= mp[s[i + 1]]) {
                 result += mp[s[i]];
-            } else {
-                if ((i + 1 < s.size()) && mp[s[i]] < mp[s[i + 1]]) {
-                    int temp = mp[s[i + 1]] - mp[s[i]];
-                    result += temp;
-                    i++;  // Skip next since it’s used
-                } else {
-                    result += mp[s[i]]; // For last single character
-                }
+            } else if ((i + 1 < s.size()) && mp[s[i]] < mp[s[i + 1]]) {
+                int temp = mp[s[i + 1]] - mp[s[i]];
+                result += temp;
+                i++; // Skip next since it’s used
             }
-            i++;
-        }
 
-        return result;
+            else {
+                result += mp[s[i]]; // For last single character
+            }
+        i++;
+        }
+    return result;
     }
 };
