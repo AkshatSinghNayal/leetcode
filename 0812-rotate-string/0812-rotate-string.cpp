95e8a8b1ecacd5a2b726c767c165670343f9153b
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-07-28 14:48:54 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.98%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    void reverse(string& s, int left, int right) {
-        while (left < right) {
-            swap(s[left++], s[right--]);
-        }
-    }
-
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return false;
-        int n = s.size();
-
-        for (int i = 0; i < n; i++) {
-            if (goal[i] == s[0]) {
-                string temp = goal;
-                reverse(temp, i, n - 1);
-                reverse(temp, 0, i - 1);
-                reverse(temp, 0, n - 1);
-                if (temp == s) return true;
-            }
+        if(s.size()!=goal.size()) return false ;
+        string newString = s+s;
+        if(newString.find(goal) != string::npos){
+            return true ; 
         }
-
-        return false;
+        return false ;
     }
 };
