79c01a3e7016ef95559bbaa36bd1ab071d466ed9
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-03 04:04:08 +0530

    Time: 5 ms (30.05%), Space: 23.6 MB (31.6%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,32 @@
 class MinStack {
 public:
-    stack<int> st ; 
+    stack<int> st; 
+    stack<int> mini ; 
+
     MinStack() {
         
     }
     
     void push(int val) {
         st.push(val);
+        if(mini.empty() || val<=mini.top()){
+            mini.push(val);
+        }
     }
     
     void pop() {
-       st.pop();
+        if(st.top()== mini.top()){
+            mini.pop();
+        }
+        st.pop();
     }
     
     int top() {
-        
         return st.top();
     }
     
     int getMin() {
-         stack<int> temp = st; // Create a copy of the current stack
-        int mini = INT_MAX;
-
-        while (!temp.empty()) {
-            mini = min(mini, temp.top());
-            temp.pop();
-    }
-    return mini;
+        return mini.top();
     }
 };
 
