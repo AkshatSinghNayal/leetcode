b19dc704afd2301c597b74a5870b161ab4523118
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-07 04:07:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.cpp
+++ Difficulty: Basic/Immediate Smaller Element/immediate-smaller-element.cpp
@@ -1,36 +1,14 @@
-// User function template for C++
 class Solution {
   public:
-    void immediateSmaller(vector<int>& nums) {
-      int n = nums.size();
-        vector<int> ans(n, -1);
-        stack<int> st;
-        // Iterate from 2n-1 to 0 to simulate circular array
-        for(int i = n - 1; i >= 0; i--) {
-        int takenAtFirst =1;  
-            
-                if(!st.empty() && st.top()>=nums[i]){
-                    ans[i] = -1 ; 
-                    st.top();
-                    takenAtFirst=0;
-                }
-
-
-            
-                if(!st.empty() && takenAtFirst == 1) {
-                   
-                    ans[i] = st.top();
-                  
-                }
-            
-
-            st.push(nums[i]);
+    void immediateSmaller(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = 0; i < n - 1; i++) {
+            if(arr[i+1] < arr[i]) {
+                arr[i] = arr[i+1];
+            } else {
+                arr[i] = -1;
+            }
         }
-
-       for( int i = 0 ; i <nums.size() ; i++){
-           nums[i] = ans[i];
-           
-       }
-           
+        arr[n-1] = -1; // Last element is always -1
     }
 };
