a6b145c9d271f450149c35c8c4870d1869a432ef
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-14 02:13:59 +0530

    Time: 22 ms (83.75%), Space: 75.1 MB (29.48%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,26 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> count; // fruit type -> frequency
-        int left = 0, maxLen = 0;
-
-        for (int right = 0; right < fruits.size(); right++) {
-            count[fruits[right]]++; // add current fruit
-
-            // shrink window if > 2 types
-            while (count.size() > 2) {
-                count[fruits[left]]--;
-                if (count[fruits[left]] == 0) {
-                    count.erase(fruits[left]);
+        unordered_map<int,int> mp ; 
+        int maxLen = 0 ; 
+        int left = 0 ; 
+        for( int right = 0 ; right<fruits.size() ;right++){
+            mp[fruits[right]]++;
+            
+            if(mp.size()>2){
+                mp[fruits[left]]--;
+                if( mp[fruits[left]] == 0){
+                    mp.erase(fruits[left]);
                 }
                 left++;
             }
 
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = max(maxLen,right-left +1);
         }
-        return maxLen;
+        return maxLen ;
     }
 };
