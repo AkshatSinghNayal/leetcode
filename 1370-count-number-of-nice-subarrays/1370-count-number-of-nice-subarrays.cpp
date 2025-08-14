ce302c412bcdd645c962d28ec56e37f8b4e4d18b
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-14 20:35:27 +0530

    Time: 12 ms (38.72%), Space: 71.4 MB (92.98%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int atmost(vector<int>& nums, int k ){
-        if(nums.size() < k ) return 0 ; 
-        int left = 0 ; 
-        int count = 0 ;
-        int right = 0; 
+    int atMost(vector<int>& nums, int k){
+        int left = 0, count = 0;
         int oddCount = 0;
-        while(right<nums.size()){
-            if(nums[right]%2!=0){
-                oddCount++;
-            }
-            while(oddCount>k){
-                if(nums[left]%2!=0){
-                    oddCount--;
-                }
+
+        for(int right = 0; right < nums.size(); right++){
+            if(nums[right] & 1) oddCount++;  // bitwise odd check
+
+            while(oddCount > k){             // shrink window from left
+                if(nums[left] & 1) oddCount--;  // check left with bitwise
                 left++;
             }
-            
-            count+=right-left+1;
 
-            right++;
+            count += right - left + 1;       // all valid subarrays ending at right
         }
+
         return count;
     }
 
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        
-        return atmost(nums,k)-atmost(nums,k-1);
+    int numberOfSubarrays(vector<int>& nums, int k){
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 };
