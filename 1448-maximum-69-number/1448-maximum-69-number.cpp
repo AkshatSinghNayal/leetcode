32398ab4dd38f659364855375f11d1c06717e43a
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-16 21:11:55 +0530

    Time: 0 ms (100%), Space: 8 MB (41.82%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,19 +1,36 @@
-class Solution {
-public:
-    int maximum69Number (int num) {
-        int maxi =0; 
-        string result = to_string(num);
-        int right = result.size()-1;
-        while(right>=0){
-            if((result[right]-'0') <9 ){
-                maxi = right;
-            }
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         int maxi =0; 
+//         string result = to_string(num);
+//         int right = result.size()-1;
+//         while(right>=0){
+//             if((result[right]-'0') <9 ){
+//                 maxi = right;
+//             }
             
 
-            right--;
+//             right--;
+//         }
+//         result[maxi] ='9';
+//          num = stoi(result);
+//         return num;
+//     }
+// };
+
+class Solution {
+public:
+    int maximum69Number(int num) {
+        string s = to_string(num);
+        
+        // Change the first '6' we encounter to '9'
+        for (char &c : s) {
+            if (c == '6') {
+                c = '9';
+                break; // only change one digit
+            }
         }
-        result[maxi] ='9';
-         num = stoi(result);
-        return num;
+        
+        return stoi(s);
     }
 };
