9caa2bf657a57346e36ffac8c507d219e3764e60
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-19 02:18:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.cpp
+++ Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.cpp
@@ -1,24 +1,23 @@
-
 class Solution {
-  public:
-
-    vector<int> count_NGE(vector<int> &arr, vector<int> &indices) {
+public:
+    vector<int> count_NGE(vector<int>& arr, vector<int>& indices) {
         int n = arr.size();
-        stack<int> st; ;
-        vector<int> ans ; 
-            for(int j = 0;  j<indices.size() ; j++){
-                int value = arr[indices[j]]; 
-             for(int i = indices[j]+1 ; i<n ; i++){
-                    
-                if(value<arr[i]){
-                    st.push(arr[i]);
-                }
-               
-                
+        vector<int> nextGreaterCount(n, 0);
+
+        // Brute force counting (correct way)
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) cnt++;
             }
-            ans.push_back(st.size());
-            st = stack<int>();
+            nextGreaterCount[i] = cnt;
         }
-        return ans ;
+
+        // Answer for queries
+        vector<int> ans;
+        for (int idx : indices) {
+            ans.push_back(nextGreaterCount[idx]);
+        }
+        return ans;
     }
 };
