b4d6346826c7d8f09383ba682d760103c0915447
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-20 02:54:56 +0530

    Time: 0 ms (100%), Space: 27.4 MB (8.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -7,16 +7,17 @@ public:
         prefix[0] = height[0];
         suffix[size] = height[size];
         int waterStored = 0 ;
+        int leftMax = -1;
 
-        for( int i = 1 ; i<size+1 ; i++){
-            prefix[i] = max(prefix[i-1] ,height[i]);
-        }
+        // for( int i = 1 ; i<size+1 ; i++){
+        //     prefix[i] = max(prefix[i-1] ,height[i]);
+        // }
         for( int j = size-1 ; j>= 0 ; j--){
             suffix[j] = max( suffix[j+1], height[j]);
         }
 
         for( int i = 0 ; i<height.size() ; i++){
-            int leftMax = prefix[i];
+            leftMax = max(leftMax,height[i]);
             int rightMax = suffix[i];
             waterStored+=min(leftMax , rightMax) - height[i];
         }
