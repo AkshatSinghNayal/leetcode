34bad0e872ab727cbdf4e92e16d47050a40b9ea9
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-20 03:10:34 +0530

    Time: 0 ms (100%), Space: 25.8 MB (99.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int> prefix(height.size() , 0 ); 
-        vector<int> suffix(height.size() , 0 ) ;
-        int size = height.size()-1;
-        prefix[0] = height[0];
-        suffix[size] = height[size];
-        int waterStored = 0 ;
-        int leftMax = -1;
+        int left = 0 ; 
+        int right = height.size()-1 ; 
+        int waterStored = 0 ; 
+        int leftMax = -1 , rightMax = -1 ;
+        while( left < right){
+             leftMax = max(leftMax,height[left]); 
+             rightMax = max(rightMax , height[right]);
 
-        // for( int i = 1 ; i<size+1 ; i++){
-        //     prefix[i] = max(prefix[i-1] ,height[i]);
-        // }
-        for( int j = size-1 ; j>= 0 ; j--){
-            suffix[j] = max( suffix[j+1], height[j]);
+             if( leftMax < rightMax){
+                waterStored+= leftMax - height[left];
+                left++;
+             }
+             else{
+                waterStored+= rightMax - height[right];
+                right--;
+             }
+
         }
-
-        for( int i = 0 ; i<height.size() ; i++){
-            leftMax = max(leftMax,height[i]);
-            int rightMax = suffix[i];
-            waterStored+=min(leftMax , rightMax) - height[i];
-        }
-
         return waterStored;
     }
 };
