59dacd62ac701a53055c4d8b921c79537de3e866
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-23 03:42:28 +0530

    Time: 4 ms (60.76%), Space: 28.6 MB (37.38%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,21 +5,21 @@ public:
         vector<int> ans(n, -1);
         stack<int> st;
 
+
         // Iterate from 2n-1 to 0 to simulate circular array
-        for(int i = 2*n - 1; i >= 0; i--) {
-            int curr = nums[i % n];
+        for( int i  = 2*n-1 ; i>= 0 ; i--){
+            int curr = (i%n);
+            while(!st.empty() && st.top()<= nums[curr]) st.pop();
 
-            while(!st.empty() && st.top() <= curr) {
-                st.pop();
-            }
-
-            if(i < n) {  // Fill ans only for first n indices
-                if(!st.empty()) {
+            if( i < n ) {
+              if(!st.empty()) {
                     ans[i] = st.top();
                 }
             }
 
-            st.push(curr);
+
+
+            st.push(nums[curr]);
         }
 
         return ans;
