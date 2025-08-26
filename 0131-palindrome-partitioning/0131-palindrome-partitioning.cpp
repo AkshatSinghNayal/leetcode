2b7f93fe1bc4b72fb51e31a1e8e2ec8b4f55e25d
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-08-27 02:31:07 +0530

    Time: 33 ms (80.38%), Space: 53.3 MB (93.97%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    // Helper to check if a string is a palindrome
-    bool isPalindrome(const string &s) {
-        int l = 0, r = s.size() - 1;
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
+    // Check if s[start..end] is a palindrome
+    bool isPalindrome(const string &s, int start, int end) {
+        while (start < end) {
+            if (s[start++] != s[end--]) return false;
         }
         return true;
     }
 
-    void partitionHelper(string s, vector<vector<string>>& ans, vector<string>& temp, int index) {
-        if (index == s.size()) {
-            ans.push_back(temp);  // reached the end, store the partition
+    void partitionHelper(const string &s, vector<vector<string>> &ans, vector<string> &temp, int start) {
+        if (start == s.size()) {
+            ans.push_back(temp);
             return;
         }
 
-        for (int i = index; i < s.size(); i++) {
-            string result = s.substr(index, i - index + 1);  // current substring
-            if (isPalindrome(result)) {                      // check palindrome
-                temp.push_back(result);                      // choose
-                partitionHelper(s, ans, temp, i + 1);       // recurse
-                temp.pop_back();                             // backtrack
+        string curr = "";
+        for (int i = start; i < s.size(); i++) {
+            curr += s[i];  // build substring manually
+            if (isPalindrome(s, start, i)) { // check palindrome on indices
+                temp.push_back(curr);
+                partitionHelper(s, ans, temp, i + 1);  // recurse
+                temp.pop_back();  // backtrack
             }
         }
     }
 
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans; 
+        vector<vector<string>> ans;
         vector<string> temp;
         partitionHelper(s, ans, temp, 0);
-        return ans;  
+        return ans;
     }
 };
