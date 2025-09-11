c616db7d163fd28f7b2d2932bbb858f330aba653
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-09-11 15:20:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
@@ -1,34 +1,56 @@
-class Solution {
-public:
+// Structure of node
+/*class Node {
+   public:
+    int data;
+    Node *left;
+    Node *right;
 
-    bool maxHeap(Node* tree, int length) {
-        if (tree == nullptr) return true; // base case
+    Node(int data) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class Solution {
+  public: 
+  
+    bool Max( Node* tree  ){
+        
+       if (tree == nullptr) return true; // base case
         int largest = tree->data;
 
         if (tree->left && tree->left->data > largest) return false;
         if (tree->right && tree->right->data > largest) return false;
 
         // Check left and right subtrees
-        return maxHeap(tree->left, length) && maxHeap(tree->right, length);
+        return Max(tree->left) && Max(tree->right);
     }
-
-    bool CBT(Node* tree, int length, int index) {
-        if (tree == nullptr) return true;
-        if (index >= length) return false;
-
-        bool left = CBT(tree->left, length, 2 * index + 1);
-        bool right = CBT(tree->right, length, 2 * index + 2);
-
-        return left && right;
+  
+    bool CBT( Node* tree  , int length , int index ){
+        
+        if( tree == nullptr) return true; 
+        if( length<=index) return false ;
+        
+        bool left = CBT(tree->left , length , index*2+1);
+        bool right = CBT(tree->right , length , index*2+2);
+        
+        
+        return (left&&right) ;
     }
-
-    int lengthOfTree(Node* tree) {
-        if (tree == nullptr) return 0; // count all nodes
-        return 1 + lengthOfTree(tree->left) + lengthOfTree(tree->right);
+    
+    int findLen(Node* tree){
+        if( tree == nullptr) return 0 ; 
+        
+        return 1+findLen( tree->left) + findLen( tree->right);
     }
-
+  
     bool isHeap(Node* tree) {
-        int length = lengthOfTree(tree);
-        return CBT(tree, length, 0) && maxHeap(tree, length);
+        
+        int length = findLen( tree );
+        if( CBT( tree , length , 0) && Max( tree )){
+            return true ; 
+        }
+        return false; 
+        
     }
 };
