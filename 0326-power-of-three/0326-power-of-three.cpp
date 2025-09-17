53c7791ebf6de134875b759cef29fb7188ece7bc
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-09-17 18:23:58 +0530

    Time: 4 ms (47.98%), Space: 8.8 MB (77.7%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,21 +1,10 @@
-// class Solution {
-// public:
-//     bool isPowerOfThree(int n) {
-//         if (n <= 0) return false;
-//         long long val = 1;
-//         while (val < n) {
-//             val *= 3;
-//         }
-//         return val == n;
-//     }
-// };
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
-        while (n % 3 == 0) {
-            n /= 3;
+        if(n <= 0 ) return false ;
+        while( n%3 == 0 ){
+            n/=3 ; 
         }
-        return n == 1;
+        return n== 1 ;
     }
 };
