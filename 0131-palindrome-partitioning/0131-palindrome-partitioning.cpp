bf3845ae1f4e5ad2315159df39ba3216a11a8a6f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-09-25 12:04:58 +0530

    Time: 27 ms (95.67%), Space: 53.5 MB (85.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    // Check if s[start..end] is a palindrome
-    bool isPalindrome(const string &s, int start, int end) {
+    bool CheckPalindrome(const string& s, int start, int end) {
         while (start < end) {
-            if (s[start++] != s[end--]) return false;
+            if (s[start++] != s[end--])
+                return false;
         }
         return true;
     }
 
-    void partitionHelper(const string &s, vector<vector<string>> &ans, vector<string> &temp, int start) {
-        if (start == s.size()) {
+    void partioning(string& s, vector<vector<string>>& ans, vector<string>& temp,
+                    int index) {
+
+        if (index == s.size()) {
             ans.push_back(temp);
-            return;
         }
 
         string curr = "";
-        for (int i = start; i < s.size(); i++) {
-            curr += s[i];  // build substring manually
-            if (isPalindrome(s, start, i)) { // check palindrome on indices
+        for (int i = index; i < s.size(); i++) {
+            curr += s[i];
+            if (CheckPalindrome(s, index, i)) {
                 temp.push_back(curr);
-                partitionHelper(s, ans, temp, i + 1);  // recurse
-                temp.pop_back();  // backtrack
+                partioning(s, ans, temp, i + 1);
+                temp.pop_back();
             }
         }
     }
@@ -28,7 +29,8 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> temp;
-        partitionHelper(s, ans, temp, 0);
+        partioning(s, ans, temp, 0);
+
         return ans;
     }
 };
