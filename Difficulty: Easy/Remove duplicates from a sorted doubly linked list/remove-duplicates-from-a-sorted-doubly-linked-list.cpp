e1d23f51c8ddcdbed9c70efa96811bed37c5cc70
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-09-26 22:42:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
+++ Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -1,39 +1,29 @@
-/*
-struct Node
-{
-    int data;
-    Node * next;
-    Node * prev;
-    Node (int x)
-    {
-        data=x;
-        next=NULL;
-        prev=NULL;
-    }
-
-};
-*/
-
 class Solution {
-  public:
+public:
 
-    Node *removeDuplicates(struct Node *head) {
-        Node* temp = head ; 
-        int ele = -1;
-       
-        while(temp){
-            if(ele!=temp->data){
-                ele=temp->data;
-            temp = temp->next;
-            }
-            else{
-                Node* nextNode = temp->next;
-                Node* prev = temp->prev;
-                
-                if(prev) prev->next = nextNode;
-                if(nextNode) nextNode->prev = prev;
-                delete temp;
-                temp = nextNode;
+    Node *removeDuplicates(Node *head) {
+        if (!head) return NULL;
+        
+        Node* temp = head;
+        int ele = temp->data - 1; // just smaller than first element
+        
+        while (temp) {
+            if (ele != temp->data) {
+                ele = temp->data;
+                temp = temp->next;
+            } else {
+                Node* before = temp->prev;
+                Node* after = temp->next;
+
+                if (before) before->next = after;
+                if (after) after->prev = before;
+
+                // update head if temp is head
+                if (temp == head) head = after;
+
+                Node* toDelete = temp;
+                temp = after; // move temp AFTER deletion
+                delete toDelete; // optional
             }
         }
         return head;
