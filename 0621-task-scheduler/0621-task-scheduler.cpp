9297ce315663aec58ab342706f3d817415179325
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-02 23:39:08 +0530

    Time: 45 ms (17.94%), Space: 47.7 MB (31.49%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,37 +1,60 @@
 class Solution {
 public:
 int leastInterval(vector<char>& tasks, int n) {
-if (n == 0) return tasks.size();
+if (n == 0) {
+return tasks.size();
+}
 
-
+    // Step 1: Count frequencies
     vector<int> freq(26, 0);
-    for (char c : tasks) freq[c - 'A']++;
+    for (int i = 0; i < tasks.size(); i++) {
+        char c = tasks[i];
+        freq[c - 'A']++;
+    }
 
+    // Step 2: Push frequencies into max heap
     priority_queue<int> pq;
-    for (int f : freq) if (f > 0) pq.push(f);
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            pq.push(freq[i]);
+        }
+    }
 
     int time = 0;
+
+    // Step 3: Process tasks in cycles of n+1
     while (!pq.empty()) {
         vector<int> temp;
         int executed = 0;
 
-        // try to execute up to n+1 tasks in this cycle
-        for (int i = 0; i <= n; ++i) {
+        // pick up to n+1 tasks
+        for (int i = 0; i <= n; i++) {
             if (!pq.empty()) {
-                int cur = pq.top(); pq.pop();
-                cur--;
+                int cur = pq.top();
+                pq.pop();
+
+                cur = cur - 1;
                 executed++;
-                if (cur > 0) temp.push_back(cur);
+
+                if (cur > 0) {
+                    temp.push_back(cur);
+                }
             }
         }
 
-        // push unfinished tasks back into heap
-        for (int v : temp) pq.push(v);
+        // push remaining tasks back into heap
+        for (int i = 0; i < temp.size(); i++) {
+            pq.push(temp[i]);
+        }
 
-        // if there are still tasks left, this cycle was full length (n+1),
-        // otherwise only count the tasks actually executed (no trailing idle).
-        if (pq.empty()) time += executed;
-        else time += (n + 1);
+        // Step 4: Add time
+        if (pq.empty()) {
+            // no more tasks left, only add executed tasks
+            time = time + executed;
+        } else {
+            // tasks still left, must count whole cycle
+            time = time + (n + 1);
+        }
     }
 
     return time;
