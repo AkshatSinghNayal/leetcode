bbf7a6aec87061cc1dfc819bdc58b6fc7505334f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-21 18:56:15 +0530

    Time: 0 ms (100%), Space: 52.3 MB (66%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size() == 1 ) return true;
+        // if(nums.size() == 1 ) return true;
         int index = 0 ; 
         int i  = 0 ; 
         int n  = nums.size() ; 
