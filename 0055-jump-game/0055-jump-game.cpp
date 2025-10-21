fda113fdbe66873da03a6129a02e6b3e4ac4037d
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-21 18:55:56 +0530

    Time: 3 ms (28.4%), Space: 52.2 MB (66%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,11 +6,11 @@ public:
         int i  = 0 ; 
         int n  = nums.size() ; 
         while(i<n){
-            if(index == i && nums[i] ==0 ) return false ;
+            // if(index == i && nums[i] ==0 ) return false ;
+            if(index<i) return false ;
             if(index<i+nums[i]){
                 index = i+nums[i]; 
             }
-            if(index<i) return false ;
             if(index>=n-1) return true ;
 
             i++; 
