ff44333e00d1bfb29b8bc9bce79bbdfb247cd2b2
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-23 17:11:10 +0530

    Time: 0 ms (100%), Space: 23 MB (31.23%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n, 1); // everyone gets at least 1 candy
+        vector<int> arr(n,1); 
+        for( int i = 1 ; i<n ; i++){
+            if( ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
 
-        // Left to right
-        for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i-1])
-                candies[i] = candies[i-1] + 1;
         }
-
-        // Right to left
-        for(int i = n-2; i >= 0; i--) {
-            if(ratings[i] > ratings[i+1])
-                candies[i] = max(candies[i], candies[i+1]+1);
+        for(int i = n-2 ;i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i] = max(arr[i],arr[i+1]+1); 
+            }
         }
-
-        int sum = 0;
-        for(int c : candies) sum += c;
-        return sum;
+        int sum = accumulate(arr.begin(),arr.end(),0);
+        return sum; 
     }
 };
