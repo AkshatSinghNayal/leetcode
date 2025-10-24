5d07123608774807d3a445f057903ea34e4be7f3
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-25 03:49:55 +0530

    Time: 0 ms (100%), Space: 17 MB (70.64%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void levelHelp(TreeNode* root, vector<vector<int>>& ans, int level) {
-        if(root == nullptr) return;
 
-        if(ans.size() == level) {
-            ans.push_back({}); // create new vector for this level
+        if( root == nullptr) return ; 
+        if(ans.size()==level){
+            ans.push_back({}); 
         }
 
-        ans[level].push_back(root->val); // add node to its level
+        ans[level].push_back(root->val); 
+        levelHelp( root->left , ans , level+1 ); 
+        levelHelp( root->right , ans , level+1 ); 
 
-        levelHelp(root->left, ans, level + 1);
-        levelHelp(root->right, ans, level + 1);
     }
 
     vector<vector<int>> levelOrder(TreeNode* root) {
