fd0959ffd8a7a1bf32554d5d07dbc938e001512e
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-10-25 23:46:52 +0530

    Time: 0 ms (100%), Space: 17.2 MB (43.39%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,21 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void levelHelp(TreeNode* root, vector<vector<int>>& ans, int level) {
 
-        if( root == nullptr) return ; 
-        if(ans.size()==level){
+    void helperFunction(TreeNode* root , vector<vector<int>>& ans , int level ){
+        if( root == nullptr ) return ;
+        if( ans.size() == level ) {
             ans.push_back({}); 
         }
-
         ans[level].push_back(root->val); 
-        levelHelp( root->left , ans , level+1 ); 
-        levelHelp( root->right , ans , level+1 ); 
-
+        helperFunction( root->left , ans , level+1); 
+        helperFunction( root->right , ans , level +1 ); 
     }
 
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        levelHelp(root, ans, 0);
-        return ans;
+        vector<vector<int>>ans ; 
+        helperFunction( root , ans , 0 ) ;
+        return ans ; 
     }
 };
