b37eae9e317753b185e5ba3d07403695d21f980c
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-01 00:06:28 +0530

    Time: 3 ms (27.59%), Space: 23.6 MB (93.16%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -6,32 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int helper(TreeNode* root , int &maxi ) {
-        if (root == nullptr)
-            return 0; // base case: empty tree → depth = 0
+    
+    int helper ( TreeNode* root , int &maxi ){
+        if(!root) return 0; 
 
-        int left = helper(root->left , maxi);
-        int right = helper(root->right , maxi );
+        int left = helper(root->left , maxi ) ; 
+        int right = helper( root->right , maxi) ; 
 
-        maxi = max(maxi , left+right);
+        maxi = max ( maxi , left+right ); 
 
-        return 1 + max(left, right); // include current node
+        return 1+ max(right,left); 
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        int maxi = INT_MIN;
-         helper( root ,maxi );
-
-         return maxi ; 
-
-        
-
-        
+        int maxi = INT_MIN; 
+        helper( root , maxi ) ; 
+        return maxi ;
     }
 };
