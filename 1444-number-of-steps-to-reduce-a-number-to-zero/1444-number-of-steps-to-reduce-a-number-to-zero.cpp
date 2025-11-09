d871976df9cb05352766bd9d84b4c5f52dd6c384
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-09 20:08:11 +0530

    Time: 0 ms (100%), Space: 8 MB (44%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int count = 0;
-        while(num>0){
-            if(num%2==0){
-                num/=2 ; 
-            }
-            else{
-                num-=1;
-            }
-            count++;
+        if (num == 0) return 0;
+
+        int steps = 0;
+        while (num > 0) {
+            // if the number is odd, subtract 1 (last bit = 1)
+            // if even, just shift right
+            steps += (num & 1) ? 2 : 1;
+            num >>= 1;
         }
-        return count ;
+        // subtract 1 because the last subtraction happens when num becomes 0
+        return steps - 1;
     }
 };
