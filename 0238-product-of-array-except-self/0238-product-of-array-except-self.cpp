7ec9fa09c3a86c51d7df3b2234e52ae92ce047d8
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-13 18:15:55 +0530

    Time: 4 ms (21.89%), Space: 40.1 MB (78.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int zero_index = -1;
-        int sum = 1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0 && zero_index > -1) {
-                return std::vector<int>(nums.size(), 0);
-            }
-            if (nums[i] == 0) {
-                zero_index = i;
-            } else {
-                sum = sum * nums[i];
-            }
+        int n = nums.size();
+        
+        // Special case for size 1
+        if (n == 1) {
+            return {0};  // No other elements to multiply with
         }
-        std::vector<int> res(nums.size(), 0);
-        if (zero_index > -1) {
-            res[zero_index] = sum;
-            return res;
+        
+        // Result array to store the answer
+        vector<int> result(n, 1);
+        
+        // Calculate prefix product
+        int prefix = 1;
+        for (int i = 0; i < n; ++i) {
+            result[i] = prefix;
+            prefix *= nums[i];
         }
-        for (int i = 0; i < nums.size(); i++) {
-            res[i] = sum / nums[i];
+        
+        // Calculate suffix product in reverse and multiply with result
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            result[i] *= suffix;
+            suffix *= nums[i];
         }
-        return res;
+        
+        return result;
     }
 };
