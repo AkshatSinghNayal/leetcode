96e945752a4de16fb5c8e1fec2c4046e1907e761
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-13 18:05:58 +0530

    Time: 1 ms (50.91%), Space: 40 MB (94.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-       int n  = nums.size() ; 
-       vector<int> preffix , suffix(n) ; 
-       int product = 1; 
-       for( auto &it : nums ){
-            product*=it; 
-            preffix.push_back( product ); 
-       }  
-      product = 1 ;
-       for( int i  = n-1 ;  i>=0 ;i--){
-            product *= nums[i]; 
-            suffix[i] = product ; 
-       }
-
-        nums[0] = suffix[1]; 
-        nums[n-1] = preffix[n-2];
-        for( int i  = 1 ; i<n-1 ; i++){
-            nums[i]= preffix[i-1]*suffix[i+1]; 
+        int zero_index = -1;
+        int sum = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0 && zero_index > -1) {
+                return std::vector<int>(nums.size(), 0);
+            }
+            if (nums[i] == 0) {
+                zero_index = i;
+            } else {
+                sum = sum * nums[i];
+            }
         }
-
-
-       
-       return nums;
+        std::vector<int> res(nums.size(), 0);
+        if (zero_index > -1) {
+            res[zero_index] = sum;
+            return res;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] = sum / nums[i];
+        }
+        return res;
     }
 };
