2d83203e78606e37fd216b3722b130036b44c1c9
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-17 00:55:16 +0530

    Time: 0 ms (100%), Space: 12.8 MB (5.66%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int numSub(string s) {
-        long long count = 0 ;  
-        const int MOD = 1e9 + 7 ; 
-        int left = 0 , right =0 ; 
 
-        while( s[left] =='0') left++; 
-        right = left ; 
-
-        while( right < s.size()) {
-
-            if(s[right] == '0'){
-            while(s[right] =='0') right++; 
-            left = right ; 
-
+        s += '0';
+        int count = 0;
+        int ans =0;
+        const int mod = 1e9+7;
+        for (char c : s) {
+            if (c == '0') {
+                ans = (ans+ (count *1LL* (count + 1)) / 2)%mod;
+                count=0;
+            } else {
+                count++;
             }
-            
-            while( s[right] == '1' && right<s.size()){
-                right++; 
-            }
-
-            long long len = (right-1)-left+1 ;
-
-            count += (len * (len + 1) / 2) % MOD;
-            
         }
-        return int(count) ;
+        return ans;
     }
 };
