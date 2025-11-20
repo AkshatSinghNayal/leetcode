ba7c49a6a5518ea9513e233be50a88863bda6520
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-20 09:30:15 +0530

    Time: 4 ms (39.54%), Space: 14.1 MB (52.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
 
-    void combinationSumHelper( vector<int>& candidates, int target , vector<vector<int>>& ans, vector<int>& temp , int index ){
-
-        if( target == 0){
-            ans.push_back(temp) ;
-            return;
+    void solve(vector<int>& nums ,  vector<int>& temp , vector<vector<int>>& ans , int target , int index , int sum  ){
+        
+        if(  target == 0 ){
+            ans.push_back(temp); 
+            return ; 
         }
-        if( target<0) return ; 
-        if( index>= candidates.size()) return ;
+        if(  index >= nums.size() || target <0 ) return ; 
 
-
-            if(candidates[index]<=target){
-            temp.push_back(candidates[index]);
-            combinationSumHelper( candidates , target-candidates[index] , ans , temp, index );
-            temp.pop_back() ; 
-            }
-             combinationSumHelper( candidates , target, ans , temp, index+1 );
-          
-
+        temp.push_back(nums[index]); 
+        solve( nums , temp , ans , target-nums[index] , index , sum+nums[index]); 
+        temp.pop_back(); 
+        solve( nums , temp , ans , target , index+1 , sum ) ;
 
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans ; 
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<int> temp ; 
-
-        combinationSumHelper( candidates , target , ans , temp , 0);
-
+        vector<vector<int>> ans ;
+        solve( nums , temp , ans , target , 0 , 0 ) ; 
         return ans ; 
-        
     }
 };
