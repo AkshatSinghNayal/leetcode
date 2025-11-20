2865ba7d7151f70409ca78571314fba2f1801203
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-20 08:48:40 +0530

    Time: 0 ms (100%), Space: 13.7 MB (14.68%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
 
-    void helperFunction( vector<int>& nums , vector<int>& temp , vector<vector<int>>& ans , int index ){
-        ans.push_back(temp) ; 
-        for( int i = index ;i<nums.size() ; i++){
-            if( i > index && nums[i] == nums[i-1]) continue;
-            temp.push_back(nums[i]); 
-            helperFunction( nums , temp , ans , i+1); 
-            temp.pop_back();
+    void solve(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans) {
+        if (index == nums.size()) {
+            ans.push_back(temp);
+            return;
         }
+
+        // -----------------------------
+        // OPTION 1: Do NOT take nums[index]
+        // -----------------------------
+        int next = index + 1;
+        // skip all duplicates when not taking
+        while (next < nums.size() && nums[next] == nums[index]) {
+            next++;
+        }
+        solve(next, nums, temp, ans);
+
+        // -----------------------------
+        // OPTION 2: Take nums[index]
+        // -----------------------------
+        temp.push_back(nums[index]);
+        solve(index + 1, nums, temp, ans);
+        temp.pop_back();
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end()); 
-        vector<int> temp ;
-        vector<vector<int>> ans ; 
-
-        helperFunction( nums , temp , ans , 0 ); 
-        return ans ; 
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0, nums, temp, ans);
+        return ans;
     }
 };
