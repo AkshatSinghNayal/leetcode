574cf68c50881cb6b6edc18a2c7c9e13874e9d5b
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-22 23:23:18 +0530

    Time: 0 ms (100%), Space: 9.1 MB (9.09%) - LeetHub

--- 3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.cpp
+++ 3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int minimumFlips(int n) {
-        string revStr =""; 
-        int k  = n ; 
-        while(k!=0){
-            revStr.push_back((k&1)+'0');
-            k>>=1;
-        }
-        int count = 0;
+        int msb = 31 - __builtin_clz(n); 
+        int k  = n ;
+        string s  = "";
+        for( int i = msb ; i>=0 ;i-- ){
+            if( (1ll << i)&(n) ){
+                s+='1';
+            }
+            else{
+                s+='0'; 
+            }
+        }  
+        string revStr = s ; 
+        int operations = 0 ;
         reverse(revStr.begin(),revStr.end()); 
-        k = n; 
-        for( int i  = 0  ; i<revStr.size() ; i++){
-            if( (k&1) != revStr[i]-'0') count++;
-            k>>=1 ;
-        }
-        return count;
+        for( int i = 0 ; i<s.size() ;i++){
+            if( s[i] != revStr[i]) operations++;
+        } 
+        return operations ;
     }
 };
