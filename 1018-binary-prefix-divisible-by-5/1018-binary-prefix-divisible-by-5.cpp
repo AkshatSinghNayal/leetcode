910750730140ff0cb80a9b128678a2ef37482975
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-25 22:08:53 +0530

    Time: 0 ms (100%), Space: 18.4 MB (37.19%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -6,7 +6,7 @@ public:
         
         for (int i = 0; i < nums.size(); i++) {
             // Update the result by shifting left and adding the current number (binary digit)
-            result = ((result << 1) + nums[i])%5;
+            result = ((result << 1) | nums[i])%5;
             
             // Check if the current result is divisible by 5
             ans.push_back(result   == 0);
