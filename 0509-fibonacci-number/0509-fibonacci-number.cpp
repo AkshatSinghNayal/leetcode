a44d679655c5ace82f54b5682f54b4994415501e
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-29 15:58:58 +0530

    Time: 21 ms (21.16%), Space: 8.3 MB (11.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
 
-    int findFib( int  n   , vector<int>& dp){
-        
-        if( dp[n]!=-1 ){
-            return dp[n] ; 
+    int helperFunction( vector<int>& dp , int n){
+        if(n<=1) return n ; 
+
+        if(dp[n] != -1){
+            dp[n]; 
         }
 
-        int left = findFib( n-1 , dp); 
-        int right= findFib(n-2  , dp ) ; 
-
-        
-         
-        return dp[n]= right+left ; 
-
-     
+        return dp[n] = helperFunction( dp , n-1 ) + helperFunction(dp , n-2 ) ;
     }
 
     int fib(int n) {
-        vector<int> dp(n+1 , -1 ) ; 
-        if( n== 0  || n== 1 ) return n; 
-        dp[0]= 0 ; 
-        dp[1]=1 ; 
-        findFib( n , dp ) ;
 
-        return dp[n] ;
+        vector<int> dp(n+1 , -1 ); 
+       return  helperFunction( dp, n ) ; 
+        
+
     }
 };
