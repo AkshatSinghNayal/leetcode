6615d8b6441ea30a77b1ad4da1ead4dd59199a3a
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-11-29 20:14:18 +0530

    Time: 0 ms (100%), Space: 22.9 MB (97.53%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,20 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int check(TreeNode* root) {
-        if (!root) return 0;
 
-        int left = check(root->left);
-        if (left == -1) return -1; // already unbalanced
+    int solve( TreeNode* root , int& ans ){
+        if( !root ) return 0; 
 
-        int right = check(root->right);
-        if (right == -1) return -1;
+        int left = solve( root->left  , ans) ; 
+        int right  = solve( root->right , ans) ; 
 
-        if (abs(left - right) > 1) return -1; // unbalanced at this node
+        if( abs(left-right) > 1 )ans = INT_MAX ; 
 
-        return 1 + max(left, right); // balanced, return height
+        return 1+ max( left , right ) ; 
+
+
     }
 
     bool isBalanced(TreeNode* root) {
-        return check(root) != -1;
+        int ans =  0 ; 
+        
+         solve( root , ans) ; 
+        return ( ans == INT_MAX  )? false : true; 
     }
 };
