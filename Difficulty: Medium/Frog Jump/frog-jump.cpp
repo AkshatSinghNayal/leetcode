b5da8ba9d8e733a3cb246f3b56d1f6aae74c1d5f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-03 18:36:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,14 +1,27 @@
 class Solution {
   public:
-    int minCost(vector<int>& arr) {
-        // Code here
-        int n=arr.size();
-        pair<int,int> a={arr[0],0},b=a;
-        for(int i=1;i<n;i++) {
-            int c=min(a.second+abs(a.first-arr[i]),b.second+abs(b.first-arr[i]));
-            b=a;
-            a={arr[i],c};
-        }
-        return a.second;
-    }
+  
+    int minCost(vector<int>& height) {
+        int n = height.size() ; 
+        if( n  == 1 ) return 0 ; 
+        
+        int prev1 = 0 ;
+        int prev2 = 0 ; 
+        
+        for( int i  = 1 ; i<n ; i++){
+            int left =  prev1 + abs(height[i] - height[i-1]);
+            int right = INT_MAX ;  
+             if (i>1)
+              {
+                  right = prev2 + abs( height[i] - height[i-2]);
+              }
+              
+            prev2 = prev1 ; 
+            
+            
+        } prev1 =  min( left , right ) ;
+            
+        
+        return prev1 ;
+    }   
 };
