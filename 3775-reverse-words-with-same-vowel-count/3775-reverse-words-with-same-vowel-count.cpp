40984a2abe63a19d7ef4aee53ec1d7eb5fe4a4fd
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-14 13:02:29 +0530

    Time: 19 ms (90%), Space: 35.5 MB (80%) - LeetHub

--- 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
+++ 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
@@ -1,42 +1,52 @@
 class Solution {
 public:
+    inline bool isVowel(char c) {
+        return c == 'a' ||
+               c == 'e' ||
+               c == 'i' ||
+               c == 'o' ||
+               c == 'u';
+    }
 
-    void reverseStr( string& s , int start , int end ){
-        while( start<end){
-            swap( s[start++] , s[end--]) ; 
+    void reverseStr(string& s, int start, int end) {
+        while (start < end) {
+            swap(s[start++], s[end--]);
         }
     }
 
     string reverseWords(string s) {
-        set<char> st ; int n = s.size() ; 
-        st.insert('a');  st.insert('e');  st.insert('i');  st.insert('o');  st.insert('u'); 
-        int i = 0 , j = 0 ; 
-        int count= 0 ; int firstCount ; 
-         while( i< n && j < n  ){
+        int n = s.size();
+        int i = 0, j = 0;
+        int count = 0, firstCount = 0;
 
-            while( j< n && s[j]!= ' '){
-                if(st.count(s[j]))count++; 
-                j++; 
-            }   
-            j++; 
-            i = j  ;
-            firstCount = count; 
-            break; 
+        // Count vowels in the first word
+        while (j < n && s[j] != ' ') {
+            if (isVowel(s[j])) count++;
+            j++;
         }
-        while( i<n && j< n ){
-             count = 0 ; 
-            while( j < n && s[j]!= ' '){
-                if(st.count(s[j]))count++; 
-                j++; 
-            }
-            if( count == firstCount ){
-                reverseStr( s, i , j-1 ); 
-            }
-            j++; 
-            i = j; 
+        firstCount = count;
 
+        // Move to second word
+        j++;
+        i = j;
+
+        // Process remaining words
+        while (i < n && j < n) {
+            count = 0;
+
+            while (j < n && s[j] != ' ') {
+                if (isVowel(s[j])) count++;
+                j++;
+            }
+
+            if (count == firstCount) {
+                reverseStr(s, i, j - 1);
+            }
+
+            j++;
+            i = j;
         }
-        return s ; 
+
+        return s;
     }
-
 };
