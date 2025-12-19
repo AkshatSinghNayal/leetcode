ac69cc22d19d0c9f094a9a8558e654ec736d4868
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-20 03:45:14 +0530

    Time: 15 ms (0.96%), Space: 9.3 MB (49.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dp(n, 0); // dp[j] stores number of ways to reach column j in current row
+         vector<vector<int>> dp(m, vector<int>(n, 0));
 
         for (int i = 0; i < m; i++) {
-            vector<int> temp(n, 0); // temporary array for the current row
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) {
-                    temp[j] = 1; // start cell
+                    dp[j][i] = 1;
                     continue;
                 }
-
-                int up = (i > 0) ? dp[j] : 0;       // value from previous row
-                int left = (j > 0) ? temp[j-1] : 0; // value from left in current row
-
-                temp[j] = up + left; // total ways to reach current cell
+                int up = (i > 0) ? dp[i-1][j] : 0;
+                int left = (j > 0) ? dp[i][j-1] : 0;
+                dp[i][j] = up + left;
             }
-            dp = temp; // update dp for the next row
         }
-
-        return dp[n-1]; // bottom-right cell
+        for( int i = 0 ; i< m ; i++){
+            for( int j  = 0 ; j<n ; j++){
+                cout<<dp[i][j] << " " ; 
+            }
+            cout<<endl; 
+        }
+        return dp[m-1][n-1];
     }
 };
