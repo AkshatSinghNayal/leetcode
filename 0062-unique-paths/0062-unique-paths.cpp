d0b137eee48570f8a3b105f4b4bd3f59a272fa84
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-20 03:36:36 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+         vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        // Initialize first row and first column
-        for(int i = 0; i < m; i++) dp[i][0] = 1;
-        for(int j = 0; j < n; j++) dp[0][j] = 1;
-
-        // Fill the rest of the DP table
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j][i] = 1;
+                    continue;
+                }
+                int up = (i > 0) ? dp[i-1][j] : 0;
+                int left = (j > 0) ? dp[i][j-1] : 0;
+                dp[i][j] = up + left;
             }
         }
 
