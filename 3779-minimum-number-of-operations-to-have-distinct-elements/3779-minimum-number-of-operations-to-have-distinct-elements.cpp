18c1ba29d4e280f97de89e5fc7d1feae0c361b2d
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-21 00:36:13 +0530

    Time: 105 ms (58.31%), Space: 187.7 MB (59.5%) - LeetHub

--- 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int dubplicate = 0 , count = 0  ;
-        int n = nums.size() ; 
-        unordered_map<int,int> mp ; 
-        for( auto &it : nums){
-            mp[it]++; 
-            if( mp[it] == 2 ) dubplicate++; 
+        map<int,int>mp;
+        int cnt=0;
+        int k=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            mp[nums[i]]++;
+            if(mp[nums[i]]>1)break;
+            cnt++;
         }
-        if( dubplicate == 0 ) return  0 ; 
-
-        int i = 0 ; 
-        while( i < n ){
-            for( int j = 0 ; j<3 and i< n ; i++, j++){
-                if(mp[nums[i]] ==  2){
-                    dubplicate--; 
-                }
-                mp[nums[i]]--; 
-            }
-            count++; 
-            if( dubplicate == 0 ) break; 
-        }
-        return count ;
+        int n=nums.size()-cnt;
+        return (n+2)/3;
     }
 };
