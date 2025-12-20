ec6944b0482d54b6297ecb7bd5e745dd6df71bda
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-21 03:02:50 +0530

    Time: 0 ms (100%), Space: 9.7 MB (43.06%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        long long head = 1, step = 1, remaining = n;
-        bool left = true;
-
-        while (remaining > 1) {
-            if (left || (remaining % 2 == 1)) {
-                head += step;
+        long long gap = 1 , rem = n , last =  1 ; 
+        bool left = true; 
+        while( rem > 1 ){
+            if( left == false and (rem%2 == 0) ){
+                rem/=2 ;
+                gap*=2;
+                left=!left;
+                continue;
             }
-            step *= 2;
-            remaining /= 2;
-            left = !left;
-        }
+            left=!left; 
+            last = last+gap ;
+            gap *=2 ; 
+            rem/=2; 
 
-        return (int)head;
+        }
+        return (int)last; 
     }
 };
