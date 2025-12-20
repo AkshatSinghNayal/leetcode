696f13df17c9541b74e6c192c99037c422c54d77
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-20 15:55:27 +0530

    Time: 0 ms (100%), Space: 9.6 MB (27.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-         vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[j][i] = 1;
-                    continue;
-                }
-                int up = (i > 0) ? dp[i-1][j] : 0;
-                int left = (j > 0) ? dp[i][j-1] : 0;
-                dp[i][j] = up + left;
-            }
-        }
-        for( int i = 0 ; i< m ; i++){
-            for( int j  = 0 ; j<n ; j++){
-                cout<<dp[i][j] << " " ; 
-            }
-            cout<<endl; 
-        }
-        return dp[m-1][n-1];
+    int solve( int m , int n , vector<vector<int>>&dp ){
+
+        if( m == 0 and n == 0 ) return 1 ;
+        if( n < 0 or m < 0  ) return  0 ; 
+
+        if( dp[m][n] != -1 ) return dp[m][n]; 
+
+        int up = solve( m-1 , n , dp) ; 
+        int left = solve( m , n-1 , dp) ; 
+
+        return dp[m][n] = ( up + left ) ; 
+
+
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp ( m , vector<int>(n , -1));
+       return solve(m-1 , n-1 , dp ) ;
+
     }
 };
