5d3e7afa6b8016f923dce09199843b950aaddb7f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-21 22:46:49 +0530

    Time: 0 ms (100%), Space: 20.4 MB (61.48%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        if (nums.size() <= 1) return 0;  // No jumps needed if the array size is 1 or less
-        
-        int left = 0, right = 0, far = 0, jump = 0;
-        
-        while (right < nums.size() ) {  // We don't need to jump if we're already at the last element
-            far = 0;  // Reset far for each jump range
-            
-            // Explore all positions within the current jump's range
-            for (int i = left; i <= right; i++) {
-                far = max(far, i + nums[i]);  // Calculate the farthest we can reach from i
+        int n = nums.size();
+        if (n <= 1) return 0;
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
-            
-            // After exploring, we've used one jump
-            jump++;
-            
-            // Update left to be the end of the current jump range, and right to the farthest we can go
-            left = right + 1;
-            right = far;
-            
-            // If we can reach the end, break early
-            if (right >= nums.size() - 1) break;
         }
-        
-        return jump;
+        return jumps;
     }
 };
