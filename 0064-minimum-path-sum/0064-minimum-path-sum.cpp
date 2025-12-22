68da52c326f0d6831c5752c1feec34362453028f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-22 23:59:47 +0530

    Time: 0 ms (100%), Space: 16.6 MB (13.37%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m  =  grid.size() ;
-        int n = grid[0].size(); 
-        vector<vector<long long>> dp(m , vector<long long>(n));
-        dp[0][0] = grid[0][0]; 
-        for( int i  = 1 ; i< n ; i++) dp[0][i] = dp[0][i-1]+ grid[0][i]; 
-        for( int j = 1 ; j< m ; j++) dp[j][0] = dp[j-1][0] + grid[j][0]; 
-        
-        for( int i = 1 ; i<m ; i++){
-            for( int j = 1 ; j<n ; j++){
-                int current =  grid[i][j]; 
-                dp[i][j] = min( current+dp[i-1][j] ,current+ dp[i][j-1]);
-                
+        int n  =  grid[0].size(); 
+        int m = grid.size(); 
+        vector<long long> prev(n); 
+        prev[0] = grid[0][0]; 
+        for( int i  = 1 ; i< n ; i++) prev[i] = prev[i-1]+grid[0][i]; 
+
+        for( int i = 1; i< m ; i++){
+            vector<long long> current(n); 
+            for( int j = 0 ; j< n ; j++){
+                if( j == 0 ) current[j] = grid[i][j] + prev[j]; 
+                else{
+                    int temp =grid[i][j]; 
+                    current[j] = (temp+prev[j] < current[j-1]+temp) ? temp+prev[j] : current[j-1]+temp;
+                }
             }
+            prev = current ; 
         }
-       
-        long long ans = dp[m-1][n-1]; 
-        return (int)ans; 
+        long long ans = prev[n-1]; 
+        return (int)ans;
     }
 };
