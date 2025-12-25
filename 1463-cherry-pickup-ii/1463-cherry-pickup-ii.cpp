cbf80857cbb3570b5c1c872940bbf639a0aa5f9b
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-25 15:16:19 +0530

    Time: 35 ms (86.86%), Space: 20.9 MB (69.62%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp, int row, int colIndexOfR1, int colIndexOfR2, int rowAtEachLayer, int colAtEachLayer) {
-       
-        if (colIndexOfR1 < 0 || colIndexOfR1 >= colAtEachLayer || colIndexOfR2 < 0 || colIndexOfR2 >= colAtEachLayer)
-            return -1e8; 
 
-       
-        if (row == rowAtEachLayer - 1) {
-          
-            if (colIndexOfR1 == colIndexOfR2)
-                return grid[row][colIndexOfR1];
-            else
-                return grid[row][colIndexOfR1] + grid[row][colIndexOfR2];
+    int solve(vector<vector<int>>& grid,vector<vector<vector<int>>>& dp , int row , int colR1 , int colR2 , int n , int totalRow ){
+
+        if( colR1<0 or colR1>=n or colR2<0 or colR2>=n ) return  -1e9; 
+
+        if (row == totalRow-1) {
+        return (colR1 == colR2) ?  grid[row][colR1] : grid[row][colR1] + grid[row][colR2];
         }
 
-        if (dp[row][colIndexOfR1][colIndexOfR2] != -1)
-            return dp[row][colIndexOfR1][colIndexOfR2];
+        if( dp[row][colR1][colR2] != -1 ) return dp[row][colR1][colR2] ; 
 
-        int maxi = -1e8;
-
-        
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                int value = 0;
-                if (colIndexOfR1 == colIndexOfR2)
-                    value = grid[row][colIndexOfR1]; 
-                else
-                    value = grid[row][colIndexOfR1] + grid[row][colIndexOfR2]; 
-
-                int result = value + solve(grid, dp, row + 1, colIndexOfR1 + i, colIndexOfR2 + j, rowAtEachLayer, colAtEachLayer);
-                maxi = max(maxi, result);
+        int maxi = -1e9 ; 
+        for( int i  = -1 ; i<= 1; i++){
+            for(int j= - 1; j<= 1; j++){
+                int value = ( colR1 ==colR2 ) ? grid[row][colR1] : grid[row][colR1] + grid[row][colR2]; 
+                int result = value + solve( grid , dp , row+1 , colR1+i , colR2+j ,n  , totalRow) ;
+                maxi = max( maxi ,result ) ; 
             }
         }
 
-        
-        return dp[row][colIndexOfR1][colIndexOfR2] = maxi;
+        return dp[row][colR1][colR2] = maxi ; 
+
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
-        int rowAtEachLayer = grid.size(); 
-        int colAtEachLayer = grid[0].size(); 
-
-        vector<vector<vector<int>>> dp(rowAtEachLayer, vector<vector<int>>(colAtEachLayer, vector<int>(colAtEachLayer, -1)));
-
-        int row = 0;
-        int colIndexOfR1 = 0, colIndexOfR2 = colAtEachLayer - 1;
-
+        int layers = grid.size(); 
+        int rowCount =  grid[0].size(); 
+        int colCount = grid[0].size(); 
         
-        return solve(grid, dp, row, colIndexOfR1, colIndexOfR2, rowAtEachLayer, colAtEachLayer);
+        vector<vector<vector<int>>>dp(layers , vector<vector<int>>(rowCount , vector<int>(colCount,-1))); 
+        return solve( grid , dp , 0 , 0 , colCount-1 , colCount , layers ); 
     }
 };
