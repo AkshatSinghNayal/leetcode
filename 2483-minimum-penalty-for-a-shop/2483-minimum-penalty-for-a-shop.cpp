6df2b028cd2ae2f8729024c38226e1deb3a4ab1c
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-26 16:13:40 +0530

    Time: 3 ms (89.07%), Space: 13.3 MB (91.48%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
+        int n = customers.size() ; 
+        int countN = 0 , countY = 0 ;
+        for( auto& it : customers ) if(it == 'Y') countY++; 
         
-        // penalty[i] = penalty if we close at hour i
-        // Before i: count 'N' (shop open, no customers)
-        // From i onwards: count 'Y' (shop closed, customers come)
-        
-        int countN = 0; // 'N's before current position
-        int countY = 0; // total 'Y's in string
-        
-        // Count total Y's
-        for (char c : customers) {
-            if (c == 'Y') countY++;
-        }
-        
-        int minPenalty = countY; // closing at hour 0
-        int bestHour = 0;
-        
-        for (int i = 0; i < n; i++) {
-            // Update counts as we move the closing time
-            if (customers[i] == 'N') {
-                countN++;
-            } else {
-                countY--;
-            }
-            
-            int penalty = countN + countY;
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                bestHour = i + 1;
+        int bestFit = countY ; 
+        int panelty =  0 ; 
+
+        for( int i = 0 ; i< n ; i++){
+            if( customers[i] ==  'N') countN++; 
+            else countY--; 
+
+            if( bestFit > countY+countN){
+                bestFit = countY+countN ; 
+                panelty = i+1 ; 
             }
         }
-        
-        return bestHour;
+
+        return panelty ; 
+
     }
 };
