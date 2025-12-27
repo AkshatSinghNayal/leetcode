4c4b5c5b7c2a2eb56c846f477e79a9c323069bea
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-28 00:44:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
+++ Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
@@ -17,37 +17,24 @@ struct Node
 class Solution {
   public:
     
-    bool isLeaf(Node* root ){
-        return ( root->left == nullptr && root->right == nullptr ) ;
-    }
-    
-    void helperFunction( Node* root , vector<vector<int>> &ans , vector<int> &temp ){
-         
-         if( root == nullptr ) return ; 
-         
-         if( isLeaf(root) ){
-             temp.push_back(root->data); 
-             ans.push_back(temp); 
-             temp.pop_back();
-             return ; 
-         }
-         
-        temp.push_back( root->data );
-        helperFunction( root->left , ans , temp ) ; 
-        helperFunction( root->right , ans , temp ) ;
-        
-        temp.pop_back() ; 
-        
+    void solve( Node* root ,vector<vector<int>>& ans , vector<int>&temp){
+        if(!root)return; 
+        if( !root->left and !root->right){
+            temp.push_back(root->data); 
+            ans.push_back(temp); 
+            temp.pop_back();
+            return; 
+        }
+        temp.push_back(root->data); 
+        solve(root->left, ans, temp ); 
+        solve(root->right , ans , temp ); 
+        temp.pop_back(); 
     }
   
     vector<vector<int>> Paths(Node* root) {
-        vector<vector<int>> ans ; 
-        if( root == nullptr ) return ans ;
-        vector<int> temp ; 
-        
-        helperFunction( root , ans ,temp ) ; 
-        
+    vector<vector<int>>ans; 
+       vector<int>temp; 
+        solve( root , ans , temp );
         return ans; 
-        
     }
 };
