e488519708726644e0c8230f6b13c95b10ee2887
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-27 15:57:19 +0530

    Time: 0 ms (100%), Space: 13.1 MB (51.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-         int n = triangle.size();
-
-        vector<vector<int>> dp(n);
-        for (int i = 0; i < n; i++) {
-            dp[i] = vector<int>(triangle[i].size(), 1e8);
-        }
-        dp[0][0] = triangle[0][0]; 
-
-        for( int i  =1 ; i< n ; i++){
-            for(int j  = 0 ; j<triangle[i].size(); j++){
-                int up = (j< triangle[i].size()-1 ) ? dp[i-1][j] : 1e8; 
-                int left = ( j-1 >=0 ) ? dp[i-1][j-1] : 1e8; 
-
-                dp[i][j]= triangle[i][j]+min(up,left);
+        int n = triangle.size(); 
+        int size = triangle[n-1].size(); 
+        vector<int>prev(size  , 1e8 ); 
+        prev[0]=triangle[0][0]; 
+        for( int i  =1 ; i<size ; i++){
+            vector<int> temp(size,1e8);
+            for(int j = 0; j<=i; j++){
+                int up = (j<i) ? prev[j] : 1e8 ; 
+                int left = (j-1>=0) ?  prev[j-1] : 1e8 ; 
+                temp[j]=triangle[i][j]+ min( up ,left ); 
             }
+            prev = temp ; 
         }
-        int mini = INT_MAX; 
-        for( int i  = 0;  i<dp[n-1].size() ; i++){
-            mini = min ( mini , dp[n-1][i]); 
-        }
-        return mini; 
+        int maxi = *min_element( prev.begin(),prev.end()); 
+        return maxi ; 
     }
 };
