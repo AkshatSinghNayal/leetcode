abaf0cf904e2d175c788101ec553d00548ac227a
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-27 23:36:10 +0530

    Time: 0 ms (100%), Space: 27.8 MB (75.23%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -6,33 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int helperFunction(TreeNode* root, int& maxi) {
-        if (root == nullptr)
-            return 0;
 
-        int left = helperFunction(root->left, maxi);
-        if (left <= 0)
-            left = 0;
-        int right = helperFunction(root->right, maxi);
-        if (right <= 0)
-            right = 0;
+    int solve(TreeNode* root , int& maxi ){
 
-        maxi = max(maxi, left + right + root->val);
-        // if (maxi < 0)
-        //     maxi = 0;
+        if( !root ) return 0 ; 
+        int left =  solve( root->left , maxi ); 
+        if( left <0 ) left = 0 ; 
+        int right = solve( root->right , maxi );
+        if(right<0) right= 0; 
 
-        return root->val + max(left, right);
+        maxi = ( maxi < left+right+root->val ) ?  left+right+root->val : maxi; 
+
+        return root->val + max( left , right ); 
+
     }
 
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN;
-        helperFunction(root, maxi);
-        return maxi;
+
+        int maxi = -1e8; 
+        int ans = solve( root , maxi );  
+        return maxi ; 
     }
 };
