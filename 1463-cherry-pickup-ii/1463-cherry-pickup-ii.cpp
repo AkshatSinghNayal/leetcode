82b2c2795a3f38b306b2574f1f9257737471f590
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2025-12-30 22:43:00 +0530

    Time: 48 ms (57.01%), Space: 20.9 MB (51.97%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>& grid,vector<vector<vector<int>>>& dp , int row , int colR1 , int colR2 , int n , int totalRow ){
+    int solve(vector<vector<int>>& grid , vector<vector<vector<int>>>& dp , int row , int rCol1 , int rCol2 , int rows  , int cols){
 
-        if( colR1<0 or colR1>=n or colR2<0 or colR2>=n ) return  -1e9; 
-
-        if (row == totalRow-1) {
-        return (colR1 == colR2) ?  grid[row][colR1] : grid[row][colR1] + grid[row][colR2];
+        if( rCol1 <0 or rCol1>=cols or rCol2<0 or rCol2>= cols) return -1e8; 
+        if( row == rows-1 ){
+            return( rCol1 == rCol2 ) ? grid[row][rCol1] : grid[row][rCol1]+grid[row][rCol2]; 
         }
 
-        if( dp[row][colR1][colR2] != -1 ) return dp[row][colR1][colR2] ; 
+        if( dp[row][rCol1][rCol2]  != -1 ) return dp[row][rCol1][rCol2]; 
 
-        int maxi = -1e9 ; 
-        for( int i  = -1 ; i<= 1; i++){
-            for(int j= - 1; j<= 1; j++){
-                int value = ( colR1 ==colR2 ) ? grid[row][colR1] : grid[row][colR1] + grid[row][colR2]; 
-                int result = value + solve( grid , dp , row+1 , colR1+i , colR2+j ,n  , totalRow) ;
-                maxi = max( maxi ,result ) ; 
-            }
+        int maxi = INT_MIN;     
+        for( int i  = -1 ; i<=1 ; i++){
+            for( int j = -1 ; j<=1 ; j++){
+                int val = (rCol1==rCol2) ? grid[row][rCol1] : grid[row][rCol1]+grid[row][rCol2]; 
+                maxi=max(maxi , val+solve(grid , dp , row+1 , rCol1+i , rCol2+j , rows ,cols)); 
+            } 
+            
         }
 
-        return dp[row][colR1][colR2] = maxi ; 
+        return dp[row][rCol1][rCol2] = maxi; 
 
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
-        int layers = grid.size(); 
-        int rowCount =  grid[0].size(); 
-        int colCount = grid[0].size(); 
-        
-        vector<vector<vector<int>>>dp(layers , vector<vector<int>>(rowCount , vector<int>(colCount,-1))); 
-        return solve( grid , dp , 0 , 0 , colCount-1 , colCount , layers ); 
+        int rows = grid.size(); 
+        int cols = grid[0].size(); 
+        vector<vector<vector<int>>>dp(rows ,vector<vector<int>>(cols , vector<int>( cols , -1 ))); 
+        return solve( grid , dp , 0 , 0 , cols-1 , rows , cols) ;
     }
 };
