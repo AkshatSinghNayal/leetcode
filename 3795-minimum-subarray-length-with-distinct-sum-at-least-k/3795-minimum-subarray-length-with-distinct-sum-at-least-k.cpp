c6dbd9af0449f92eab2c0c74840dbedc7002b566
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-01-04 01:38:49 +0530

    Time: 212 ms (33.33%), Space: 285.2 MB (33.33%) - LeetHub

--- 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int minLength(vector<int>& nums, int k) {
-        int len = INT_MAX;
-        int left = 0, right = 0;
-        int n = nums.size();
         unordered_map<int,int> mp;
+        int left = 0;
+        int n = nums.size();
         long long sum = 0;
-       
-
-        while (right < n) {
-            if (mp[nums[right]] == 0) {
+        int length = INT_MAX;
+        
+        for(int right = 0; right < n; right++) {
+            if(mp[nums[right]] == 0) {
                 sum += nums[right];
             }
             mp[nums[right]]++;
-
-            while (sum >= k) {
-                len = min(len, right - left + 1);
+            
+            while(sum >= k) {
+                length = min(length, right - left + 1);
                 mp[nums[left]]--;
-                if (mp[nums[left]] == 0) {
+                if(mp[nums[left]] == 0) {
                     sum -= nums[left];
                 }
                 left++;
             }
-            right++;
         }
-        return len == INT_MAX ? -1 : len;
+        
+        return (length == INT_MAX) ? -1 : length;
     }
 };
