c44b8b4865efa01495cbdcc55e60c9ea424adc44
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-01-04 12:01:14 +0530

    Time: 0 ms (100%), Space: 15.6 MB (48.89%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n = strs.size() ;
-        int count = 0 ; 
-        int m = strs[0].size(); 
-        for( int i  = 0  ; i< m ; i++){
-            string s= "" ; 
-            bool fine = true; 
-            for( int j = 0  ; j< n ; j++){
-                char c = strs[j][i]; 
-                if(!s.empty()){
-                    if(s.back() > c ) {
-                        fine = false; 
-                        break; 
-                    }
-                    else{
-                        s.push_back(c); 
-                    }
-                }
-                else if( s.empty()){
-                    s.push_back(c); 
-                }
+        int n = strs.size() ; 
+        int col = strs[0].size(); 
+        int count= 0; 
 
+        for(int i= 0 ; i<col ; i++){
+            for(int j=1 ; j<n ; j++){
+                if( j-1>=0 and strs[j][i]<strs[j-1][i] ){
+                    count++; 
+                    break; 
+                }
             }
-                if( !fine ) count++; 
-
         }
-        return count; 
+        return count ;
     }
 };
