e027ab95b63812ae96cd959a518b466019107f6d
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-01-05 00:28:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,25 +1,23 @@
 class Solution {
   public:
-    
-    int solve(vector<int>& arr, vector<vector<int>>& dp, int target, int i) {
-        
-        if (i < 0) return (target == 0);
-
-        if( dp[i][target] != -1 ) return dp[i][target]; 
-        
-        int notTake = solve(arr, dp, target, i - 1);
-        int take = 0;
-        
-        if (arr[i] <= target) {
-            take = solve(arr, dp, target - arr[i], i - 1);
-        }
-        
-        return dp[i][target] = (take + notTake);
-    }
-        
     int perfectSum(vector<int>& arr, int target) {
-        int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        return solve(arr, dp, target, n - 1);
+        int n = arr.size(); 
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+
+        dp[0][0] = 1;
+        if (arr[0] <= target)
+            dp[0][arr[0]] += 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int tar = 0; tar <= target; tar++) {
+                dp[i][tar] = dp[i-1][tar];  // not take
+
+                if (arr[i] <= tar) {
+                    dp[i][tar] += dp[i-1][tar - arr[i]]; // take
+                }
+            }
+        }
+
+        return dp[n-1][target];
     }
 };
