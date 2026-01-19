d3780d10c3f77c624a80e574aaa5b0b87b9949d8
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-01-19 23:29:20 +0530

    Time: 0 ms (100%), Space: 9.1 MB (100%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int vowelConsonantScore(string s) {
-        int vowel = 0, consonent = 0;
+        
+        int vowel = 0, consonant = 0;
 
-        unordered_set<char> st = {'1', '2', '3', '4', '5',
-                                  '6', '7', '8', '9', '0'};
-
-        for (auto& it : s) {
-            if (it == 'a' or it == 'e' or it == 'i' or it == 'o' or it == 'u') {
-                vowel++;
-            } else {
-                if (it == ' ' or st.count(it)) {
-
+        for (char c : s) {
+            if (c >= 'a' && c <= 'z') {
+                if (c == 'a' || c == 'e' || c == 'i' ||
+                    c == 'o' || c == 'u') {
+                    vowel++;
                 } else {
-                    consonent++;
+                    consonant++;
                 }
             }
         }
-        return (consonent > 0) ? vowel / consonent : 0;
+
+        return consonant > 0 ? vowel / consonant : 0;
     }
 };
