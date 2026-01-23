4c5253716a37dc024f7154a6ceb7c96bcc52b483
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-01-23 23:30:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,29 +1,19 @@
 class Solution {
   public:
-    
-    int solve(int W, vector<int> &val, vector<int> &wt, vector<vector<int>>& dp , int index   ){
-        if(index == 0 ){
-            if(wt[0]<= W){
-                return val[index];      
-            }
-            else{
-                return 0; 
-            }
-        }
-            
-        if( dp[index][W] != -1 ) return dp[index][W]; 
-        
-        int notTake =  solve( W , val , wt , dp , index - 1 );
-        int take = ( wt[index]<= W ) ? val[index]+solve( W-wt[index] , val , wt , dp , index-1) : INT_MIN;
-
-        return dp[index][W] = max( take , notTake ); 
-        
-    }
-  
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        int index = val.size(); 
-        vector<vector<int>> dp( index , vector<int>(W+1 , -1)); 
-        return solve( W , val , wt , dp , index-1 ); 
+        int m =  wt.size(); 
+        vector<int> prev(W+1,0), curr(W+1); 
         
+        for( int i = wt[0] ; i<=W ; i++  ){
+            prev[i] = val[0];
+        }
+        
+        for( int i  =  1 ; i<m ; i++ ){
+            for( int j = 1 ; (j<=W ) ; j++ ){
+        curr[j] = (j >= wt[i]) ? max(val[i] + prev[j - wt[i]], prev[j]) : prev[j];
+            }
+            prev = curr ; 
+        }
+        return prev[W];
     }
 };
