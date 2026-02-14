299bed9399bf58ba7b325345a3b90efc31360fd2
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-02-14 22:49:13 +0530

    Time: 91 ms (30.68%), Space: 106.6 MB (95.58%) - LeetHub

--- 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.cpp
+++ 3839-number-of-prefix-connected-groups/3839-number-of-prefix-connected-groups.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int prefixConnected(vector<string>& words, int k) {
-        sort(words.begin(), words.end());
-        unordered_set<string> st;
+        sort(words.begin() , words.end()) ;
+        unordered_set<string> st;  
+        int n = words.size() ; 
+        for( int i = 0 ; i< n-1 ;i++ ){
+            if( words[i].size()< k or words[i+1].size() < k ) continue;
 
-        for (int i = 0; i < words.size() - 1; i++) {
+            string s1 = words[i].substr(0, k ); 
+            if(st.count(s1)) continue ; 
+            string s2 = words[i+1].substr(0,k ); 
 
-            if (words[i].size() < k || words[i+1].size() < k)
-                continue;
-
-            string prefix1 = words[i].substr(0, k);
-            string prefix2 = words[i+1].substr(0, k);
-
-            if (prefix1 == prefix2) {
-                st.insert(prefix1);
-            }
+            if( s1 == s2 ) st.insert( s1 ); 
         }
-
         return st.size();
     }
 };
