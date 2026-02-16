2ed9ffb59f5cf8a9911d272db9952178f348f121
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-02-16 17:42:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (74.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,51 +1,61 @@
 class Disjointset{
-    public :
-    vector<int> parent , size ; 
-    Disjointset( int n ){
-        parent.resize(n+1); 
-        size.resize(n+1, 1); 
-        for (int i = 0; i <=n; i++) {
-        parent[i] = i;
-    }
-    }
+public:
+    vector<int> parent , size;
 
-    int findParent( int node ){
-        if( node == parent[node]) return node; 
-        return parent[node] = findParent(parent[node]); 
+    Disjointset(int n){
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i = 1; i <= n; i++){
+            parent[i] = i;
+        }
     }
 
-    void unionBySize( int u , int v ){
-        int rootA = findParent(u); 
-        int rootB = findParent(v); 
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
 
-        if( rootA == rootB ) return ; 
-        
-        else if( size[rootA] < size[rootB]){
-            parent[rootA] = rootB ; 
-            size[rootB]+=size[rootA]; 
+    void unionBySize(int u , int v){
+        int rootA = findParent(u);
+        int rootB = findParent(v);
+
+        if(rootA == rootB) return;
+
+        if(size[rootA] < size[rootB]){
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
         }
         else{
-            parent[rootB] = rootA ; 
-            size[rootA]+=size[rootB]; 
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
         }
     }
-}; 
+};
 
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size();  Disjointset d1(V); 
-        for(int i  = 0  ; i< V ; i++ ){
-            for( int j = 0 ; j<V; j++ ){
-                if( isConnected[i][j] == 1 ){
-                    d1.unionBySize(i , j ); 
+
+        int V = isConnected.size();
+        Disjointset d1(V);
+
+        // shift +1 while calling union
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                if(isConnected[i][j] == 1){
+                    d1.unionBySize(i+1 , j+1);
                 }
             }
         }
-        int count = 0 ;
-        for(int i = 0 ; i<=V;  i++ ){
-            if( i == d1.parent[i]) count++; 
+
+        int count = 0;
+
+        // only check 1 to V
+        for(int i = 1; i <= V; i++){
+            if(d1.parent[i] == i)
+                count++;
         }
-        return count-1;
+
+        return count;
     }
 };
