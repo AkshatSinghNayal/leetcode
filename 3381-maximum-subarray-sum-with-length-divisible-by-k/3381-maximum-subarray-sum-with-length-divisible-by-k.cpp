4ed35beba51ebc5fbf32bb608452affdcf5a401c
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-02-27 05:54:04 +0530

    Time: 97 ms (16.67%), Space: 184.6 MB (17.62%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-        int  n =  nums.size() ; 
-        vector<long long> prefixSum(n); 
-        prefixSum[0] = nums[0] ; 
-        for( int i = 1  ; i< n ;i++){
-            prefixSum[i]  = prefixSum[i-1]+nums[i] ; 
-        }
+        int n = nums.size();
+        long long prefix = 0;
+        long long ans = LLONG_MIN;
 
-        long long maxi =  LLONG_MIN ; 
-        for( int i = 0 ; i < k  ; i++ ){
-            long long sum = 0  ; 
-            int index = i ; 
-            while( index < n && index+k-1 < n ){
-                int j =  index+k-1 ;
-                long long subarrSum =  prefixSum[j] - ((index>0) ? prefixSum[index-1] : 0) ; 
-                 sum =  max(subarrSum, sum + subarrSum);
-                maxi = max( sum , maxi ) ; 
-                index+=k ;
+        unordered_map<int, long long> mp;
+        mp[0] = 0;   // base case: prefix before array starts
 
+        for(int i = 0; i < n; i++) {
+            prefix += nums[i];
+
+            int rem = (i + 1) % k;  // length remainder
+
+            if(mp.count(rem)) {
+                ans = max(ans, prefix - mp[rem]);
+                mp[rem] = min(mp[rem], prefix);
+            } else {
+                mp[rem] = prefix;
             }
-
         }
-        
-        return maxi ; 
 
+        return ans;
     }
 };
