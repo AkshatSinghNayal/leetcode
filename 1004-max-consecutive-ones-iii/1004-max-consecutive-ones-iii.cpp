5d8c28aa7f237b450f0c86d294e22cf961b15133
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-04 21:59:07 +0530

    Time: 4 ms (21.77%), Space: 69.4 MB (99.79%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,29 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int right = 0  , left=0; 
-        int n  = nums.size(); 
-        int temp = k ; 
-        int maxi = 0 ; 
-        while( right  < n and left < n ){
-            if( nums[right] ==1 ) right++; 
-            else if( temp> 0 and nums[right] == 0  ){
-                temp-- ; 
-                right++ ; 
+        int left = 0, right = 0;
+        int n = nums.size();
+        int temp = k;
+        int maxi = 0;
+
+        while (right < n) {
+            if (nums[right] == 0) {
+                temp--;
             }
-            else{
-                while( left < n and temp ==  0 ){
-                    if( nums[ left] == 0  ){
-                        temp++; 
-                    }
-                    left++; 
+
+            // If temp becomes negative (i.e., we used too many flips), move the left pointer
+            while (temp < 0) {
+                if (nums[left] == 0) {
+                    temp++;
                 }
+                left++;
             }
 
-            maxi = max( maxi , right-left); 
-
-            
+            maxi = max(maxi, right - left + 1);
+            right++;
         }
-        return maxi; 
+
+        return maxi;
     }
 };
+
