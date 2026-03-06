0c0a97036cd6a11c31a3685fbf48aabae7d95d69
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-06 07:44:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,25 +1,25 @@
 class Solution {
-public:
+  public:
     int countPartitions(vector<int>& arr, int diff) {
-        const int MOD = 1e9 + 7;
 
-        long long total = accumulate(arr.begin(), arr.end(), 0LL);
+        long long target = accumulate(arr.begin(), arr.end(), 0);
 
-        // Feasibility checks
-        if (total < diff || (total - diff) % 2 != 0)
+        if (target < diff || (target + diff) % 2 != 0)
             return 0;
 
-        int target = (total - diff) / 2;
+        int s1 = (target + diff) / 2;
 
-        vector<long long> dp(target + 1, 0);
+        vector<int> dp(target + 1, 0);
         dp[0] = 1;
 
-        for (int num : arr) {
-            for (int tar = target; tar >= num; tar--) {
-                dp[tar] = (dp[tar] + dp[tar - num]) % MOD;
+        if (arr[0] <= target) dp[arr[0]] += 1;
+
+        for (int i = 1; i < arr.size(); i++) {
+            for (int tar = target; tar >= 1 && arr[i] <= tar; tar--) {
+                dp[tar] += dp[tar - arr[i]];
             }
         }
 
-        return dp[target];
+        return dp[s1];
     }
 };
