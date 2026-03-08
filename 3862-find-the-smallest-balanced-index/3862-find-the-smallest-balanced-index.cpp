b56e1a26c2968191c07b28d239da9703b86ffff8
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-08 14:59:26 +0530

    Time: 6 ms (87.28%), Space: 209.4 MB (73.68%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int smallestBalancedIndex(vector<int>& nums) {
-        long long product = 1;
+        long long  product = 1;
         int index = -1;
         int n = nums.size();
         vector<long long> prefix(n+1);
-        prefix[0] = 0;
+        prefix[0] = 0; 
+        for( int i  =1 ; i<=n; i++ ) prefix[i] = nums[i-1]+prefix[i-1]; 
 
-        for(int i = 1; i <= n; i++)
-            prefix[i] = nums[i-1] + prefix[i-1];
-
-        long long LIMIT = 1e14;
-
-        for(int i = n-1; i >= 0; i--){
-            if(product == prefix[i])
+        // for(auto& it : prefix ) cout << it << " "; 
+        long long LIMIT = LLONG_MAX;
+        for(int i  = n-1 ; i>=0 ; i-- ){
+            if( product == prefix[i]){
                 index = i;
-
-            if(product > prefix[i])   // safe early stop
-                break;
-
+            }
             if(product > LIMIT / nums[i])
-                product = LIMIT + 1;
+                product = LLONG_MAX-1;
             else
                 product *= nums[i];
         }
