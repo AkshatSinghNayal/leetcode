052a83302c0604f6241b05ce69c477462e2ed492
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-12 22:57:45 +0530

    Time: 0 ms (100%), Space: 17.4 MB (82.98%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans; 
-        sort(nums.begin() , nums.end()) ; 
-        int n = nums.size() ; 
 
-        for(int i  = 0  ; i< n ; i++ ){
-            if( i>0 and nums[i] == nums[i-1]) continue;
-            for(int j =  i+1 ; j<n ;j++){
-                if( j-i+1 > 2 and nums[j] == nums[j-1]) continue;
-                int first = nums[i];
-                int second = nums[j];
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if(n < 4) return ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n-3; i++) {
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            if((long long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
+            if((long long)nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target) continue;
+
+            for(int j = i+1; j < n-2; j++) {
+
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                if((long long)nums[i] + nums[j] + nums[j+1] + nums[j+2] > target) break;
+                if((long long)nums[i] + nums[j] + nums[n-1] + nums[n-2] < target) continue;
+
                 int left = j+1;
                 int right = n-1;
 
-                while( left < right ){
-                    long long sum = (long long)first + second + nums[left] + nums[right];
-                    
+                while(left < right) {
 
-                    if( sum<  target ){
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if(sum < target) left++;
+                    else if(sum > target) right--;
+                    else {
+
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+
                         left++;
-                    }
-                    else if( sum > target ){
-                        right--; 
-                    }
-                    else{
-                        ans.push_back({ first , second , nums[left] , nums[right]}); 
-                        left++; 
-                        right--; 
+                        right--;
 
-                        while( left < right and nums[left] == nums[left-1] ) left++;
-                        while( right > left and nums[right] == nums[right+1]) right--;
+                        while(left < right && nums[left] == nums[left-1]) left++;
+                        while(left < right && nums[right] == nums[right+1]) right--;
                     }
                 }
-                
             }
         }
-        return ans; 
+
+        return ans;
     }
 };
