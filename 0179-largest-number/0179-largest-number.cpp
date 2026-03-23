c240bbd918a66ecc6ef563e5335c9539f2580f8a
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-23 22:44:48 +0530

    Time: 6 ms (29.09%), Space: 17.3 MB (10.26%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,17 +3,15 @@ public:
     string largestNumber(vector<int>& nums) {
         vector<string> str;
         for(auto& it : nums ){
-            str.push_back(to_string(it));
+            str.push_back(to_string(it)); 
         }
-
-        sort( str.begin() , str.end() , [&] (auto a , auto b ){
-            return a+b > b+a ;
+        string result ="";
+        sort(str.begin() , str.end() ,[&] (auto a , auto b ){
+            return a+b > b+a;
         }); 
-        string result= "";
         for(auto& it : str ){
             result+=it;
         }
-        if(result[0]  == '0'  ) return "0";
-        return result;
+        return (result[0] ==  '0') ? "0" : result;
     }
 };
