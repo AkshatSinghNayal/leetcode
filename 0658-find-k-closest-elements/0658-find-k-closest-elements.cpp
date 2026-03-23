4ec8bcf51e7e9906b0feb493cc9a6e28deb74b3f
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-24 02:02:27 +0530

    Time: 23 ms (29.57%), Space: 38.7 MB (21.09%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        auto right  = lower_bound(arr.begin() , arr.end() , x ) - arr.begin(); 
-        int left = right-1 , n = arr.size();
-        while(k--){
-            if( right < n and left >=0 and  abs( x - arr[right]) < abs(x - arr[left]) ){
-                right++; 
-            }
-            else if( left < 0 ){
-                right++;
-            }
-            else if(right>=n){
-                left--; 
-            }
-            else{
-                left--; 
-            }
+
+    class comp{
+        public : 
+        bool operator()(const pair<int,int>& a , const pair<int,int>& b ){
+            return a.first < b.first ;
         }
-        return vector<int>( arr.begin() + left + 1 , arr.begin() + right );
+    }; 
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int> , vector<pair<int,int>> , comp>pq; // freq , number
+        int i =0;
+        int n  = arr.size();
+        while(i < k ){
+            pq.push({ abs(x-arr[i]), arr[i]});
+            i++;
+        }
+        while( i< n ){
+            if( pq.top().first > abs(x - arr[i]) ){
+                pq.pop();
+                pq.push( {abs(x-arr[i]) , arr[i]});
+            }
+            i++;
+        }
+        vector<int>ans;
+        while(!pq.empty()){
+            ans.push_back( pq.top().second );
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
