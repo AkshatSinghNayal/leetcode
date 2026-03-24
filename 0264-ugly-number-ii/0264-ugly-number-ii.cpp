d8848f48c5b0b239aec8a12ed61a01164071f994
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-25 02:45:08 +0530

    Time: 3 ms (78.23%), Space: 11.3 MB (67.87%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<long, vector<long>, greater<long>> minHeap;
-        unordered_set<long> seen;
+        if (n <= 6) return n;
 
-        minHeap.push(1);
-        seen.insert(1);
+        vector<int> dp(n);  // store first n ugly numbers
+        dp[0] = 1;          // first ugly number
 
-        long number = 1;
+        int i2 = 0, i3 = 0, i5 = 0; // pointers for multiples of 2,3,5
 
-        for (int i = 0; i < n; i++) {
-            number = minHeap.top();
-            minHeap.pop();
+        for (int i = 1; i < n; i++) {
+            int next = min({dp[i2]*2, dp[i3]*3, dp[i5]*5});
+            dp[i] = next;
 
-            for (int p : {2, 3, 5}) {
-                long next = number * p;
-                if (!seen.count(next)) {
-                    minHeap.push(next);
-                    seen.insert(next);
-                }
-            }
+            if (next == dp[i2]*2) i2++;
+            if (next == dp[i3]*3) i3++;
+            if (next == dp[i5]*5) i5++;
         }
 
-        return (int)number;
+        return dp[n-1];
     }
 };
