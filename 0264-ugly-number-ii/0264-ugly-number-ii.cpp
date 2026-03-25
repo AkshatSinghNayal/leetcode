48ec6e594de8511b65a312d5ef10303d0acd2fbe
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-25 11:29:46 +0530

    Time: 3 ms (78.23%), Space: 11.4 MB (67.87%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if (n <= 6) return n;
-
-        vector<int> dp(n);  // store first n ugly numbers
-        dp[0] = 1;          // first ugly number
-
-        int i2 = 0, i3 = 0, i5 = 0; // pointers for multiples of 2,3,5
-
+        vector<int> dp(n);
+        int i1 = 0, i2 = 0, i3 = 0;
+        dp[0] = 1;
         for (int i = 1; i < n; i++) {
-            int next = min({dp[i2]*2, dp[i3]*3, dp[i5]*5});
-            dp[i] = next;
-
-            if (next == dp[i2]*2) i2++;
-            if (next == dp[i3]*3) i3++;
-            if (next == dp[i5]*5) i5++;
+            int temp  = min({ dp[i1]*2 , dp[i2]*3  , dp[i3]*5}); 
+            dp[i] = temp; 
+            if(  temp == dp[i1]*2 ) i1++;
+            if( temp == dp[i2]*3 ) i2++;
+            if(temp == dp[i3]*5 ) i3++;
         }
-
-        return dp[n-1];
+        return dp[n - 1];
     }
 };
