c5ae4719cc1b0f81515d76eeb92aa87e54554b8c
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-25 23:02:07 +0530

    Time: 7 ms (73.38%), Space: 130 MB (60.43%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        long long total = 0;
-        for(auto& it : grid ){
-            total+=accumulate(it.begin(),it.end(),0LL);
+        int n = grid.size()  , m = grid[0].size();
+        long long total = 0; 
+        vector<long long> row(n) ;  vector<long long> col(m); 
+        
+        
+        for(int i = 0 ; i< n ;i++ ){
+            row[i] = accumulate(grid[i].begin(), grid[i].end() , 0LL);
+            total+=row[i]; 
         }
-        if(total%2 != 0 ) return false;
-        long long temp = total/2;
-        for(auto& it : grid ){
-            temp -= accumulate(it.begin(),it.end(),0LL); 
-            if( temp == 0  ) return true;
-            if(temp<0 ) break;
-        }
-        temp = total/2;
-        int m = grid[0].size() , n = grid.size();
-        for( int i = 0 ;i<m ;i++ ){
+        for( int i = 0 ; i< m ; i++ ){
+            long long temp = 0; 
             for(int j = 0 ; j< n ;j++ ){
-                temp-=grid[j][i]; 
+                temp+= grid[j][i]; 
             }
-            if( temp == 0  ) return true;
-            if( temp<0 ) return false;
+            col[i] = temp;
+            
+        }
+
+        if(total%2 != 0 ) return false;
+        long long temp1 = total / 2;
+
+        for( int i = 0 ; i< n; i++ ){
+            temp1-=row[i]; 
+            if(temp1 == 0  ) return true;
+            if( temp1< 0 ) break;
+        }
+
+        temp1 = total/2; 
+
+        for(int i  = 0 ;i< m ;i++ ){
+            temp1-=col[i]; 
+            if( temp1 == 0 ) return true; 
+            if( temp1< 0 ) break;
         }
         return false;
     }
