73bc3ae0b072c5955583e680ec56a2b93b511607
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-27 00:08:35 +0530

    Time: 816 ms (30.85%), Space: 332.3 MB (14.31%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
+    int minRemovals(vector<int>& nums, int target) {
+        int n = nums.size();
+        int MAX_VAL = 1 << 14;
+        const int INF = 1e9;
 
-    int solve( vector<int>& nums , int target , int i , vector<vector<int>>& dp ){
-        // 
-        if( i<0 ){
-            if( target == 0  ) return 0; 
-            else return 1e9;
+        vector<vector<int>> dp(n, vector<int>(MAX_VAL, INF));
+
+        // Base case
+        dp[0][nums[0]] = min(dp[0][nums[0]], 0);
+        dp[0][0] = min(dp[0][0], 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int x = 0; x < MAX_VAL; x++) {
+                // Remove
+                dp[i][x] = min(dp[i][x], 1 + dp[i-1][x]);
+
+                // Keep
+                int prev = x ^ nums[i];
+                dp[i][x] = min(dp[i][x], dp[i-1][prev]);
+            }
         }
 
-        if(dp[i][target] != -1 ) return dp[i][target] ; 
-
-        return dp[i][target] = min(solve(nums ,target^nums[i] , i-1 , dp), 1+solve(nums, target , i-1 , dp )); 
-    }
-
-    int minRemovals(vector<int>& nums, int target) {
-        int MAX_VAL = 1<<14 , n = nums.size();
-        vector<vector<int>>dp(n , vector<int>(MAX_VAL,-1)); 
-        int ans = solve( nums , target , n-1 , dp );
-        return (ans>= 1e9 ) ? -1 : ans ; 
+        int ans = dp[n-1][target];
+        return (ans >= INF) ? -1 : ans;
     }
 };
