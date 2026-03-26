09e3a7f077637872e7ff3beb38091cf0a55d65b1
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-27 00:01:48 +0530

    Time: 163 ms (85.38%), Space: 332.4 MB (12.15%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
 
-    int solve(vector<int>& nums , vector<vector<int>>& dp , int target , int i ){
-        if( i < 0 ){
-            if( target == 0 ){
-                return 0;
-            }
-            return 1e9;
+    int solve( vector<int>& nums , int target , int i , vector<vector<int>>& dp ){
+        // 
+        if( i<0 ){
+            if( target == 0  ) return 0; 
+            else return 1e9;
         }
 
-        if(dp[i][target] != -1 ) return dp[i][target]; 
+        if(dp[i][target] != -1 ) return dp[i][target] ; 
 
-        int take = solve( nums , dp , target^nums[i] , i-1 ); 
-        int notTake = 1+ solve( nums ,dp, target , i-1); 
-
-        return dp[i][target] = min(take , notTake);
-
+        return dp[i][target] = min(solve(nums ,target^nums[i] , i-1 , dp), 1+solve(nums, target , i-1 , dp )); 
     }
 
     int minRemovals(vector<int>& nums, int target) {
-        int n  = nums.size(); 
-        int MAX_XOR = 1 << 14; // 16384 ->> 2^x 
-        vector<vector<int>> dp(n, vector<int>(MAX_XOR, -1));
-        int ans = solve( nums , dp , target , n-1);
-
-        return ( ans >= 1e9 ) ? -1 : ans; 
+        int MAX_VAL = 1<<14 , n = nums.size();
+        vector<vector<int>>dp(n , vector<int>(MAX_VAL,-1)); 
+        int ans = solve( nums , target , n-1 , dp );
+        return (ans>= 1e9 ) ? -1 : ans ; 
     }
 };
