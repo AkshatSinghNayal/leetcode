e8f475730c5da43bb80c35aa7ba477b375c94633
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-26 18:53:45 +0530

    Time: 0 ms (100%), Space: 32.1 MB (40.2%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,30 +1,22 @@
-////// I bow to Lord Satyanarayan and Lord Hanuman ///////////
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        
-        vector<long long> prefix(n + 1, 0);
-        
-        for(int i = 1; i <= n; i++){
-            prefix[i] = prefix[i - 1] + nums[i - 1];
-        }
-        
-        int ans = INT_MAX;
-        
-        for(int i = 0; i < n; i++){
-            long long needed = target + prefix[i];
-            
-            int j = lower_bound(prefix.begin() + i + 1, prefix.end(), needed) - prefix.begin();
-            
-            if(j <= n){
-                ans = min(ans, j - i);
+        int left = 0 , right = 0 , maxi = n+1; 
+        long long total=0; 
+        while( right < n ){
+            int ele = nums[right]; 
+
+            total+=ele;
+
+            while( left < n and total>=target){
+                maxi = min(maxi , right-left+1);
+                total-=nums[left];
+                left++; 
             }
+
+            right++; 
         }
-        
-        return ans == INT_MAX ? 0 : ans;
+        return ( maxi == n+1 ) ? 0 : maxi; 
     }
 };
