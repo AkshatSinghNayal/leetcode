7d8049cfa3b9095a6bee5871d2b9949c28fc99d7
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-28 02:12:39 +0530

    Time: 94 ms (11.23%), Space: 87.7 MB (70.23%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
+        unordered_map<int,int> mp;
+        mp[0] = -1 ;
+        long long target  = 0; 
         int n = nums.size();
-
-        unordered_map<int, int> mp;
-        mp[0] = -1;
-        long long sum = 0;
-        for (auto& it : nums) {
-            sum = (sum + it) % p;
+        for(auto& it : nums ){
+            target =(target+ it)%p;
         }
-        if (sum == 0)
-            return 0;
-        int target = sum;
-        sum = 0;
-        int mini = n;
+        if( target == 0 ) return 0 ;
 
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            int MOD = (sum % p);
-            int need = (MOD - target + p) % p;
-            if (mp.count(need)) {
-                mini = min(mini, i - mp[need]);
+        int prefix =0 , mini =INT_MAX; 
+
+        for(int i = 0 ;i< n ;i++ ){
+            prefix = (prefix+nums[i])%p;
+            const int MOD = (prefix - target + p )%p;
+            if(mp.count(MOD)){
+                mini = min(mini , i-mp[MOD]);
             }
+            mp[prefix]=i; 
 
-            mp[MOD] = i;
         }
 
-        return (mini == n) ? -1 : mini;
+        return (mini == INT_MAX or mini ==  n ) ? -1 : mini ; 
     }
 };
+
+
