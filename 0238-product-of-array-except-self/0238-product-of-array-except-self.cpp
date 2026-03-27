bf20fce016ba36e9b287a675ec37d48448c032d5
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-27 10:58:02 +0530

    Time: 5 ms (8.66%), Space: 41.2 MB (40.29%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        
-        // Special case for size 1
-        if (n == 1) {
-            return {0};  // No other elements to multiply with
+        int n  = nums.size();
+        vector<int>ans(n);  
+        vector<int>suffix(n);
+        suffix[n-1] = 1 ; 
+        for(int i = n-2 ; i>=0 ;i-- ){
+            suffix[i]=suffix[i+1]*nums[i+1]; 
         }
-        
-        // Result array to store the answer
-        vector<int> result(n, 1);
-        
-        // Calculate prefix product
-        int prefix = 1;
-        for (int i = 0; i < n; ++i) {
-            result[i] = prefix;
-            prefix *= nums[i];
+        int prefix = 1; 
+        for(int i = 0 ;i< n ;i++ ){
+            ans[i] = prefix*suffix[i];
+            prefix*=nums[i]; 
         }
-        
-        // Calculate suffix product in reverse and multiply with result
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; --i) {
-            result[i] *= suffix;
-            suffix *= nums[i];
-        }
-        
-        return result;
+
+        return ans; 
     }
 };
