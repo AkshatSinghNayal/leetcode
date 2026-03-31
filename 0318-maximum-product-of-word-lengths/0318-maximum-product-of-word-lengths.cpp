a96759848819c3bd26907ba04ef649b8af3e7728
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-31 23:33:24 +0530

    Time: 12 ms (42.75%), Space: 26.1 MB (70.6%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -1,29 +1,24 @@
-////// I bow to Lord Satyanarayan and Lord Hanuman ///////////
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        int n =  words.size();
-        vector<int>bits(n);
-        for(int i = 0 ;i< n; i++ ){
-            int num = 0; 
-            for(int j = 0  ; j<words[i].size() ; j++ ){
-                num |= 1<<(words[i][j]-'a'); 
-            }
-            bits[i] = num; 
-        }
+        int n  = words.size();
         int maxi = 0; 
-        for(int i = 0 ;i< n-1 ;i++ ){
-            for( int j =i+1; j<n ;j++ ){
-                if( (bits[i] & bits[j]) == 0  ){
-                    maxi = max( maxi , (int)words[i].size()*(int)words[j].size());
+        vector<int>bits(n);
+        for(int i  = 0 ; i< n ; i++ ){
+            int temp = 0; 
+            for(int j  = 0 ; j<words[i].size(); j++ ){
+                temp |= 1<<(words[i][j]-'a'); 
+            }
+            bits[i] = temp; 
+        }
+
+        for(int i  = 0 ;i< n-1 ; i++ ){
+            for(int j=i+1; j< n ; j++ ){
+                if( !(bits[i]&bits[j])){
+                    maxi = max(maxi , (int)(words[i].size()*words[j].size()) ); 
                 }
             }
         }
-        return maxi ; 
+        return maxi; 
     }
 };
-
-
-// video link ->>> https://www.youtube.com/watch?v=3XHAikDnB2w
