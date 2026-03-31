25ccda386bb5a9c1a7695580f811a4d150723415
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-03-31 18:17:51 +0530

    Time: 11 ms (97.29%), Space: 139.1 MB (66.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>ans; 
-        deque<int> dq ;
-        int n  = nums.size() ; 
-        if( k  == 1 ) return nums; 
-        if( k > n ) return ans; 
-        
-        for(int i = 0 ;i < n ;i++ ){
-
-            while(!dq.empty() and dq.front() <= i-k ){
-                dq.pop_front(); 
-            }
-
-            while(!dq.empty() and nums[dq.back()]< nums[i]){
+        vector<int>ans;
+        deque<int>dq;
+        int n = nums.size() , right = 0 , left = 0 ; 
+        if( k> n ) return ans;
+        while(right <n ){
+            
+            while(!dq.empty() and nums[dq.back()] < nums[right] ){
                 dq.pop_back(); 
             }
-
-
-            dq.push_back(i);
-            if( i>= k -1 ){
-                ans.push_back(nums[dq.front()]); 
+            dq.push_back(right);
+            
+            if( right -left +1 == k ){
+                ans.push_back(nums[dq.front()]);
+                if( left == dq.front() ) dq.pop_front();
+                left++;
             }
+
+            right++; 
         }
+
         return ans ; 
     }
 };
