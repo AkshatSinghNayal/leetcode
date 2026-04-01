ef2ff7e0aef161d0a0e675b6b7b7e72be0bfb779
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-01 18:50:58 +0530

    Time: 0 ms (100%), Space: 11.8 MB (70.36%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.cpp
+++ 0677-map-sum-pairs/0677-map-sum-pairs.cpp
@@ -1,24 +1,23 @@
 class MapSum {
 public:
-    unordered_map<string,int> mp; 
-
+    unordered_map<string,int>mp;
+    unordered_map<string,int>prefixArry;
     MapSum() {
         
     }
     
     void insert(string key, int val) {
+        auto leftValueAddOrSub = val - mp[key];
         mp[key] = val;
+        string temp = "";
+        for(auto& it : key ){
+            temp+=it;
+            prefixArry[temp]+=leftValueAddOrSub;
+        }
     }
     
     int sum(string prefix) {
-        int cost = 0; 
-        for(auto& it : mp ){
-            auto temp = it.first.substr(0, prefix.size());
-            if( temp == prefix ){
-                cost+=it.second;
-            }
-        }
-        return cost;
+        return prefixArry[prefix];
     }
 };
 
