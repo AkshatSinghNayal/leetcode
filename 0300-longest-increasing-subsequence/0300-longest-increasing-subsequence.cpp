89323cc9dbe9d0baa8506aaf95b52dd9206b706d
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-05 20:16:42 +0530

    Time: 295 ms (25.51%), Space: 296.3 MB (22.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-
-
     int lengthOfLIS(vector<int>& nums) {
-        int n  = nums.size(); 
-        vector<vector<int>>dp(n+1 , vector(n+1 , 0 )); 
-        // dp[n][*] for dp[n][-1 to n+1 ] all things 0, because  if( currIdx >= nums.size() ) return 0; 
+        int n  = nums.size() ;
+        vector<vector<int>>dp(n+1,vector<int>(n+1 ,0 )); 
 
-         for(int currIdx = n-1; currIdx >= 0; currIdx--){
-            for(int prevIdx = currIdx-1; prevIdx >= -1; prevIdx--){
+        // for(int i  = 0 ; i<=n; i++ ){
+        //     dp[n][i] = 0; 
+        // } base case see memoization for clear vision  
+
+        for(int currIdx = n-1 ;currIdx>=0 ;currIdx-- ){
+            for(int prevIdx = currIdx-1 ; prevIdx>=-1 ; prevIdx-- ){
                 int take = 0; 
-
-                if(prevIdx == -1 || nums[currIdx] > nums[prevIdx]){
-                    take = 1 + dp[currIdx+1][currIdx+1];
+                if( prevIdx == -1 || nums[currIdx] > nums[prevIdx]  ){
+                    take = 1 + dp[currIdx+1][currIdx+1]; 
                 }
-
-                dp[currIdx][prevIdx+1] = max(take, dp[currIdx+1][prevIdx+1]); 
+                dp[currIdx][prevIdx+1] = max( take , dp[currIdx+1][prevIdx+1]); 
             }
         }
-
-
         return dp[0][0]; 
     }
 };
