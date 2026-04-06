9a16087ddb2657155b0b05d0d1761702a2c32bf6
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-06 14:49:25 +0530

    Time: 308 ms (11.66%), Space: 73.6 MB (6.09%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq ; // ele indexOfVector indexOfEle 
-        int k = nums.size(); 
 
-        int start = 0 , maxi  = INT_MIN , end = INT_MAX ; 
-        for(int i =0 ;i< k ;i++ ){
+    class comp{
+        public:
+        bool operator()( const vector<int>& a , const vector<int>& b ){
+            return a[0]>b[0];
+        }
+    };
+
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        priority_queue<vector<int>,vector<vector<int>>, comp>pq; // element ,indexOfVec ,indexOfEle
+        int start = 0 , end = INT_MAX , maxi = INT_MIN;
+        int  k =nums.size(); 
+        for(int i = 0 ;i<k ;i++){
             pq.push({nums[i][0] , i , 0 }); 
             maxi = max(maxi, nums[i][0]);
+        }
 
-        } 
+        while(!pq.empty()){
+            auto top  = pq.top(); pq.pop(); 
+            int ele = top[0] ;
+            int indexOfVec = top[1];
+            int indexOfEle= top[2];
 
-
-        while( true ){
-            auto top =pq.top();pq.pop();
-            int ele = top[0];
-            int indexOfVector = top[1]; 
-            int indexOfEle = top[2]; 
-
-            if( maxi - ele < end -start ){
-                start = ele ;
-                end   = maxi;
+            if( end - start > maxi - ele ){
+                start = ele;
+                end = maxi;
             }
 
-            if(indexOfEle+1 < nums[indexOfVector].size() ){
-                pq.push({nums[indexOfVector][indexOfEle+1],indexOfVector, indexOfEle+1});
-                maxi = max(maxi, nums[indexOfVector][indexOfEle+1]);
-            }
-            else{
-                break;
-            }
+            if(indexOfEle+1 <nums[indexOfVec].size() ){
+                    pq.push({ nums[indexOfVec][indexOfEle+1], indexOfVec , indexOfEle+1 });
+                    maxi = max(maxi , nums[indexOfVec][indexOfEle+1]);
+                }
+                else{
+                    break;
+                }
+                
+           
 
 
         }
-        return{start ,end};
+        return { start , end };
 
     }
 };
