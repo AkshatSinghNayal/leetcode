29ffea070d4273affab6c1428e5064c896f0e650
Akshat SIngh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-08 15:40:20 +0530

    Time: 139 ms (6.3%), Space: 62.6 MB (5.66%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,69 +1,54 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        multiset<pair<double,int>> leftSide;  // smaller half
-        multiset<pair<double,int>, greater<pair<double,int>>> rightSide; // larger half
-        vector<double> ans;
-        int n = nums.size();
-        int left = 0, right = 0;
-
-        while(right < n) {
-            double ele = (double)nums[right];
-            pair<double,int> newPair = {ele, right};
-
-            // insert into rightSide first
+        multiset<pair<long,int>>rightSide;
+        multiset<pair<long,int>,greater<pair<long,int>>> leftSide;
+        vector<double>ans; 
+        int n = nums.size() , left =0 ; 
+        for(int right = 0 ;right< n ; right++ ){
+            pair<long,int> newPair = {nums[right],right}; 
             rightSide.insert(newPair);
+            auto it  = prev(rightSide.end());
+            leftSide.insert(*it);
+            rightSide.erase(it);
 
-            // move smallest of rightSide to leftSide
-            auto itRight = prev(rightSide.end()); // largest in rightSide (descending order)
-            leftSide.insert(*itRight);
-            rightSide.erase(itRight);
-
-            // rebalance sizes
-            if(leftSide.size() > rightSide.size() + 1) {
-                auto itLeft = prev(leftSide.end());
-                rightSide.insert(*itLeft);
-                leftSide.erase(itLeft);
+            if(leftSide.size() > rightSide.size()+1 ){
+                auto it  = prev(leftSide.end());
+                rightSide.insert(*it);
+                leftSide.erase(it);
             }
 
-            // remove outgoing element when window slides
-            if(right - left + 1 > k) {
-                pair<double,int> out = { (double)nums[left], left };
-                if(leftSide.find(out) != leftSide.end()) {
-                    leftSide.erase(leftSide.find(out));
-                } else {
-                    rightSide.erase(rightSide.find(out));
-                }
-                left++;
+            if( right-left +1 > k ){ // this is the case when we have more element in multiset
+                pair<long , int> toRemove = {nums[right-k],right-k}; 
+                if( leftSide.find(toRemove)!=leftSide.end()) leftSide.erase(toRemove);
+                else if(rightSide.find(toRemove)!=rightSide.end()) rightSide.erase(toRemove);
 
-                // rebalance after removal
-                if(leftSide.size() > rightSide.size() + 1) {
-                    auto itLeft = prev(leftSide.end());
-                    rightSide.insert(*itLeft);
-                    leftSide.erase(itLeft);
-                } else if(leftSide.size() < rightSide.size()) {
-                    auto itRight2 = prev(rightSide.end());
-                    leftSide.insert(*itRight2);
-                    rightSide.erase(itRight2);
+                if(leftSide.size() > rightSide.size()+1 ){
+                    auto it  = prev(leftSide.end());
+                    rightSide.insert(*it);
+                    leftSide.erase(it);
+                }
+                else if (leftSide.size() < rightSide.size()){
+                    auto it = prev(rightSide.end()); 
+                    leftSide.insert(*it);
+                    rightSide.erase(it); 
                 }
             }
 
-            // compute median if window has size k
-            if(right - left + 1 == k) {
-                double median;
-                if(k % 2 != 0) {
-                    median = prev(leftSide.end())->first; // max of leftSide
-                } else {
-                    double leftMax = prev(leftSide.end())->first;
-                    double rightMin = prev(rightSide.end())->first;
-                    median = (leftMax + rightMin) / 2.0;
+            if( right>=k-1 ){
+                double median; 
+                if( k%2 != 0 ){
+                    auto it = prev(leftSide.end()); 
+                    median = it->first;
+                }
+                else{
+                    auto it = prev(leftSide.end());
+                    auto itt = prev(rightSide.end());
+                    median = (it->first+itt->first)/2.0; 
                 }
                 ans.push_back(median);
             }
-
-            right++;
-        }
-
-        return ans;
+        } 
+        return ans; 
     }
 };
