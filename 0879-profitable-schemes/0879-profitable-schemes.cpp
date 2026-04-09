e43d1b61655a8b2c9dd0f6c3c4fa62473f77351d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-09 15:40:30 +0530

    Time: 112 ms (37.86%), Space: 56.3 MB (26.73%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     const int MOD = 1e9+7;
 
-    int solve(vector<vector<vector<int>>>&dp , int i , int n, int minProfit, vector<int>& group, vector<int>& profit ){
-        // base
-        minProfit = max(0 , minProfit );
-        if( i< 0 ){
-            return (minProfit == 0 ) ? 1 : 0 ; 
-        }
-
-        if( dp[i][n][minProfit] != -1 ) return dp[i][n][minProfit]; 
-
-        long long take = 0; 
-        if( n-group[i]>=0 ){
-            take = solve( dp , i-1 , n-group[i], minProfit- profit[i] , group , profit );
-        }
-        long long notTake = solve( dp , i-1 , n , minProfit , group , profit );
-
-        return dp[i][n][minProfit] = (take+notTake)%MOD;
-        
-    }            
-
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        int m = group.size(); 
-        vector<vector<vector<int>>>dp( m , vector<vector<int>>( n+1 , vector<int>(minProfit+1 , -1))); 
-        return solve( dp, m-1 , n , minProfit , group , profit  ); 
-    
+        int m = group.size();
+        vector<vector<int>>prev(n+1, vector<int>(minProfit+1, 0));
+
+        // base case: no crimes, profit 0
+        for(int j = 0; j <= n; j++)
+            prev[j][0] = 1;
+
+        for(int i = 1; i <= m; i++){
+            vector<vector<int>>curr(n+1, vector<int>(minProfit+1, 0));
+            for(int j = 0; j <= n; j++){
+                for(int k = 0; k <= minProfit; k++){
+                    // not take crime i-1
+                    curr[j][k] = prev[j][k];
+
+                    // take crime i-1 if enough members
+                    if(j >= group[i-1]){
+                        int prevProfit = max(0, k - profit[i-1]);
+                        curr[j][k] = (curr[j][k] + prev[j - group[i-1]][prevProfit]) % MOD;
+                    }
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n][minProfit];
     }
 };
