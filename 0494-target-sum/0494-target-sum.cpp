915cc9930c2b198642b161a3440fcf13b812d4ed
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-14 18:55:32 +0530

    Time: 3 ms (84.84%), Space: 15.5 MB (47.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>&dp , int i , int target , vector<int>& arr ){
-        //base 
-        if( i < 0 ){
-            return (target == 0  ) ? 1 : 0;
-        }
-        
-        if( dp[i][target] !=-1 ) return dp[i][target]; 
-        
-        int take = 0; 
-        if( target >= arr[i]){
-            take = solve( dp , i-1 , target-arr[i] , arr ); 
-        }
-        int notTake = solve( dp , i-1 , target , arr ); 
-        
-        
-        return dp[i][target] = (take + notTake ); 
-    }
-
     int findTargetSumWays(vector<int>& arr, int diff) {
         int n  = arr.size(); 
         int sum = accumulate(arr.begin() , arr.end() , 0); 
         if ((sum - diff) < 0 || (sum - diff) % 2 != 0) return 0;
         int target = (sum-diff)/2;
-        vector<vector<int>>dp( n , vector<int>(target+1 ,-1)); 
-        return solve( dp , n-1 , target , arr );
+        vector<vector<int>>dp( n+1 , vector<int>(target+1 ,0)); 
+        
+        dp[0][0] = 1; 
+
+
+        for(int i = 1; i<= n ; i++ ){
+            for(int j= 0; j<= target ; j++ ){
+                int take = 0; 
+                if( j >= arr[i-1]){
+                    take =dp[i-1][j-arr[i-1]];  
+                }
+                int notTake = dp[i-1][j]; 
+
+
+                dp[i][j] = (take + notTake ); 
+            }
+        }
+        return dp[n][target]; 
     }
 };
