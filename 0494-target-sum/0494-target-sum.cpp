e9974c44f629022ce7fa251f085ad75f69f72ecd
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-14 18:56:46 +0530

    Time: 7 ms (66.29%), Space: 15.1 MB (57.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,23 +6,26 @@ public:
         int sum = accumulate(arr.begin() , arr.end() , 0); 
         if ((sum - diff) < 0 || (sum - diff) % 2 != 0) return 0;
         int target = (sum-diff)/2;
-        vector<vector<int>>dp( n+1 , vector<int>(target+1 ,0)); 
+        vector<int>prev(target+1 ,0); 
         
-        dp[0][0] = 1; 
+        prev[0] = 1; 
 
 
         for(int i = 1; i<= n ; i++ ){
+        vector<int>curr(target+1 ,0); 
+
             for(int j= 0; j<= target ; j++ ){
                 int take = 0; 
                 if( j >= arr[i-1]){
-                    take =dp[i-1][j-arr[i-1]];  
+                    take =prev[j-arr[i-1]];  
                 }
-                int notTake = dp[i-1][j]; 
+                int notTake = prev[j]; 
 
 
-                dp[i][j] = (take + notTake ); 
+                curr[j] = (take + notTake ); 
             }
+            prev = curr; 
         }
-        return dp[n][target]; 
+        return prev[target]; 
     }
 };
