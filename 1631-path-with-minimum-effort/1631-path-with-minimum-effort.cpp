a611bc248e7861e614fc5aff20ec2cacbf0be90d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-18 01:49:06 +0530

    Time: 69 ms (35.53%), Space: 31.2 MB (27.7%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> ,greater<pair<int,pair<int,int>>>> pq; 
-        int n = heights.size() ; 
-        int m = heights[0].size();
-        vector<vector<int>> ans(n ,vector<int>(m , INT_MAX)); 
-        pq.push({0 , {0 , 0 }}); 
-        // ans[0][0] = 0 ; 
 
-        int d1[4] = {-1 , 0 , 0, 1 }; 
-        int d2[4] = {0 , -1 , 1 , 0}; 
+    class comp{
+        public:
+        bool operator()(const tuple<int,int,int>& a , const tuple<int,int,int>& b ){
+            auto [ maxDiff , i , j] =a ;
+            auto[ maxDiff1 , i1 , j1] =b; 
 
-        while(!pq.empty()){
-            auto [dis  , index ] = pq.top() ; 
-            auto [ row , col] = index ;  
-            pq.pop() ; 
-            if( row == n-1 and col == m-1 ) return dis ;      
-            for( int i  = 0 ; i<4 ; i++ ){
-                int movei =row+d1[i]; 
-                int movej =col+d2[i]; 
-                if (dis > ans[row][col]) continue;
-                if( movei>= 0 and movei< n and movej>=0 and movej<m ){
-                    
-                    int distance = max(dis,abs(heights[movei][movej] - heights[row][col])
-);
-                    if(ans[movei][movej]>distance){
-                        ans[movei][movej] = distance ; 
-                        pq.push({distance,{movei ,movej}}); 
-                    }
-
-
-                }
-            }
+            return maxDiff > maxDiff1 ;
         }
-        return -1; 
+    }; 
+
+    vector<pair<int,int>>move= {{1,0},{0,-1},{0,1},{-1,0}}; 
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>> , comp> pq;
+        int n = heights.size() , m = heights[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,INT_MIN));
+        vector<vector<int>>vis(n,vector<int>(m,-1));
+        pq.push({0,0,0}); 
+        while(!pq.empty()){
+            int size = pq.size(); 
+            auto [ maxDiff , i , j ] = pq.top(); pq.pop();
+            vis[i][j] = 1; 
+            if( i == n-1 and j == m-1 ){
+                return maxDiff; 
+            }
+
+            for(auto& it : move ){
+                auto [row , col ] = it; 
+                if( i+row<n and i+row>=0 and j+col<m and j+col>=0 and vis[i+row][j+col]  == -1){
+                    int diff = abs(heights[i][j] - heights[i+row][j+col]); 
+                    
+                        ans[row+i][col+j] = max(diff , maxDiff);
+                        pq.push({ ans[row+i][col+j] , row+i , col+ j}); 
+                    
+                } 
+            }
+
+        }
+
+        return 0; 
+
     }
 };
