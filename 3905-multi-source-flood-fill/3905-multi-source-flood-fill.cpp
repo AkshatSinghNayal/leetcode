b3614632b6e4e989e5c38d9b50a0d09123e6139c
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-19 13:44:23 +0530

    Time: 140 ms (78.96%), Space: 227.4 MB (60.75%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
-        priority_queue<tuple<int,int,int>>pq; 
-        vector<vector<int>> canvas(n , vector<int>(m , 0)); 
         
-        for(auto& it : sources ){
-            pq.push({ it[2] ,it[0], it[1]}); 
-            canvas[it[0]][it[1]] = it[2]; 
-        }
-        vector<vector<int>>direction = {{1,0},{-1,0},{0,1},{0,-1}}; 
+        vector<vector<int>> grid(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 
-        while(!pq.empty()){
-            vector<tuple<int,int,int>> temp; 
-            int size = pq.size(); 
-            while( size-- ){
-                    auto [maxi , row , col ] = pq.top() ; pq.pop();
-                for(auto& it : direction ){
-                    int move1 = row+it[0] , move2 = col+it[1]; 
-                    if( move1>=0 and move1 <n and move2 >= 0 and move2<m and canvas[move1][move2]  == 0  ){
-                        temp.push_back({maxi , move1 , move2 });
-                        canvas[move1][move2] = maxi; 
+        // {time, color, row, col}
+        using T = tuple<int,int,int,int>;
+        queue<T> q;
+
+        // initialize
+        for (auto &s : sources) {
+            int r = s[0], c = s[1], color = s[2];
+            q.push({0, color, r, c});
+            grid[r][c] = color;
+            dist[r][c] = 0;
+        }
+
+        vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!q.empty()) {
+            auto [time, color, r, c] = q.front();
+            q.pop();
+
+            for (auto &d : dir) {
+                int nr = r + d[0], nc = c + d[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+
+                    // first time reaching
+                    if (dist[nr][nc] > time + 1) {
+                        dist[nr][nc] = time + 1;
+                        grid[nr][nc] = color;
+                        q.push({time + 1, color, nr, nc});
+                    }
+                    // same time → take max color
+                    else if (dist[nr][nc] == time + 1 && grid[nr][nc] < color) {
+                        grid[nr][nc] = color;
+                        q.push({time + 1, color, nr, nc});
                     }
                 }
             }
-            for(auto& it : temp){
-                pq.push(it); 
-            }
         }
-        return canvas; 
+
+        return grid;
     }
 };
