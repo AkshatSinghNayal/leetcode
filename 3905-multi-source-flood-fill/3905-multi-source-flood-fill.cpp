57c9b62628ac12e38d6208a7690f337fadce8d66
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-19 13:51:08 +0530

    Time: 439 ms (32.63%), Space: 273.3 MB (39.47%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -3,43 +3,39 @@ public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         
         vector<vector<int>> grid(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        queue<pair<int,int>> q;
 
-        // {time, color, row, col}
-        using T = tuple<int,int,int,int>;
-        queue<T> q;
-
-        // initialize
         for (auto &s : sources) {
             int r = s[0], c = s[1], color = s[2];
-            q.push({0, color, r, c});
             grid[r][c] = color;
-            dist[r][c] = 0;
+            q.push({r, c});
         }
 
         vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while (!q.empty()) {
-            auto [time, color, r, c] = q.front();
-            q.pop();
+            int sz = q.size();
 
-            for (auto &d : dir) {
-                int nr = r + d[0], nc = c + d[1];
+            map<pair<int,int>, int> nextColor;
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+            while (sz--) {
+                auto [r, c] = q.front();
+                q.pop();
 
-                    // first time reaching
-                    if (dist[nr][nc] > time + 1) {
-                        dist[nr][nc] = time + 1;
-                        grid[nr][nc] = color;
-                        q.push({time + 1, color, nr, nc});
-                    }
-                    // same time → take max color
-                    else if (dist[nr][nc] == time + 1 && grid[nr][nc] < color) {
-                        grid[nr][nc] = color;
-                        q.push({time + 1, color, nr, nc});
+                for (auto &d : dir) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 0) {
+                        nextColor[{nr, nc}] = max(nextColor[{nr, nc}], grid[r][c]);
                     }
                 }
+            }
+
+            for (auto &[pos, color] : nextColor) {
+                int r = pos.first, c = pos.second;
+                grid[r][c] = color;
+                q.push({r, c});
             }
         }
 
