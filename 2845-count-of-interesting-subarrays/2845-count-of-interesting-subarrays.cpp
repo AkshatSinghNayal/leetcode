906e19603adfebc12e6121eb36f4da57104f9ab4
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-19 21:49:55 +0530

    Time: 42 ms (89.25%), Space: 115.4 MB (84.58%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size(); 
         unordered_map<int,int>freq; 
         freq[0] =1 ; 
-        for(int i = 0 ;i< n; i++ ){
-            nums[i] = ( nums[i]% MOD == k ) ? 1 : 0; 
-        }
+        
         long long currentMOD = 0 , ans = 0; 
-        for(auto& it : nums ){
+        for(auto& temp : nums ){
+            int it = ( temp%MOD == k ) ? 1 : 0 ; 
             currentMOD = (currentMOD+it)%MOD;
             int target = ( currentMOD -k + MOD)%MOD; 
             if(freq.count(target)){
@@ -19,3 +18,15 @@ public:
         return ans; 
     }
 };
+
+// if nums[i]%MOD == k then count++ in arr = nums[i ........ n-1 ]
+// we can take nums[i] in terms of 0 and 1 
+// and while traversing we get the countSum 
+
+// here we have to find the sum of count such that its mod == k
+
+// so for that we have taken (curr - prev)%mod  = k 
+// so curr%mod - k = prev%mod
+
+// if .............currMOD%MOD - k = prev
+// nd ......prev..................... we remove prev to get currMOD%MOD == k
