b450efcc20838fd9f160904480d48f30f2b59ce5
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-22 02:19:07 +0530

    Time: 80 ms (70.29%), Space: 44.3 MB (81.51%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int V, int dst) {
-        vector<int>ans(V+1 ,INT_MAX); 
-        // time node 
-        priority_queue< pair<int,int> ,  vector<pair<int,int>> , greater<pair<int,int>>> pq ; 
-        pq.push({ 0  , dst }); 
-        ans[dst] = 0 ;  
-        vector<vector<pair<int,int>>> list(V+1); 
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int>ans(n+1 , INT_MAX ); 
+        vector<vector<pair<int,int>>>list(n+1); 
+        queue<pair<int,int>> q; // to time 
+        q.push({ k , 0 }); 
+        ans[k] = 0; 
+
         for(auto& it : times ){
-            list[it[0]].push_back({it[1] , it[2]}); 
+            list[it[0]].push_back({it[1],it[2]}); 
         }
 
-        while(!pq.empty()){
-            auto [ time , node  ] = pq.top() ; 
-            pq.pop() ; 
+        while(!q.empty()){
+            auto [ node , time ] = q.front(); 
+            q.pop(); 
 
-            if( ans[node] < time ) continue; 
+            if( ans[node] < time ) continue ;
 
             for(auto& it : list[node]){
-                auto [node1 , t ] = it; 
-                
-                if( ans[node1] > time+t ){
-                    ans[node1] = time+t ; 
-                    pq.push({ans[node1], node1} ); 
+                auto [ toNode , reachTime ] = it; 
+                if( ans[toNode] > time+ reachTime ){
+                    ans[toNode] = time+reachTime ; 
+                    q.push({toNode , ans[toNode]}); 
                 }
             }
         }
-        int result = *max_element(ans.begin()+1 , ans.end()); 
-        return ( result == INT_MAX ) ? -1 : result   ; 
+        int result  = *max_element(ans.begin()+1 , ans.end()); 
+        return ( result == INT_MAX ) ? -1 : result; 
 
-        
     }
 };
