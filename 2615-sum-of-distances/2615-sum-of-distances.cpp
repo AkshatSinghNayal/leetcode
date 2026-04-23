a0ad119a2a8314d5c65314d7d992392f9395296e
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-23 22:04:52 +0530

    Time: 44 ms (96%), Space: 109.6 MB (83.75%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -15,15 +15,16 @@ public:
             int k = it.size();
 
             if(k == 1) continue;
-            vector<long long> prefix(k); 
-            prefix[0] = it[0]; 
-            for(int i =1 ; i<k ; i++ ){
-                prefix[i] = prefix[i-1]+it[i];
-            }
+            // vector<long long> prefix(k); 
+            // prefix[0] = it[0]; 
+            // for(int i =1 ; i<k ; i++ ){
+            //     prefix[i] = prefix[i-1]+it[i];
+            // }
+            long long prefix = 0 , suffix = accumulate( it.begin() , it.end() , 0LL); 
             for(int i = 0 ;i < k ;i++ ){
-                long long left = ( i> 0 ) ? ( 1LL*it[i]*i - prefix[i-1] ) : 0; 
-                long long right= ( i <k-1) ? ( (prefix[k-1]-prefix[i])-1LL*it[i]*(k-i-1)) : 0; 
-
+                long long left = ( i> 0 ) ? ( 1LL*it[i]*i - prefix ) : 0; 
+                prefix+=it[i];
+                long long right= ( i <k-1) ? ( (suffix - prefix)-1LL*it[i]*(k-i-1)) : 0; 
                 ans[it[i]] = left+right;
             }
         }
