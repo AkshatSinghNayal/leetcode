176863256ab9019f46dec0d432d3fff7a749d50b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-24 00:41:49 +0530

    Time: 34 ms (41.85%), Space: 36.7 MB (47.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
 
-    // int solve(vector<vector<int>>&dp , int i , int amount , vector<int>& coins ){
-    //     //base
-    //     if( i == 0 ){
-    //         if( amount == 0 ) return 1; 
-    //         else if( amount%coins[i] == 0  ) return 1 ; 
-    //         return 0; 
-    //     }
-    //     if( amount <0 ) return 0; 
+    int solve(vector<vector<int>>&dp, int i , int amount , vector<int>& nums ){
+        //base  
 
-        
-    //     if( dp[i][amount] != -1 ) return dp[i][amount]; 
+        if( i== 0 ){
+            if( amount% nums[i] == 0  ) return 1 ; 
+            return 0; 
+        }
 
-    //     int take = 0; 
-    //     if(coins[i]<=amount){
-    //         take = solve( dp , i , amount-coins[i] , coins );
-    //     }
-    //     int notTake = solve( dp , i-1 , amount , coins );
+        if(dp[i][amount] != -1 ) return dp[i][amount]; 
+        int take = 0; 
+        if( nums[i] <= amount ){
+            take = solve(dp , i , amount-nums[i] , nums ); 
+        }
+        int notTake = solve( dp , i-1 , amount , nums ); 
 
-    //     return dp[i][amount] = (take+notTake); 
-    // }
+        return dp[i][amount] = (take+notTake); 
+    }
 
     int change(int amount, vector<int>& coins) {
-        int n  = coins.size(); 
-         vector<int>prev(amount+1 , 0); 
-
-        for(int i =0 ; i<= amount ; i++ ){
-            prev[i] = (i%coins[0] == 0 ) ? 1 : 0; 
-        }
-        // dp[0][0] =1;
-
-        for(int i = 1; i<n ;i++ ){
-         vector<int>curr(amount+1 , 0); 
-
-            for(int j = 0; j<= amount ;j++ ){
-
-                int take = 0; 
-                if(coins[i]<=j){
-                    take = curr[j-coins[i]]; 
-                }
-                int notTake = prev[j]; 
-                curr[j] = (0LL+take+notTake); 
-            }
-            prev= curr; 
-        }
-        return prev[amount]; 
+        int n = coins.size(); 
+        vector<vector<int>>dp(n , vector<int>(amount+1 , -1)); 
+        return solve( dp , n-1 , amount , coins ); 
     }
 };
