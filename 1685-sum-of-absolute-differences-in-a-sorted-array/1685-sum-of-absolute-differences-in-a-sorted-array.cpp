d789b54b8049076477deed71075dc605a87b59b8
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-04-23 22:26:45 +0530

    Time: 84 ms (28.15%), Space: 87.1 MB (99.87%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -6,10 +6,8 @@ public:
         long long prefix = 0 ; 
         vector<int>result(n,0); 
         for(int i  = 0 ;i < n ; i++ ){
-            int left = ( i > 0 ) ? (1LL*nums[i]*i - prefix): 0; 
-            prefix += nums[i]; 
-            int right = (i< n-1 ) ? ( (suffix - prefix) - 1LL*nums[i]*(n-1-i) ) : 0; 
-            result[i] = left+right; 
+            result[i] = ( i > 0 ) ? (1LL*nums[i]*i - prefix): 0 ; 
+            prefix+=nums[i]; result[i]+= (i< n-1 ) ? ( (suffix - prefix) - 1LL*nums[i]*(n-1-i) ) : 0; 
         }
         return result;
 
