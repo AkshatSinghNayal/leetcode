a1043e28da93f7f34268d449b5c005f58968c838
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-09 23:11:22 +0530

    Time: 12 ms (31.8%), Space: 19.7 MB (27.42%) - LeetHub

--- 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
+++ 3922-minimum-flips-to-make-binary-string-coherent/3922-minimum-flips-to-make-binary-string-coherent.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int cnt0=0;int cnt1=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0')cnt0++;
-            else cnt1++;
-        }
-        if(cnt1<2)return 0;
-        if(cnt0==0)return 0;
-        
-        if(s[0]=='1'&&s.back()=='1')return min(cnt1-2,cnt0);
-        else return min(cnt1-1,cnt0);
+        int n = s.length();
+        if (n < 3) return 0;
+        int cnt0 = count(s.begin(), s.end(), '0');
+        int cnt1 = n - cnt0;
+        int res1 = cnt0;
+        int res2 = max(cnt1 - 1, 0);
+        int res3 = cnt1 - (s[0] - '0') - (s[n - 1] - '0');
+        return min({res1, res2, res3});
     }
 };
