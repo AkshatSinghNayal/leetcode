14310bb8b952c9b57bcd79c1a6be90444edf8e5e
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-10 15:38:42 +0530

    Time: 1126 ms (27.97%), Space: 227.6 MB (46.67%) - LeetHub

--- 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
+++ 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
@@ -1,60 +1,37 @@
 class Solution {
 public:
-
-    struct VectorHash {
-        size_t operator()(const vector<int>& v) const {
-            size_t h = 0;
-            for (int x : v) {
-                h ^= std::hash<int>()(x) + 0x9e3779b9 + (h << 6) + (h >> 2);
-            }
-            return h;
-        }
-    };
-
     int minGenerations(vector<vector<int>>& points, vector<int>& target) {
+        set<vector<int>>st; 
+        int k = 0; 
+        for(auto& it : points ){
+            if(it == target) return 0; 
+            st.insert(it); 
+        }
+        if(st.size() <= 1 ) return -1;
+        vector<vector<int>> ans(st.begin(),st.end()); 
 
-        unordered_set<vector<int>, VectorHash> all(points.begin(), points.end());
+        while(true){
+            int n = ans.size();
+            bool found = false; 
+            for(int i = 0 ; i<n-1 ; i++ ){
+                for(int j = i+1; j< n ; j++ ){
+                    vector<int>mid(3); 
+                    for(int k = 0 ; k<3 ; k++ ){
+                        mid[k] = (ans[i][k] + ans[j][k])/2; 
+                    }
+                    if( mid == target) return k+1;
 
-        if (all.count(target)) return 0;
-        if (all.size() <= 1) return -1;
-
-        vector<vector<int>> current = points;
-
-        int k = 0;
-
-        while (true) {
-
-            vector<vector<int>> next;
-            unordered_set<vector<int>, VectorHash> addedThisRound;
-
-            int n = current.size();
-
-            for (int i = 0; i < n-1; i++) {
-                for (int j = i + 1; j < n; j++) {
-
-                    vector<int> mid(3);
-
-                    for (int d = 0; d < 3; d++) {
-                        mid[d] = (current[i][d] + current[j][d]) / 2;
+                    if(!st.count(mid)) {
+                        st.insert(mid); 
+                        found = true; 
+                        ans.push_back(mid);  
                     }
 
-                    if (mid == target) return k + 1;
-
-                    // only add new points
-                    if (!all.count(mid)) {
-                        all.insert(mid);
-                        addedThisRound.insert(mid);
-                        next.push_back(mid);
-                    }
                 }
             }
-
-            if (next.empty()) return -1;
-
-            // move to next generation
-            current.insert(current.end(), next.begin(), next.end());
-
-            k++;
-        }
+            if(!found) return -1; 
+            k++; 
+        } 
+        return -1; 
     }
 };
