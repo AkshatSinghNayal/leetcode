2534d6fab0de17336fd7eaaac65bcf30ba0e1210
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-10 15:14:58 +0530

    Time: 948 ms (51.94%), Space: 234 MB (29.27%) - LeetHub

--- 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
+++ 3923-minimum-generations-to-target-point/3923-minimum-generations-to-target-point.cpp
@@ -29,7 +29,7 @@ public:
 
             int n = current.size();
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n-1; i++) {
                 for (int j = i + 1; j < n; j++) {
 
                     vector<int> mid(3);
