c56532125e5a33c97cfe5a8b14053e6e67178de3
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-13 00:39:02 +0530

    Time: 51 ms (22.46%), Space: 52.9 MB (13.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<int>prev( amount + 1, 0);
-        for (int i = 0; i <= amount; i++) {
-            prev[i]= (i % coins[0] == 0) ? i/coins[0] : 1e9;
-        }
-        for (int i = 1; i <= n; i++) {
-            vector<int>curr( amount + 1, 0);
-            for (int j = 0; j <= amount; j++) {
-                long long take = 1e9;
-                if (j >= coins[i - 1]) {
-                    take = 1 + curr[j - coins[i - 1]];
-                }
+        int n  = coins.size(); 
+        if( amount == 0 ) return 0; 
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,INT_MAX-1));
+        dp[0][0] =0;
+        for(int i = 1 ; i<= n; i++ ){
+            for(int j = 0 ; j<=amount ; j++ ){
+                int take = ( coins[i-1] <= j ) ? 1+dp[i][j-coins[i-1]]: INT_MAX-1; 
+                int notTake = dp[i-1][j];
 
-                long long notTake = prev[j];
-
-                curr[j] = min(take, notTake);
+                dp[i][j] = min(take,notTake); 
             }
-            prev = curr;
         }
-        return (prev[amount] == 1e9 ) ? -1 : prev[amount];
+
+        return dp[n][amount] == INT_MAX-1 ? -1 : dp[n][amount];
     }
 };
