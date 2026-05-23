b15f2eaf0763f404dfbb05ba5c9d3a6ad74a7a16
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-23 22:21:01 +0530

    Time: 0 ms (100%), Space: 8.8 MB (66.8%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
@@ -1,52 +1,11 @@
 class Solution {
 public:
-    long long power(long long a, int k, long long limit) {
-    if (a == 0) return 0;
-
-    long long res = 1;
-
-    while (k--) {
-        if (res > limit / a)
-            return limit + 1;
-
-        res *= a;
-    }
-
-    return res;
-}
     int countKthRoots(int l, int r, int k) {
         if (k == 1) return r - l + 1;
 
-        int left = 0, right = 1e6;
+        int left = ceil(pow(l, 1.0 / k) - 1e-8);
+        int right = floor(pow(r, 1.0 / k) + 1e-8);
 
-        // first x such that x^k >= l
-        int low = 1e6, high = -1;
-
-        int lo = 0, hi = 1e6;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (power(mid, k, r) >= l) {
-                low = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-
-        // last x such that x^k <= r
-        lo = 0, hi = 1e6;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (power(mid, k, r) <= r) {
-                high = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        return max(0, high - low + 1);
+        return max(0, right - left + 1);
     }
 };
