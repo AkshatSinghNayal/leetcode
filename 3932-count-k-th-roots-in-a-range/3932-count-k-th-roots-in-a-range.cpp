d122d3fc360bc8ecc1353d9f8edb04eb19ef989c
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-24 00:24:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (5.55%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int countKthRoots(int l, int r, int k) {
-        if (k == 1) return r - l + 1;
 
-        int left = ceil(pow(l, 1.0 / k) - 1e-8);
-        int right = floor(pow(r, 1.0 / k) + 1e-8);
+        if( k  ==1  ) return r - l +1  ; 
 
-        return max(0, right - left + 1);
+        int left = ceil( pow( l , 1.0/k) - 1e-9 ); 
+        int right = floor( pow(r , 1.0/k) +1e-9); 
+        return max(0 , right - left +1 ); 
+        
     }
 };
