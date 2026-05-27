f3dbea926aa4e8884c0a056ed66371050876b371
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-28 00:23:28 +0530

    Time: 0 ms (100%), Space: 141.4 MB (70.62%) - LeetHub

--- 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
+++ 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-    int minOperations(vector<int>& a) {
-        int n=a.size();
-        int pos0=0,posLast=0,ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(a[i]==0) pos0=i;
-            if(a[i]==n-1) posLast=i;
-        }
-        bool inc=true,dec=true;
-        for(int i=0;i<n;i++){
-            if(a[(i+1)%n]!=(a[i]+1)%n){
-                inc=false;
+    int minOperations(vector<int>& nums) {
+        int n = (int)nums.size();
+        int id = 0;
+
+        for(int x: nums){
+            if(x == 0){
                 break;
             }
+            ++id;
         }
-        if(inc) ans=min({ans,pos0,n-pos0+2});
-        for(int i=0;i<n;i++){
-            if(a[(i+1)%n]!=(a[i]-1+n)%n){
-                dec=false;
-                break;
-            }
+
+        int ogid = id;
+        int cnt = 1;
+        int pr = nums[id];
+
+        bool ok1 = true;
+        bool ok2 = true;
+
+        while(cnt < n){
+            id = (id + 1) % n;
+            if(nums[id] != pr + 1) { ok1 = false; break; }
+            pr = nums[id];
+            ++cnt;
         }
-        if(dec) ans=min({ans,posLast+1,n-pos0});
-        return ans==INT_MAX?-1:ans;
+
+        id = ogid;
+        cnt = 1;
+        pr = nums[id];
+
+        while(cnt < n){
+            id = (id - 1 + n) % n;
+            if(nums[id] != pr + 1) { ok2 = false; break; }
+            pr = nums[id];
+            ++cnt;
+        }
+
+        if(!ok1 && !ok2) return -1;
+
+        int ans = 1e9; // Use a large number as a placeholder for minimum comparison
+        
+        // If it's circularly ascending, check standard left rotation vs. reverse right rotation trick
+        if (ok1) {
+            ans = min(ans, min(ogid, n - ogid + 2));
+        }
+        
+        // If it's circularly descending, check reverse-then-rotate vs. rotate-then-reverse trick
+        if (ok2) {
+            ans = min(ans, min(n - ogid, ogid + 2));
+        }
+
+        return ans;
     }
 };
