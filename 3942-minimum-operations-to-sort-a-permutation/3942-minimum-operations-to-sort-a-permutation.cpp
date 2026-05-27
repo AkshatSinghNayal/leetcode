17406938ce0b3f110dd57d85a61e17c7047d87eb
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-28 00:28:41 +0530

    Time: 2 ms (73.94%), Space: 141.5 MB (42.48%) - LeetHub

--- 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
+++ 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
@@ -1,55 +1,45 @@
 class Solution {
+    bool checkIfIncreasing(vector<int>& nums,int& min_idx,int& n){
+        int i;
+        for(i=min_idx;i<min_idx+n;++i){
+            if(nums[(i+1)%n]-1 != nums[i%n])
+                break;
+        }
+        if((i+1)%n == min_idx)
+            return true;
+        return false;
+    }
 public:
     int minOperations(vector<int>& nums) {
-        int n = (int)nums.size();
-        int id = 0;
+        int n = nums.size();
 
-        for(int x: nums){
-            if(x == 0){
+        //Step-1: Find min_idx
+        int min_idx;
+        int i;
+        for(i=0;i<n;++i)
+            if(nums[i]==0){
+                min_idx = i;
                 break;
             }
-            ++id;
-        }
-
-        int ogid = id;
-        int cnt = 1;
-        int pr = nums[id];
-
-        bool ok1 = true;
-        bool ok2 = true;
-
-        while(cnt < n){
-            id = (id + 1) % n;
-            if(nums[id] != pr + 1) { ok1 = false; break; }
-            pr = nums[id];
-            ++cnt;
-        }
-
-        id = ogid;
-        cnt = 1;
-        pr = nums[id];
-
-        while(cnt < n){
-            id = (id - 1 + n) % n;
-            if(nums[id] != pr + 1) { ok2 = false; break; }
-            pr = nums[id];
-            ++cnt;
-        }
-
-        if(!ok1 && !ok2) return -1;
-
-        int ans = 1e9; // Use a large number as a placeholder for minimum comparison
         
-        // If it's circularly ascending, check standard left rotation vs. reverse right rotation trick
-        if (ok1) {
-            ans = min(ans, min(ogid, n - ogid + 2));
+        //Step-2: Find if increasing or decreasing ?
+        bool is_increasing = checkIfIncreasing(nums,min_idx,n);
+        bool is_decreasing = false;
+        if(!is_increasing){
+            reverse(nums.begin(),nums.end());
+            min_idx = n-min_idx-1;
+            is_decreasing = checkIfIncreasing(nums,min_idx,n);
         }
-        
-        // If it's circularly descending, check reverse-then-rotate vs. rotate-then-reverse trick
-        if (ok2) {
-            ans = min(ans, min(n - ogid, ogid + 2));
-        }
+        if(!is_increasing and !is_decreasing)
+            return -1;
 
-        return ans;
+        //Step-3: Calculate min_operations
+        int min_ops = n;
+        if(is_increasing){
+            min_ops = min(min_idx, 2+n-min_idx);
+        }else{
+            min_ops = min((1+min_idx), 1+(n-min_idx));
+        }
+        return min_ops;
     }
 };
