9d301f288a5414439c450abc419a0fe8fccbc435
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-28 17:35:30 +0530

    Time: 3 ms (71.52%), Space: 141.5 MB (70.1%) - LeetHub

--- 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
+++ 3942-minimum-operations-to-sort-a-permutation/3942-minimum-operations-to-sort-a-permutation.cpp
@@ -1,45 +1,48 @@
 class Solution {
-    bool checkIfIncreasing(vector<int>& nums,int& min_idx,int& n){
-        int i;
-        for(i=min_idx;i<min_idx+n;++i){
-            if(nums[(i+1)%n]-1 != nums[i%n])
-                break;
-        }
-        if((i+1)%n == min_idx)
-            return true;
-        return false;
-    }
 public:
-    int minOperations(vector<int>& nums) {
-        int n = nums.size();
 
-        //Step-1: Find min_idx
-        int min_idx;
-        int i;
-        for(i=0;i<n;++i)
-            if(nums[i]==0){
-                min_idx = i;
+    bool solve(vector<int>& nums , int minIdx , int n ){
+        int i = minIdx ; 
+        for( ;i<n+minIdx ;i++ ){
+            if( nums[i%n] != nums[(i+1)%n] -1) break;
+        }
+        if( (i+1)%n == minIdx ){
+            return true; 
+        }
+        return false ;
+    }
+
+    int minOperations(vector<int>& nums) {
+        int n = nums.size(); 
+        int minIdx = 0; 
+        for(int i = 0 ; i< n; i++ ){
+            if( nums[i] == 0 ){
+                minIdx = i; 
                 break;
             }
-        
-        //Step-2: Find if increasing or decreasing ?
-        bool is_increasing = checkIfIncreasing(nums,min_idx,n);
-        bool is_decreasing = false;
-        if(!is_increasing){
-            reverse(nums.begin(),nums.end());
-            min_idx = n-min_idx-1;
         }
-            is_decreasing = checkIfIncreasing(nums,min_idx,n);
-        if(!is_increasing and !is_decreasing)
-            return -1;
 
-        //Step-3: Calculate min_operations
-        int min_ops = n;
-        if(is_increasing){
-            min_ops = min(min_idx, 2+n-min_idx);
-        }else{
-            min_ops = min((1+min_idx), 1+(n-min_idx));
+        int globalMinIdx = minIdx; 
+        bool increasing = solve(nums, minIdx , n); 
+        bool decreasing = false;
+
+        if( !increasing){
+            reverse(nums.begin() , nums.end() ) ; 
+            minIdx = (n-1)-minIdx ;
+            decreasing = solve(nums, minIdx , n) ;
         }
-        return min_ops;
+
+        if(!increasing and !decreasing ){
+            return -1; 
+        }
+        int minOperations = n ; 
+        if( increasing ){
+            minOperations = min({minOperations , minIdx ,(n-1)-minIdx+3 } ); 
+        }
+        else{
+            minOperations = min( {minOperations , (globalMinIdx+2) , minIdx+1 } ); 
+        }
+        cout<<n-minIdx;
+        return minOperations ; 
     }
 };
