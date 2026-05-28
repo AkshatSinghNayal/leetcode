b47bb3781e99c82f7b8e67f871f8acf1a27fe76d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-29 01:07:47 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    
-    int solve(vector<int>& nums, int idx, int prev, int end,
-              vector<vector<int>>& dp) {
-        
-        if (idx > end) return 0;
 
-        if (dp[idx][prev + 1] != -1)
-            return dp[idx][prev + 1];
+    int robLinear(vector<int>& nums, int l, int r) {
+        int prev2 = 0; // dp[i-2]
+        int prev1 = 0; // dp[i-1]
 
-        // not take
-        int notTake = solve(nums, idx + 1, prev, end, dp);
+        for (int i = l; i <= r; i++) {
+            int take = nums[i] + prev2;
+            int notTake = prev1;
 
-        // take
-        int take = 0;
-        if (prev == -1 || idx - prev > 1) {
-            take = nums[idx] +
-                   solve(nums, idx + 1, idx, end, dp);
+            int cur = max(take, notTake);
+
+            prev2 = prev1;
+            prev1 = cur;
         }
 
-        return dp[idx][prev + 1] = max(take, notTake);
-    }
-
-    int helper(vector<int>& nums, int start, int end) {
-        int n = nums.size();
-
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-
-        return solve(nums, start, -1, end, dp);
+        return prev1;
     }
 
     int rob(vector<int>& nums) {
@@ -35,11 +23,11 @@ public:
 
         if (n == 1) return nums[0];
 
-        // case 1 -> exclude last
-        int case1 = helper(nums, 0, n - 2);
+        // case 1: exclude last
+        int case1 = robLinear(nums, 0, n - 2);
 
-        // case 2 -> exclude first
-        int case2 = helper(nums, 1, n - 1);
+        // case 2: exclude first
+        int case2 = robLinear(nums, 1, n - 1);
 
         return max(case1, case2);
     }
