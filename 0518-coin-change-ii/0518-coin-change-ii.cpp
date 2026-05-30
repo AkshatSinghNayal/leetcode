3622ecbd90078cb01bf5d8a3e597be666e8468d9
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-31 02:00:27 +0530

    Time: 27 ms (64.34%), Space: 36.5 MB (53.93%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
 
-    // int solve(vector<vector<int>>&dp , int i , int amount , vector<int>& coins ){
-    //     //base
-    //     if( i == 0 ){
-    //         if( amount == 0 ) return 1; 
-    //         else if( amount%coins[i] == 0  ) return 1 ; 
-    //         return 0; 
-    //     }
-    //     if( amount <0 ) return 0; 
+    int solve(vector<int>& coins , vector<vector<int>>& dp , int i , int amount ){
+        //base
 
-        
-    //     if( dp[i][amount] != -1 ) return dp[i][amount]; 
+        if( i==0 ){
+            return ( amount%coins[i] == 0 ) ? 1 : 0;  ; 
+        }
+         
 
-    //     int take = 0; 
-    //     if(coins[i]<=amount){
-    //         take = solve( dp , i , amount-coins[i] , coins );
-    //     }
-    //     int notTake = solve( dp , i-1 , amount , coins );
+        if(dp[i][amount] != -1 ) return dp[i][amount] ; 
 
-    //     return dp[i][amount] = (take+notTake); 
-    // }
+        int take = ( amount>=coins[i] ) ? solve( coins , dp , i , amount-coins[i]) : 0 ; 
+        int notTake = solve( coins , dp , i-1 , amount ) ;
+
+        return dp[i][amount] = (take+notTake);
+
+    }
 
     int change(int amount, vector<int>& coins) {
         int n  = coins.size(); 
-        vector<vector<int>>dp( n , vector<int>(amount+1 , -1)); 
-
-        for(int i =0 ; i<= amount ; i++ ){
-            dp[0][i] = (i%coins[0] == 0 ) ? 1 : 0; 
-        }
-        // dp[0][0] =1;
-
-        for(int i = 1; i<n ;i++ ){
-            for(int j = 0; j<= amount ;j++ ){
-
-                int take = 0; 
-                if(coins[i]<=j){
-                    take = dp[i][j-coins[i]]; 
-                }
-                int notTake = dp[i-1][j]; 
-                dp[i][j] = (0LL+take+notTake); 
-            }
-        }
-        return dp[n-1][amount]; 
+        vector<vector<int>>dp(n,vector<int>(amount+1 , -1));
+        return solve( coins, dp , n-1 , amount); 
+        
     }
 };
