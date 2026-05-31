d7b2ed72f779a7eaaf09aefa6eb71d5a3def9270
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-05-31 10:43:48 +0530

    Time: 252 ms (20.54%), Space: 133 MB (20.32%) - LeetHub

--- 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
+++ 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
@@ -14,23 +14,19 @@ public:
 
         for(int i = 1 ; i <= n ; i++){
 
-            int price = items[i - 1][1];
+    int price = items[i-1][1];
 
-            for(int budget = 0 ; budget <= bud ; budget++){
+    for(int budget = 0 ; budget <= bud ; budget++){
 
-                int notTake = dp[i - 1][budget];
+        int take = ( budget >= price ) ? dp[i-1][budget-price] + 1 + bonus[i-1] : 0 ;
+        int notTake = dp[i-1][budget] ;
 
-                int take = (budget >= price)
-                    ? dp[i - 1][budget - price] + 1 + bonus[i - 1]
-                    : 0;
+        dp[i][budget] = max( take , notTake ) ;
+    }
 
-                dp[i][budget] = max(take , notTake);
-            }
-
-            for(int budget = price ; budget <= bud ; budget++)
-                dp[i][budget] = max(dp[i][budget] ,
-                                    dp[i][budget - price] + 1);
-        }
+    for(int budget = price ; budget <= bud ; budget++)
+        dp[i][budget] = max( dp[i][budget] , dp[i][budget-price] + 1 ) ;
+}
 
         return dp[n][bud];
     }
