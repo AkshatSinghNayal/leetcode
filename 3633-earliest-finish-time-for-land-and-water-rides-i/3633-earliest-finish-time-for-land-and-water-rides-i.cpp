5800504b618050cf5a962c5d64b2652d5dfc02d8
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-03 00:27:27 +0530

    Time: 8 ms (46.41%), Space: 94.8 MB (6.33%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,34 +1,55 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int n = landStartTime.size(); 
-        int m= waterStartTime.size(); 
-        int mini = INT_MAX; 
+    int earliestFinishTime(vector<int>& landStartTime,vector<int>& landDuration,vector<int>& waterStartTime,vector<int>& waterDuration) {
 
-        for(int i  = 0 ;i<n ;i++ ){
-            int dist = landStartTime[i]+landDuration[i]; 
-            for(int j =0;j<m ;j++ ){
-                if(dist>=waterStartTime[j]){
-                    mini = min(mini, dist+ waterDuration[j]); 
+        long long ans = LLONG_MAX;
+
+        auto solve = [&](vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+
+            int n = C.size();
+            vector<pair<int,int>> rides(n);
+
+            for(int i = 0; i < n; i++)
+                rides[i] = {C[i], D[i]};
+
+            sort(rides.begin(), rides.end());
+
+            vector<int> start(n);
+            vector<long long> prefMinDur(n);
+            vector<long long> suffMin(n + 1, LLONG_MAX);
+
+            for(int i = 0; i < n; i++) {
+                start[i] = rides[i].first;
+                prefMinDur[i] = rides[i].second;
+                if(i) prefMinDur[i] = min(prefMinDur[i], prefMinDur[i - 1]);
+            }
+
+            for(int i = n - 1; i >= 0; i--) {
+                suffMin[i] = min(suffMin[i + 1], 1LL * rides[i].first + rides[i].second);
+            }
+
+            for(int i = 0; i < A.size(); i++) {
+
+                long long T = 1LL * A[i] + B[i];
+
+                int idx = upper_bound(start.begin(), start.end(), T)- start.begin();
+
+                // case 1: start <= T
+                if(idx > 0) {
+                    ans = min(ans, T + prefMinDur[idx - 1]);
                 }
-                else{
-                    mini = min( waterStartTime[j]+waterDuration[j] , mini ); 
+
+                // case 2: start > T
+                if(idx < n) {
+                    ans = min(ans, suffMin[idx]);
                 }
             }
-        }
+        };
 
-        for(int i  = 0 ;i<m ;i++ ){
-            int dist = waterStartTime[i]+waterDuration[i]; 
-            for(int j =0;j<n ;j++ ){
-                if(dist>=landStartTime[j]){
-                    mini = min(mini, dist+ landDuration[j]); 
-                }
-                else{
-                    mini = min( landStartTime[j]+landDuration[j] , mini ); 
-                }
-            }
-        }
+        solve(landStartTime, landDuration,waterStartTime, waterDuration);
 
-        return mini ; 
+        solve(waterStartTime, waterDuration,landStartTime, landDuration);
+
+        return (int)ans;
     }
 };
