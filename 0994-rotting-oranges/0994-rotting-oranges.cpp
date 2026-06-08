d5b9897ff7e049b8728a420c9f10250ea87d9d01
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-08 11:49:51 +0530

    Time: 6 ms (7.28%), Space: 17.1 MB (28.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,62 +1,45 @@
 class Solution {
 public:
+
+    vector<vector<int>> dir ={{1,0} ,{0,-1} , {0,1} , {-1,0 }}; 
+
     int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>>q; 
+        int count =0 ;  
         int n = grid.size(); 
         int m = grid[0].size(); 
-        queue<pair<int,int>> q;  
-        int oneCount = 0;
-
-        for( int i  =  0 ; i< n; i++ ){
-            for( int j  = 0 ; j<m ; j++ ){
-                if( grid[i][j] == 2){
-                    q.push({i,j}); 
+        
+        for(int j =0; j<n ; j++ ){
+            for(int i =0; i<m ; i++ ){
+                if(grid[j][i] == 2 ){
+                    q.push({j,i}); 
+                    grid[j][i]=-1 ;
                 }
-                if( grid[i][j] ==  1){
-                    oneCount++; 
+                if(grid[j][i] == 1 ){
+                    count++;
                 }
             }
         }
+        int steps = 0;
+        while(!q.empty() and count > 0 ){
+            int size = q.size(); 
+            steps++; 
+            for(int i  = 0;i < size ; i++ ){
+            auto [ row , col  ] = q.front(); q.pop(); 
+                    for(auto& it : dir ){
+                int nr = row+it[0]; 
+                int nc = col+it[1]; 
 
-        if( oneCount == 0 ) return 0 ; 
-        int count =0 ;
-        vector<vector<int>> vis( n ,vector<int>(m ,0)); 
-
-        while(!q.empty()){
-            count++; 
-            int size = q.size()  ;
-            
-            for(int i  = 0 ;i < size ; i++ ){
-
-                auto [first , second] = q.front() ; 
-                 q.pop() ; 
-
-                // case up 1
-            if( first-1>=0 and grid[first-1][second] ==  1 and vis[first-1][second] == 0){
-                vis[first-1][second] =1 ; 
-                oneCount--; 
-                q.push({first-1, second}); 
-            }
-            // case down 2
-            if( first+1<n and grid[first+1][second] ==  1 and vis[first+1][second] == 0){
-                vis[first+1][second] =1 ; 
-                oneCount--; 
-                q.push({first+1, second}); 
-            }
-            // case left 3
-
-            if( second-1>=0 and grid[first][second-1] ==1 and vis[first][second-1] == 0  ){
-                vis[first][second-1] =1 ; 
-                oneCount--; 
-                q.push({first , second-1}); 
-            }
-            // case right 4
-            if( second+1<m and grid[first][second+1] ==1 and vis[first][second+1] == 0  ){
-                vis[first][second+1] =1 ; 
-                oneCount--; 
-                q.push({first , second+1}); 
-            }
+                if( nr < n and nc < m and nr>=0 and nc>=0 and grid[nr][nc] == 1 ){
+                    q.push({nr , nc}); 
+                    grid[nr][nc] = -1 ; 
+                    count--; 
+                }
+            } 
             }
         }
-        return ( oneCount == 0 ) ? count-1 : -1;
+
+        return (count == 0 ) ? steps :  -1 ; 
+
     }
 };
