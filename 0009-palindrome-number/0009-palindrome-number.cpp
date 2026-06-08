efe6304291b69ca411b907a007cb8c4d3bcbc979
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-08 19:26:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-    // Negative numbers or numbers ending with 0 (except 0 itself) are not palindrome
-    if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-    int reversedHalf = 0;
-
-    while (x > reversedHalf) {
-        reversedHalf = reversedHalf * 10 + x % 10; // Add last digit to reversed half
-        x /= 10;                                   // Remove last digit
+        if(x<0) return false;
+        int temp = x; 
+        int reverse = 0; 
+        while(temp){
+            reverse = reverse*10+(temp%10);
+            temp/=10;            
+        }
+        return x == reverse;
     }
-
-    // For even length: x == reversedHalf
-    // For odd length: x == reversedHalf / 10
-    return (x == reversedHalf || x == reversedHalf / 10);
-}
-
 };
