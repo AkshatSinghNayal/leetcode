7228788d4a3bd7376f59fd56a5fd0e4fe11eff9d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-16 15:59:54 +0530

    Time: 151 ms (8.23%), Space: 98.6 MB (5.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> st(nums.begin(), nums.end());  // Insert all numbers into the set
-        int maxi = 0;
-        
-        for (const auto& num : st) {
-            // Only start counting if `num - 1` is not in the set, to avoid counting the same sequence twice
-            if (st.find(num - 1) == st.end()) {
-                int currentNum = num;
-                int count = 1;
-                
-                // Count the length of the current sequence
-                while (st.find(currentNum + 1) != st.end()) {
-                    count++;
-                    currentNum++;
-                }
-                
-                maxi = max(maxi, count);  // Update the maximum length
-            }
+        unordered_map<int,int>mp ;
+        int maxi = INT_MIN;
+        for(auto& it : nums ){
+            mp[it] = 1 ; 
         }
-        
-        return maxi;
+        for(auto& it : nums ){
+            auto digit = it; 
+            int count = 0 ; 
+            if( mp.count( it-1)){
+                while(mp.count(it-1)){
+                    count+=mp[it-1]; 
+                    mp.erase(it-1);
+                    it--; 
+                }
+            }
+            mp[digit]+=count; 
+            maxi = max(maxi , mp[digit]); 
+            
+        }
+        return maxi == INT_MIN ? 0 : maxi  ; 
+
     }
 };
+
