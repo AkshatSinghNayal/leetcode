a10c7b36cc782c370f542bce9f413ebafc611c6c
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-18 01:08:09 +0530

    Time: 4 ms (18.23%), Space: 28.3 MB (27.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
+
+    class comp{
+        public : 
+        bool operator()( pair<int,int>&a , pair<int,int>& b ){
+            auto [ freq1 , a1 ] = a;
+            auto [freq2 , b1 ] = b; 
+            return freq1<freq2;
+        }
+    };
+
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int>mp; 
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>, comp>pq;
         for(auto& it : nums ){
             mp[it]++; 
         }
