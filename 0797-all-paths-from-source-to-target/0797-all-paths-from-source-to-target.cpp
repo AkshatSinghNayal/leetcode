4ba6ad8e17696b07765e1493231d9028d0eea7a9
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-17 16:19:16 +0530

    Time: 26 ms (5.07%), Space: 22.7 MB (5.82%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
 
-    void dfs(vector<vector<int>>& list,vector<vector<int>>&paths , vector<int>&temp, int target ){
+        vector<vector<int>> paths;
+        queue<vector<int>> q;
 
-        if( target == temp.back() ){
-            paths.push_back(temp);
+        q.push({0});
+
+        while (!q.empty()) {
+            vector<int> path = q.front();
+            q.pop();
+
+            int node = path.back();
+
+            if (node == n - 1) {
+                paths.push_back(path);
+                continue;
+            }
+
+            for (int next : graph[node]) {
+                vector<int> newPath = path;
+                newPath.push_back(next);
+                q.push(newPath);
+            }
         }
 
-        for(auto& it : list[temp.back()]){
-            temp.push_back(it);
-            dfs(list , paths , temp , target );
-            temp.pop_back();
-
-        }
-
-    }
-
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& list){
-        vector<vector<int>>paths; vector<int>temp; 
-        int n  = list.size(); 
-        temp.push_back(0);
-        dfs( list , paths , temp , n-1 ); 
-        return paths;   
+        return paths;
     }
 };
