f0531acc656f44abf1cff0cfe6db8390741bfd29
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-19 14:21:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (72.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     void mergeSort(vector<int>& nums1,  vector<int>& nums2 , vector<int>& temp){
-        int i = 0 ; int j =0;
+        int i = 0; int j =0;
         while( i <(nums1.size()-nums2.size()) && j<nums2.size()){
             if(nums1[i]<=nums2[j]){
                 temp.push_back(nums1[i]);
