658fb078b76fa20e1ad17d2a767614d19504bdb3
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-29 02:41:56 +0530

    Time: 8 ms (23.55%), Space: 15.5 MB (31.39%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        unordered_map<TreeNode*, vector<TreeNode*>> mp;
+        int maxi = INT_MIN;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            maxi = max(maxi, node->val);
+
+            if (node->left) {
+                mp[node].push_back(node->left);
+                mp[node->left].push_back(node);
+                q.push(node->left);
+            }
+
+            if (node->right) {
+                mp[node].push_back(node->right);
+                mp[node->right].push_back(node);
+                q.push(node->right);
+            }
+        }
+
+        vector<int> vis(maxi + 1, -1);
+
+        queue<TreeNode*> q1;
+        q1.push(target);
+        vis[target->val] = 1;
+
+        while (!q1.empty()) {
+
+            if (k == 0) {
+                while (!q1.empty()) {
+                    ans.push_back(q1.front()->val);
+                    q1.pop();
+                }
+                return ans;
+            }
+
+            int size = q1.size();
+
+            while (size--) {
+                TreeNode* node = q1.front();
+                q1.pop();
+
+                for (auto it : mp[node]) {
+                    if (vis[it->val] == -1) {
+                        vis[it->val] = 1;
+                        q1.push(it);
+                    }
+                }
+            }
+
+            k--;
+        }
+
+        return ans;
+    }
+};
