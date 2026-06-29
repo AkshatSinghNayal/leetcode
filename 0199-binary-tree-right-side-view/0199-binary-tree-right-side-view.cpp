6dc616a211d4fbab92555b5be00c14dd4ae51ead
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-06-30 02:42:56 +0530

    Time: 0 ms (100%), Space: 15 MB (63.99%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,37 +11,22 @@
  */
 class Solution {
 public:
-    
-    void bfs( TreeNode* root , vector<int> &ans) {
-        
-        
-        queue<TreeNode*>q ; 
-        q.push(root) ; 
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans; 
+        if(!root) return ans ; 
+        queue<TreeNode*>q;
+        q.push(root); 
         while(!q.empty()){
-            int size = q.size();
-            for( int i = 0; i<size ; i++){
-            TreeNode* node = q.front();
-                if( i == size-1 ){
-                    ans.push_back(node->val);
-                }
-                if(node->left != nullptr ){
-                    q.push(node->left);
-                }
-                if(node->right !=nullptr){
-                    q.push(node->right);
-                }
-                q.pop();
+            int size = q.size(); 
+            for(int i  = 0;i<size; i++ ){
+                auto node = q.front() ; q.pop(); 
+
+                if( i == size-1 ) ans.push_back(node->val); 
+
+                if(node->left ) q.push(node->left); 
+                if(node->right) q.push(node->right); 
             }
         }
-        
-
-    }
-
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans ;
-        if( root == nullptr ) return ans ; 
-        bfs( root , ans) ;
-
-        return ans ; 
+        return ans; 
     }
 };
