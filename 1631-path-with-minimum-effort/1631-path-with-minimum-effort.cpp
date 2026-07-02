ee3364c6b37b0fdd5caa99c3fcd5a7651ca600ea
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-03 00:52:55 +0530

    Time: 435 ms (5.03%), Space: 25.9 MB (30.62%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-
-    class comp{
-        public:
-        bool operator()(const tuple<int,int,int>& a , const tuple<int,int,int>& b ){
-            auto [ maxDiff , i , j] =a ;
-            auto[ maxDiff1 , i1 , j1] =b; 
-
-            return maxDiff > maxDiff1 ;
-        }
-    }; 
-
-    vector<pair<int,int>>move= {{1,0},{0,-1},{0,1},{-1,0}}; 
-
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>> , comp> pq;
-        int n = heights.size() , m = heights[0].size();
-        vector<vector<int>>ans(n,vector<int>(m,INT_MIN));
-        vector<vector<int>>vis(n,vector<int>(m,-1));
+
+        int n = heights.size();
+        int m = heights[0].size();
+
+        vector<vector<int>>dir = {{0,-1},{0,1},{1,0},{-1,0}};
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
+        ans[0][0] = 0; 
         pq.push({0,0,0}); 
         while(!pq.empty()){
-            int size = pq.size(); 
-            auto [ maxDiff , i , j ] = pq.top(); pq.pop();
-            vis[i][j] = 1; 
-            if( i == n-1 and j == m-1 ){
-                return maxDiff; 
-            }
 
-            for(auto& it : move ){
-                auto [row , col ] = it; 
-                if( i+row<n and i+row>=0 and j+col<m and j+col>=0 and vis[i+row][j+col]  == -1){
-                    int diff = abs(heights[i][j] - heights[i+row][j+col]); 
-                    
-                        ans[row+i][col+j] = max(diff , maxDiff);
-                        pq.push({ ans[row+i][col+j] , row+i , col+ j}); 
-                    
-                } 
-            }
+            auto [ dist , row ,col ] = pq.top() ; pq.pop(); 
 
+            for(auto& it : dir ){
+                int nr = row+it[0]; 
+                int nc = col+it[1]; 
+
+
+                if( nr<n and nr>=0 and nc<m and nc>=0 ){
+                    int newDist = max(dist, abs(heights[row][col] - heights[nr][nc])); 
+                    if(ans[nr][nc] > newDist){
+                        ans[nr][nc] = newDist; 
+                        pq.push({ans[nr][nc],nr,nc}); 
+                    }
+                }
+
+
+            }
         }
-
-        return 0; 
+        for(auto& it : ans){
+            for(int i  = 0;i<m ; i++ ){
+                cout << it[i] << " " ; 
+            }
+            cout<<endl; 
+        }
+        return ans[n-1][m-1]; 
 
     }
+    
 };
