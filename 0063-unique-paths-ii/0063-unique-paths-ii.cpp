23d821d20c8836c02a9e239835f42b0d3ac0597a
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-06 03:05:39 +0530

    Time: 16 ms (2.6%), Space: 11.9 MB (30.95%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,51 +1,60 @@
 class Solution {
 public:
 
-    // int solve(vector<vector<int>>& obstacleGrid , vector<vector<int>>&dp , int i , int j  ){
-    //     // base 
-    //     if( i< 0 or j < 0 or obstacleGrid[i][j] == 1 ) return 0 ; 
-    //     if( i == 0 and j == 0  ) return 1; 
+    // int solve(vector<vector<int>>& grid,vector<vector<int>>& dp,int i , int j ){
+    //     //base
+
+    //     if( i == 0 and j == 0 ) return 1; 
 
     //     if(dp[i][j] != -1 ) return dp[i][j]; 
-    //     int left = solve(obstacleGrid , dp , i-1 , j ); 
-    //     int up = solve(obstacleGrid , dp , i , j-1 ); 
 
-    //     return dp[i][j] = left+up; 
+    //     long long up =   ( i-1>=0 and grid[i-1][j] != 1 ) ? solve(grid , dp , i-1 , j) : 0; 
+    //     long long left = ( j-1 >=0 and grid[i][j-1] != 1 ) ? solve(grid,dp, i , j-1 ) : 0;
+
+    //     return dp[i][j] =(left+up); 
     // }
 
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid[0].size();
-        int n = obstacleGrid.size();
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1 ) return 0; 
-        vector<vector<int>>dp(n,vector<int>(m , 0)); 
-        bool flag1 = false , flag2 = false;
-        for(int i = 0 ; i< n ; i++ ){
-           if(!flag1){
-            dp[i][0] = (obstacleGrid[i][0] == 0 ) ? 1 : 0;
-            if(dp[i][0] ==0 ) flag1 = true;
-           }
-           else{
-            dp[i][0] = 0; 
-           }
-        }
-        for(int i = 0 ; i< m ; i++ ){
-            if(!flag2){
-                dp[0][i] = (obstacleGrid[0][i] == 0 ) ? 1 : 0; 
-                if(dp[0][i] == 0 ){
-                    flag2 = true;
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n  = grid.size() , m = grid[0].size(); 
+        if(grid[0][0] == 1 or grid[n-1][m-1] ==1 ) return 0; 
+        vector<vector<int>>dp(n,vector<int>(m ,1));
+
+        for(int i  = 0;i<m ; i++ ){
+            if(grid[0][i] == 1 ){
+                while(i<m){
+                    dp[0][i] = 0;
+                    i++; 
                 }
             }
-            else{
-                dp[0][i] =0; 
+        }
+        for(int i = 0; i< n; i++ ){
+            if(grid[i][0] ==  1 ){
+                while(i<n ){
+                    dp[i][0] = 0; 
+                    i++;
+                }
             }
         }
 
-        for(int i =  1; i< n ; i++ ){
-            for(int j =  1 ; j< m ; j++ ){
-                dp[i][j] = (obstacleGrid[i][j] == 0 ) ? dp[i-1][j]+dp[i][j-1] : 0; 
+        for(int i  =1 ; i< n; i++ ){
+            for(int j =1 ;j<m ; j++ ){
+                if(grid[i][j] == 1 or i == 0 and j == 0) continue;
+                long long up =   ( i-1>=0 and grid[i-1][j] != 1 ) ? dp[i-1][j]: 0; 
+                long long left = ( j-1 >=0 and grid[i][j-1] != 1 ) ? dp[i][j-1]: 0;
+
+                dp[i][j] = up+left;
+
             }
         }
-        return dp[n-1][m-1]; 
-        
+
+        for(auto& it : dp ){
+            for(int i = 0;i<m ;i++ ){
+                cout<<it[i] << "  "; 
+            }
+            cout<<endl;
+        }
+
+        return dp[n-1][m-1];
+
     }
 };
