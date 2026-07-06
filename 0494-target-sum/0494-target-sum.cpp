24a8efd1a113c1e8b15073dcd85c9cd74c64389a
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-07 00:58:26 +0530

    Time: 11 ms (57.2%), Space: 25.2 MB (30.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
 
-    int solve( vector<int>& nums , vector<vector<int>>& dp , int i , int target ){
-        //base 
-        if(i<0){
-            return (target == 0 ) ? 1 : 0 ;
-        }
-        if( dp[i][target] != -1 ) return dp[i][target]; 
+    int solve(vector<int>& nums ,vector<vector<int>>&dp , int i , int target , int offset){
+        //base
+        if(target > offset or target< -offset ) return 0; 
 
-        long long  take = ( target>= nums[i]) ? solve( nums , dp , i-1 , target-nums[i]) : 0 ; 
-        long long notTake = solve(nums,dp , i-1 , target); 
+        if( i<0 ) return target == 0 ;
 
-        return dp[i][target] = take+notTake;
+        if(dp[i][target+offset] != -1 ) return dp[i][target+offset];
+
+        int add = ( target+nums[i]<= offset) ? solve( nums , dp , i-1 , target+nums[i] , offset) : 0; 
+        int sub = solve(nums , dp , i-1 , target-nums[i] , offset ); 
+
+        return dp[i][target+offset] = add+sub; 
     }
 
-    int findTargetSumWays(vector<int>& nums, int want) {
-        int n = nums.size();
-        long long total = accumulate( nums.begin() , nums.end() , 0LL ); 
-        if(  (total - want) <0 or  (total - want) %2 != 0 ) return 0 ; 
-        int target = ( total - want )/2; 
-        vector<vector<int>>dp(n,vector<int>(target+1 , -1 )); 
-       
-        return solve( nums , dp , n-1 , target ) ;   
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n  = nums.size(); 
+        int total = accumulate(nums.begin(), nums.end() , 0 ) ;
+        if(target > total or target< -total ) return 0; 
+        int offset = total ;
+        vector<vector<int>>dp(n , vector<int>(2*total+1 , -1 )); 
+        return solve(nums, dp , n-1 , target , offset ); 
     }
 };
