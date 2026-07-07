ee557642524d6996d1ecad34f21586b3a1e48389
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-08 02:35:11 +0530

    Time: 20 ms (95.07%), Space: 12.9 MB (99.65%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,44 +1,34 @@
 class Solution {
     int n;
-    int memo[50][50][50];
+    int dp[50][50][50];
 
     int dfs(int r1, int c1, int r2, vector<vector<int>>& grid) {
-        int c2 = r1 + c1 - r2;
+        int c2 = r1+c1 -r2; 
+        int nr[] = {1,0}; 
+        int nc[] = {0,1}; 
+        
+        if( r1 == n-1 and c1 == n-1 ) return grid[r1][c1];
+        if(r1>=n or r2>=n or c1>=n or c2 >=n or grid[r1][c1] == -1 or grid[r2][c2] == -1 ) return -1e9; 
 
-        // Out of bounds or thorn
-        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n || grid[r1][c1] == -1 ||
-            grid[r2][c2] == -1) {
-            return -1e9;
-        }
+        if(dp[r1][c1][r2] !=-1 ) return dp[r1][c1][r2]; 
 
-        // Reached destination
-        if (r1 == n - 1 && c1 == n - 1) {
-            return grid[r1][c1];
-        }
+        int currSum =  grid[r1][c1] + ( r1 == r2 and c1 == c2 ? 0 : grid[r2][c2] ) ; 
+        int maxi = -1e9; 
 
-        if (memo[r1][c1][r2] != -1)
-            return memo[r1][c1][r2];
-
-        int cherries = grid[r1][c1] + (r1 == r2 ? 0 : grid[r2][c2]);
-
-        int dr[] = {1, 0}; // Down, Right
-        int dc[] = {0, 1};
-
-        int max_next = -1e9;
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                max_next = max(max_next, dfs(r1 + dr[i], c1 + dc[i], r2 + dr[j], grid));
+        for(int i  = 0;i<2 ;i++ ){
+            for(int j = 0;j<2 ; j++ ){
+                maxi = max(maxi , dfs( r1+nr[i] , c1+nc[i] , r2+nr[j] , grid)); 
             }
         }
 
-        return memo[r1][c1][r2] = cherries + max_next;
+        return dp[r1][c1][r2] = maxi+currSum; 
+
     }
 
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         n = grid.size();
-        memset(memo, -1, sizeof(memo));
+        memset(dp, -1, sizeof(dp));
         return max(0, dfs(0, 0, 0, grid));
     }
 };
