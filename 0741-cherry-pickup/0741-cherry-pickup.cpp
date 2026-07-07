1a5fc0205e0039087a0848bf531295bf183b0aad
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-08 02:43:44 +0530

    Time: 19 ms (96.27%), Space: 13 MB (99.65%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,34 +1,57 @@
 class Solution {
-    int n;
-    int dp[50][50][50];
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        const int NEG = -1e9;
 
-    int dfs(int r1, int c1, int r2, vector<vector<int>>& grid) {
-        int c2 = r1+c1 -r2; 
-        int nr[] = {1,0}; 
-        int nc[] = {0,1}; 
-        
-        if( r1 == n-1 and c1 == n-1 ) return grid[r1][c1];
-        if(r1>=n or r2>=n or c1>=n or c2 >=n or grid[r1][c1] == -1 or grid[r2][c2] == -1 ) return -1e9; 
+        static int dp[50][50][50];
 
-        if(dp[r1][c1][r2] !=-1 ) return dp[r1][c1][r2]; 
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    dp[i][j][k] = NEG;
 
-        int currSum =  grid[r1][c1] + ( r1 == r2 and c1 == c2 ? 0 : grid[r2][c2] ) ; 
-        int maxi = -1e9; 
+        // Base 
+        dp[n - 1][n - 1][n - 1] = grid[n - 1][n - 1];
 
-        for(int i  = 0;i<2 ;i++ ){
-            for(int j = 0;j<2 ; j++ ){
-                maxi = max(maxi , dfs( r1+nr[i] , c1+nc[i] , r2+nr[j] , grid)); 
+        int nr[] = {1, 0};
+        int nc[] = {0, 1};
+
+        for (int r1 = n - 1; r1 >= 0; r1--) {
+            for (int c1 = n - 1; c1 >= 0; c1--) {
+                for (int r2 = n - 1; r2 >= 0; r2--) {
+
+                    int c2 = r1 + c1 - r2;
+
+                    if (c2 < 0 || c2 >= n) continue;
+
+                    if (grid[r1][c1] == -1 || grid[r2][c2] == -1) continue;
+
+                    if (r1 == n - 1 && c1 == n - 1) continue; 
+
+                    int currSum = grid[r1][c1] + ((r1 == r2 && c1 == c2) ? 0 : grid[r2][c2]);
+
+                    int maxi = NEG;
+
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+
+                            int nr1 = r1 + nr[i];
+                            int nc1 = c1 + nc[i];
+                            int nr2 = r2 + nr[j];
+                            int nc2 = nr1 + nc1 - nr2;
+
+                            if (nr1 >= n || nc1 >= n || nr2 >= n || nc2 >= n) continue;
+
+                            maxi = max(maxi, dp[nr1][nc1][nr2]);
+                        }
+                    }
+
+                    dp[r1][c1][r2] = currSum + maxi;
+                }
             }
         }
 
-        return dp[r1][c1][r2] = maxi+currSum; 
-
-    }
-
-public:
-    int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size();
-        memset(dp, -1, sizeof(dp));
-        return max(0, dfs(0, 0, 0, grid));
+        return max(0, dp[0][0][0]);
     }
 };
