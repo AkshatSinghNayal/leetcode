7dcdae60a957a84ec75eb84ea61f21bdf5ec3bdc
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-09 02:52:35 +0530

    Time: 362 ms (13.14%), Space: 292.9 MB (5.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string text1) {
+
         string text2 = text1;
         reverse(text2.begin(), text2.end());
 
         int n = text1.size();
+
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        pair<int, int> index;
 
         int maxi = 0;
-        int end = 0;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
 
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                int call = (text1[i - 1] == text2[j - 1]) ? 1 + dp[i - 1][j - 1] : 0;
+                dp[i][j] = call;
 
-                    int start1 = i - dp[i][j];
+                if (call) {
 
+                    int start1 = i - call;
                     int start2 = n - j;
 
-                    if (start1 == start2 && dp[i][j] > maxi) {
-                        maxi = dp[i][j];
-                        end = i;
+                    if (start1 == start2 && maxi < call) {
+                        maxi = call;
+                        index = {i - 1, j - 1};
                     }
-                } else {
-                    dp[i][j] = 0;
                 }
             }
         }
 
-        return text1.substr(end - maxi, maxi);
+        if (maxi == 0) return "";
+
+        return text1.substr(index.first - maxi + 1, maxi);
     }
 };
