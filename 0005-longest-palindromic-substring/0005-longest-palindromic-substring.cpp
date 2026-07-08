a61b3f4715849ed3a4242f46ae1813ec2cf1105b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-09 02:29:04 +0530

    Time: 332 ms (14.88%), Space: 292.8 MB (5.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    string palindromicCheck(string& s, int left, int right) {
-        int n = s.size();
-        while ( left >= 0 && right < n) {
-            if (s[left] == s[right]) {
-                left--;
-                right++;
+    string longestPalindrome(string text1) {
+        string text2 = text1;
+        reverse(text2.begin(), text2.end());
+
+        int n = text1.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        int maxi = 0;
+        int end = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+
+                    int start1 = i - dp[i][j];
+
+                    int start2 = n - j;
+
+                    if (start1 == start2 && dp[i][j] > maxi) {
+                        maxi = dp[i][j];
+                        end = i;
+                    }
+                } else {
+                    dp[i][j] = 0;
+                }
             }
-            else break;
-        }
-        return s.substr(left+1, right-left-1);
-    }
-
-    string longestPalindrome(string s) {
-        int i = 0;
-       
-        string result = "";
-        string ans1 = "";
-        string ans2 = "";
-        int n = s.size();
-        while (i < n ) {
-            ans1 = palindromicCheck(s, i, i);
-            ans2 = palindromicCheck(s, i, i + 1);
-            
-            i++;
-       
-            if (ans1.size() > result.size()) result = ans1;
-            if (ans2.size() > result.size()) result = ans2;
         }
 
-        return result;
+        return text1.substr(end - maxi, maxi);
     }
 };
