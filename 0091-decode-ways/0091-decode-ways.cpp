ae0f23a0fb01670ddac233fc39d2020ec77a2d42
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-09 00:38:49 +0530

    Time: 1 ms (25.39%), Space: 9 MB (32%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,33 +1,57 @@
 class Solution {
 public:
 
-    int solve( string s , int i , vector<int>& dp ){
-        if( i == 0  and s[i] == '0') return 0; 
-        if( i <= 0 ) return 1; 
+    // int solve( string s , int i , vector<int>& dp ){
+    //     if( i == 0  and s[i] == '0') return 0; 
+    //     if( i <= 0 ) return 1; 
 
 
-        if(dp[i] != -1 ) return dp[i]; 
+    //     if(dp[i] != -1 ) return dp[i]; 
 
-        int one = ( s[i] != '0' ) ? solve( s , i-1 , dp ) : 0 ;
-        int two = 0; 
-        int temp = 0 ; 
+    //     int one = ( s[i] != '0' ) ? solve( s , i-1 , dp ) : 0 ;
+    //     int two = 0; 
+    //     int temp = 0 ; 
+    //     if( i-1 >=0  ){
+    //         temp = stoi( s.substr( i-1 , 2 )); 
+    //         if(s[i-1] == '0') temp =0 ; 
+    //     }
+    //     if( temp>=1 and temp<=26 ){
+    //        two = solve( s, i-2 , dp ); 
+    //     }
+        
+
+    //     return dp[i] = one+two;
+        
+    // }
+
+    int numDecodings(string s) {
+        int n = s.size(); 
+        vector<int>dp(n,0); 
+
+        if( s[0] != '0' ) dp[0] =1; 
+
+        for(int i  = 1 ;i< n ;i++ ){
+
+
+            int one = ( s[i] != '0' ) ? dp[i-1] : 0 ;
+            int two = 0; 
+            int temp = 0 ; 
+
+
         if( i-1 >=0  ){
             temp = stoi( s.substr( i-1 , 2 )); 
             if(s[i-1] == '0') temp =0 ; 
         }
-        if( temp>=1 and temp<=26 ){
-           two = solve( s, i-2 , dp ); 
+        if( temp>=10 and temp<=26 ){
+            if( i-2 < 0 ) two = 1; 
+            else two = dp[i-2]; 
         }
         
 
-        return dp[i] = one+two;
+        dp[i] = one+two;
         
-    }
+        }
 
-    int numDecodings(string s) {
-        int n = s.size(); 
-        vector<int>dp(n,-1); 
-
-        return solve( s , n-1 , dp );
+        return dp[n-1] ; 
     }
 };
