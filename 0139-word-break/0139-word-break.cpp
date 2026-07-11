34369453c856192a3699d237940dcde7045bf388
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-12 05:22:36 +0530

    Time: 0 ms (100%), Space: 11 MB (80.44%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    bool solve(int i, string &s, unordered_map<char, vector<string>> &mp,vector<int> &dp) {
 
-        if (i == s.size()) return true;
-        if (dp[i] != -1) return dp[i];
+    bool solve( int i , string& s , unordered_map<char,vector<string>>&mp , vector<int>&dp  ){
+        // base
+        if( i >= s.size() ) return true; 
 
-        char ch = s[i];
+        if(dp[i] != -1 ) return dp[i] ; 
 
-        for (auto &temp : mp[ch]) {
+        char ch = s[i]; 
 
-            int j = 0;
-            int k = i;
-
-            while (j < temp.size() && k < s.size() && s[k] == temp[j]) {
-                j++;
-                k++;
+        for(auto& temp : mp[ch]){
+            int k  = i; 
+            int j = 0; 
+            while( j<temp.size() and s[k] == temp[j]){
+                j++; k++; 
             }
 
-            if (j == temp.size()) {          // whole word matched
-                if (solve(k, s, mp, dp))
-                    return dp[i] = true;
+            if( j == temp.size()){
+                if(solve( k , s , mp , dp )) return dp[i] = true; 
             }
         }
-
+        
         return dp[i] = false;
+
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<char,vector<string>>mp; 
+        for(auto& it : wordDict ){
+            mp[it[0]].push_back(it); 
+        }
 
-        unordered_map<char, vector<string>> mp;
-
-        for (auto &it : wordDict)
-            mp[it[0]].push_back(it);
-
-        vector<int> dp(s.size(), -1);
-
-        return solve(0, s, mp, dp);
+        vector<int>dp(s.size() , -1); 
+        return solve(0,s,mp,dp); 
     }
 };
