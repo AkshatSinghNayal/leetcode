fc2c1911f2d4d615d15bb10ec789169df629dbe5
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-12 18:25:49 +0530

    Time: 27 ms (5.02%), Space: 24.3 MB (5.3%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
+        vector<vector<int>>ans; 
+        queue<pair<int,vector<int>>>q; 
+        vector<vector<int>>list(n);
 
-        vector<vector<int>> paths;
-        queue<vector<int>> q;
-
-        q.push({0});
-
-        while (!q.empty()) {
-            vector<int> path = q.front();
-            q.pop();
-
-            int node = path.back();
-
-            if (node == n - 1) {
-                paths.push_back(path);
-                continue;
-            }
-
-            for (int next : graph[node]) {
-                vector<int> newPath = path;
-                newPath.push_back(next);
-                q.push(newPath);
+        for(int i = 0;i<graph.size(); i++ ){
+            for(int j = 0; j<graph[i].size(); j++ ){
+                list[i].push_back(graph[i][j]); 
             }
         }
 
-        return paths;
+        q.push({0,{0}});
+        while(!q.empty()){
+            auto [ node , vec ] = q.front(); q.pop(); 
+
+            if(node == n-1 ) ans.push_back(vec); 
+
+            for(auto& it : list[node]){
+                vector<int>temp = vec; 
+                temp.push_back(it); 
+                q.push({it , temp}); 
+            }
+        }
+
+        return ans; 
     }
 };
