eef4e6e6539b34dc899ae377029f7da10aeaa1f5
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-12 19:14:52 +0530

    Time: 33 ms (25.33%), Space: 24.7 MB (67.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-
-        vector<vector<int>> ans(n, vector<int>(n, INT_MAX));
-        priority_queue<pair<int, pair<int,int>>,vector<pair<int, pair<int,int>>>,greater<pair<int, pair<int,int>>>> pq;
-
-        pq.push({1, {0, 0}});
-        ans[0][0] = 1;
-
+        int n  =  grid.size(); 
+        if(grid[0][0] == 1 or grid[n-1][n-1] == 1 ) return -1; 
+        vector<vector<int>>vis(n,vector<int>(n ,-1)); 
+        vis[0][0] =1; 
+        priority_queue<tuple<int,int,int> , vector<tuple<int,int,int>> , greater<tuple<int,int,int>>>pq;
+        pq.push({1,0,0}); 
         int dr[8] = {-1,-1,-1, 0, 0, 1, 1, 1};
         int dc[8] = {-1, 0, 1,-1, 1,-1, 0, 1};
 
-        while (!pq.empty()) {
-            auto [dist, cell] = pq.top();
-            auto [r, c] = cell;
-            pq.pop();
+        while(!pq.empty()){
+            auto [ cost , row , col  ] = pq.top(); pq.pop(); 
+            if( row == n-1 and col == n-1 ) return cost; 
 
-            // reach last → break and return
-            if (r == n-1 && c == n-1)
-                return dist;
+            for(int i  = 0;i<8 ; i++ ){
+                int nr = row+dr[i]; 
+                int nc = col+dc[i]; 
 
-            for (int i = 0; i < 8; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0 && ans[nr][nc] > dist + 1) {
-
-                    ans[nr][nc] = dist + 1;
-                    pq.push({ans[nr][nc], {nr, nc}});
+                if( nr>=0 and nr<n and nc>=0 and nc<n and grid[nr][nc] != 1 and vis[nr][nc] == -1){
+                    pq.push({cost+1 , nr , nc }); 
+                    vis[nr][nc] = 1; 
                 }
             }
         }
-
-        return -1;
+        return -1; 
     }
 };
