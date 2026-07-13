431781d4f218a1c39e4dc920bc850cabffef2238
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-13 19:32:52 +0530

    Time: 23 ms (0%), Space: 8.8 MB (5.54%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    long long ok = 0 ; unordered_set<int> st;
-    void solve( vector<int>& ans , int low, int high , long long initial , long long temp , long long ok ){
-        if( initial > high) return ; 
-        if (initial % 10 == 9) {
-            if(initial >= low && !st.count(initial)){
-                st.insert(initial);
-                ans.push_back(initial); 
-            }
-            solve(ans , low, high , 1LL*temp*10+temp%10+1 , 1LL*temp*10+temp%10+1 , ok*1LL*10+1 ); 
-            return;
-        }
 
-        if(initial >= low && !st.count(initial)){
+bool check(int num) {
+
+while (num >= 10) {
+
+int last_digit = num % 10;
+
+int prev_digit = (num / 10) % 10;
+
+
+if (last_digit != prev_digit + 1) {
+
+return false;
+
+}
+
+num /= 10;
+
+}
+
+return true;
+
+}
+
+    long long ok = 0 ; unordered_set<int> st;
+    void solve( vector<int>& ans , int high , long long initial , long long temp , long long ok ){
+        //base
+        if( initial > high) return ; 
+        if( check(initial) == true and  temp%10 == 9 and !st.count(initial)){
+            st.insert(initial);
+            ans.push_back(initial); 
+            solve(ans , high , 1LL*temp*10+temp%10+1 , 1LL*temp*10+temp%10+1 , ok*1LL*10+1 ); 
+            return ; 
+        }
+        if( check(initial) == true and !st.count(initial)){
             st.insert(initial);
             ans.push_back(initial); 
         }
         
-        solve(ans , low, high , initial+ok , temp , ok);
+        if( check(initial) == true ) solve(ans , high , initial+ok , temp , ok);
+        solve(ans , high , 1LL*temp*10+temp%10+1 , 1LL*temp*10+temp%10+1 , ok*1LL*10+1 ); 
+
     }
 
     vector<int> sequentialDigits(int low, int high) {
         vector<int>ans;
         int digits = (int)log10(low) + 1; 
-        if (digits < 2) digits = 2; 
         long long initial = 0 , temp = 0 ;
 
         for(int i = 1 ; i<= digits ; i++ ){
@@ -31,10 +54,15 @@ public:
             initial = initial*10 + i ; 
         }
         temp = initial;
+        while(initial<low){
+            initial+=ok;
+        }
 
+        if(initial > high ) return ans; 
 
-        solve( ans , low, high , initial , temp , ok) ; 
+        solve( ans , high , initial , temp , ok) ; 
 
         return ans; 
+
     }
 };
