3ea08722a4b1104e0b72580a39f623f825c827e7
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-17 03:53:05 +0530

    Time: 166 ms (42.19%), Space: 39 MB (61.66%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int dp[601][101][101]; 
 
-    int solve( vector<pair<int,int>>&data , int one , int zero , int i ){
-        //base
-        if( i< 0 ) return 0;
+    // int solve( vector<pair<int,int>>&data , int one , int zero , int i ){
+    //     //base
+    //     if( i< 0 ) return 0;
 
-        if(dp[i][one][zero] != -1  ) return dp[i][one][zero]; 
-        auto [ t_one , t_zero ] = data[i]; 
-        int take = ( t_one <= one and t_zero <= zero ) ? 1 + solve(data , one-t_one , zero-t_zero , i-1 ) : 0;
-        int notTake = solve(data , one , zero , i-1 );
+    //     if(dp[i][one][zero] != -1  ) return dp[i][one][zero]; 
+    //     auto [ t_one , t_zero ] = data[i]; 
+    //     int take = ( t_one <= one and t_zero <= zero ) ? 1 + solve(data , one-t_one , zero-t_zero , i-1 ) : 0;
+    //     int notTake = solve(data , one , zero , i-1 );
 
-        return dp[i][one][zero] = max(take , notTake);
+    //     return dp[i][one][zero] = max(take , notTake);
 
-    }
+    // }
 
     int findMaxForm(vector<string>& strs, int m, int n1) {
         int n  = strs.size(); 
@@ -27,7 +27,23 @@ public:
             }
             data.push_back({one , zero}); 
         }
-        memset(dp,-1,sizeof(dp));
-        return solve( data, n1 , m , n-1 ); 
+        memset(dp,0,sizeof(dp));
+
+
+        for(int i =1 ; i<=n ; i++ ){
+            auto [ t_one , t_zero ] = data[i-1]; 
+            for(int one =n1 ; one>=0; one-- ){
+                for(int zero = m; zero>=0; zero-- ){
+
+                    int take = ( t_one <= one and t_zero <= zero ) ? 1 + dp[i-1][one-t_one][zero-t_zero] : 0;
+                    int notTake = dp[i-1][one][zero];
+                    dp[i][one][zero] = max(take , notTake);
+                }
+            }
+        }
+
+        return dp[n][n1][m]; 
+
+
     }
 };
