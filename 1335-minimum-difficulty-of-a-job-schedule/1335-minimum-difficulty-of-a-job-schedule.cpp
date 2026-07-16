efaa1420fe1a8eb05a62e60087190edb9cc0a4b9
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-17 02:11:30 +0530

    Time: 11 ms (92.41%), Space: 11.3 MB (33.47%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     int minDifficulty(vector<int>& job, int d) {
-
-        int n = job.size();
-
-        if (n < d) return -1;
-
-        vector<vector<int>> dp(n + 1, vector<int>(d + 1, INT_MAX));
-
-        // Base case: day = 1
-        int mx = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            mx = max(mx, job[i]);
-            dp[i][1] = mx;
+        int n = job.size(); 
+        if( n < d ) return -1; 
+        vector<vector<int>>dp(n+1 , vector<int>(d+1 , -1)); 
+        int maxii = INT_MIN;
+        for(int i = n-1; i>= 0 ;i-- ){
+            maxii = max(maxii , job[i]);
+            dp[i][1] = maxii;
         }
 
-        // Fill DP
-        for (int day = 2; day <= d; day++) {
+        for( int idx =n-1; idx>=0 ;idx-- ){
+            for(int day =2 ; day<= d ; day++ ){
 
-            for (int idx = n - day; idx >= 0; idx--) {
+                int maxi = job[idx] , result = INT_MAX; 
 
-                int maxi = 0;
-
-                for (int i = idx; i <= n - day; i++) {
-
-                    maxi = max(maxi, job[i]);
-
-                    if (dp[i + 1][day - 1] != INT_MAX)
-                        dp[idx][day] = min(dp[idx][day], maxi + dp[i + 1][day - 1]);
+                for(int i = idx ; i<=n-day; i++ ){
+                    maxi = max(maxi , job[i]); 
+                    int temp = maxi + dp[i+1][day-1]; 
+                    result = min(result, temp);
                 }
+                dp[idx][day] = result;
             }
         }
-
-        return dp[0][d];
+        return dp[0][d]; 
     }
 };
