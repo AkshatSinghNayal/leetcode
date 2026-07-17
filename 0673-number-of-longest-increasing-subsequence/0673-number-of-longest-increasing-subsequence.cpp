7cf6810a4ecc2e0920d63b038f888318a385f1bf
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-18 03:14:25 +0530

    Time: 322 ms (5%), Space: 252.5 MB (5.45%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    vector<vector<pair<int,int>>> dp;
 
-    pair<int,int> solve(vector<int>& nums, int i, int prev) {
-        int n = nums.size();
-
-        if (i == n) return {0, 1};
-
-        if (dp[i][prev + 1].first != -1)
-            return dp[i][prev + 1];
-
-        auto notTake = solve(nums, i + 1, prev);
-
-        pair<int,int> take = {-1000000, 0};
-        if (prev == -1 || nums[prev] < nums[i]) {
-            take = solve(nums, i + 1, i);
-            take.first++;
+    pair<int,int> solve(vector<int>& nums,vector<vector<pair<int,int>>>&dp,int i, int prev , int n ){
+        if( i>=n ) return {0,1}; 
+        if(dp[i][prev+1].first != -1 ){
+            return dp[i][prev+1];
         }
 
-        if (take.first > notTake.first)
-            return dp[i][prev + 1] = take;
+        pair<int,int> take = {-1e9 , 0}; 
+        if( prev == -1 or nums[prev] < nums[i]){
+            take = solve(nums , dp , i+1 , i , n ); 
+            take.first++;
+        }
+        auto notTake = solve(nums,dp,i+1 ,prev , n ); 
 
-        if (take.first < notTake.first)
-            return dp[i][prev + 1] = notTake;
+        if(take.first > notTake.first ){
+            return dp[i][prev+1] = take;
+        }
+        else if( take.first < notTake.first ){
+            return dp[i][prev+1] = notTake;
+        }
 
-        return dp[i][prev + 1] = {take.first, take.second + notTake.second};
+        return dp[i][prev+1] = { take.first , take.second+notTake.second}; 
     }
 
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        dp.assign(n, vector<pair<int,int>>(n + 1, {-1, -1}));
-        return solve(nums, 0, -1).second;
+        int n = nums.size(); 
+        vector<vector<pair<int,int>>>dp(n,vector<pair<int,int>>(n+1 , {-1,-1})); 
+        return solve(nums,dp,0,-1,n).second;
     }
 };
