da53b505566ce46f019a8eae7e9049cef2237fd5
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-19 04:18:33 +0530

    Time: 29 ms (17.09%), Space: 53.4 MB (5.32%) - LeetHub

--- 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
+
+    int solve(string& s, vector<int>&mp, vector<int>&dp, int i, int n, string& chars){
+
+        if(i >= n) return 0;
+
+        if(dp[i] != -1e7) return dp[i];
+
+        int take = (chars.find(s[i]) != string::npos) 
+                    ? mp[s[i]-'a'] 
+                    : s[i]-'a'+1;
+
+        int next = solve(s, mp, dp, i+1, n, chars);
+
+        return dp[i] = max(take, take + next);
+    }
+
     int maximumCostSubstring(string s, string chars, vector<int>& vals) {
-        int n  = vals.size();
-        unordered_map<char,int>mp ; 
-        for(int i = 0; i<n ;i++ ){
-            mp[chars[i]] = vals[i]; 
-        }
-        // for(auto& it : mp ) cout<<it.second << " " ; 
-        
-        int maxi = -1e9 , global = -1e9 ; 
+        int n = s.size();
 
-        for(int j = 0 ; j<s.size(); j++ ){
-            if(mp.count(s[j])){
-                maxi = max({maxi+mp[s[j]] , mp[s[j]] }); 
-            }
-            else{
-                maxi = max({maxi+(s[j]-'a'+1) , (s[j]-'a'+1)}); 
-            }
-            global = max({global,maxi}); 
-        }
-        return max(0,global); 
+        vector<int> dp(n, -1e7);
+        vector<int> mp(26);
 
+        for(int i = 0; i < vals.size(); i++){
+            mp[chars[i]-'a'] = vals[i];
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            ans = max(ans, solve(s, mp, dp, i, n, chars));
+        }
+
+        return ans;
     }
 };
