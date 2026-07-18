cc8377ad5c607f144392125ecba429dbb1f0cbaa
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-19 03:42:10 +0530

    Time: 41 ms (5.32%), Space: 38 MB (40.06%) - LeetHub

---
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumCostSubstring(string s, string chars, vector<int>& vals) {
+        int n  = vals.size();
+        unordered_map<char,int>mp ; 
+        for(int i = 0; i<n ;i++ ){
+            mp[chars[i]] = vals[i]; 
+        }
+        for(auto& it : mp ) cout<<it.second << " " ; 
+        
+        int maxi = -1e9 , global = -1e9 ; 
+
+        for(int j = 0 ; j<s.size(); j++ ){
+            if(mp.count(s[j])){
+                maxi = max({maxi+mp[s[j]] , mp[s[j]] }); 
+            }
+            else{
+                maxi = max({maxi+(s[j]-'a'+1) , (s[j]-'a'+1)}); 
+            }
+            global = max({global,maxi}); 
+        }
+        return max(0,global); 
+
+    }
+};
