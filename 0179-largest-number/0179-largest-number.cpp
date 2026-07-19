c7fbcf23b781b1d92f527f12a666088ec962f41d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-19 07:28:49 +0530

    Time: 3 ms (74.79%), Space: 17.1 MB (55.38%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-
-    class comp{
-        public : 
-        bool operator()(const string& a , const string& b){
-            return a+b > b+a ; 
-        }
-    };
-
     string largestNumber(vector<int>& nums) {
-        vector<string> str;
-        for(auto& it : nums ){
-            str.push_back(to_string(it)); 
+        vector<string>str; 
+        for(auto& it : nums){
+            string temp = to_string(it);
+            str.push_back(temp); 
         }
-        string result ="";
-        sort(str.begin() , str.end() ,comp());
+        sort(str.begin(),str.end(), [&](auto a , auto b ){
+            return a+b>b+a;
+        }); 
+
+        string result = ""; 
         for(auto& it : str ){
             result+=it;
         }
-        return (result[0] ==  '0') ? "0" : result;
+        int i  = 0 , n  =  result.size() , count = 0 ; 
+        
+        return result[0] == '0' ? "0" : result;
     }
 };
