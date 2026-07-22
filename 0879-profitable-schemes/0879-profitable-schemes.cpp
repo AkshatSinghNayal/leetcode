e779038b08ad58736f60a57eddf6cb1ddff02064
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-23 03:00:04 +0530

    Time: 103 ms (52.1%), Space: 56.3 MB (29.88%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
+    // int solve(int i , int minProfit,vector<int>& group, vector<int>& profit ,int n ){
+    //     //base 
 
-    int dp[101][101][101];
-    const int MOD = 1e9+7; 
-
-
-    int solve(int i , int minProfit,vector<int>& group, vector<int>& profit ,int n ){
-        //base 
-
-        if( i < 0 ){
-            return minProfit == 0 ? 1 : 0 ; 
-        }
+    //     if( i < 0 ){
+    //         return minProfit == 0 ? 1 : 0 ; 
+    //     }
         
-        if(dp[i][minProfit][n] != -1 ) return dp[i][minProfit][n] ; 
+    //     if(dp[i][minProfit][n] != -1 ) return dp[i][minProfit][n] ; 
 
-        long long  take = (n>= group[i] and minProfit >=0 ) ? solve(i-1, max(0,minProfit-profit[i]), group, profit ,  n-group[i]) : 0 ; 
-        long long  notTake = solve(i-1,minProfit , group , profit , n ); 
+    //     long long  take = (n>= group[i] and minProfit >=0 ) ? solve(i-1, max(0,minProfit-profit[i]), group, profit ,  n-group[i]) : 0 ; 
+    //     long long  notTake = solve(i-1,minProfit , group , profit , n ); 
 
-        return dp[i][minProfit][n] = ( 0LL+take + notTake )%MOD;  
+    //     return dp[i][minProfit][n] = ( 0LL+take + notTake )%MOD;  
 
-    }
+    // }
 
 
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(dp,-1,sizeof(dp)); 
-        return solve(group.size() -1 ,minProfit,group,profit, n ); 
+        int m = group.size();
+        const int MOD = 1e9+7; 
+
+        vector<vector<int>>dp(n+1, vector<int>(minProfit+1, 0));
+
+        for(int i = 0 ; i<=n ; i++ ){
+            dp[i][0] = 1 ; 
+        }
+
+        for(int i  = 1; i<=m;i++ ){
+            vector<vector<int>>curr(n+1 , vector<int>(minProfit+1 ,  0 )) ; 
+            for( int j= 0; j<= n; j++ ){
+                for(int k  = 0; k<=minProfit; k++ ){
+                    long long take = ( j>= group[i-1]) ? dp[j-group[i-1]][max(0,k-profit[i-1])] : 0; 
+                    long long notTake= dp[j][k]; 
+
+                    curr[j][k] = (take+notTake)%MOD;
+                }
+            }
+            dp = curr; 
+        }
+        return dp[n][minProfit]; 
     }
 };
