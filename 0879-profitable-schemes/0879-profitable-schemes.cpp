c96c9d59ac723f1ef5459d3e7a83a2a496d86f7e
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-23 02:46:27 +0530

    Time: 64 ms (82.92%), Space: 15.4 MB (79.67%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    const int MOD = 1e9+7;
+
+    int dp[101][101][101];
+    const int MOD = 1e9+7; 
+
+
+    int solve(int i , int minProfit,vector<int>& group, vector<int>& profit ,int n ){
+        //base 
+
+        if( i < 0 ){
+            return minProfit == 0 ? 1 : 0 ; 
+        }
+        
+        if(dp[i][minProfit][n] != -1 ) return dp[i][minProfit][n] ; 
+
+        long long  take = (n>= group[i] and minProfit >=0 ) ? solve(i-1, max(0,minProfit-profit[i]), group, profit ,  n-group[i]) : 0 ; 
+        long long  notTake = solve(i-1,minProfit , group , profit , n ); 
+
+        return dp[i][minProfit][n] = ( 0LL+take + notTake )%MOD;  
+
+    }
+
 
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        int m = group.size();
-        vector<vector<int>>prev(n+1, vector<int>(minProfit+1, 0));
-
-        // base case: no crimes, profit 0
-        for(int j = 0; j <= n; j++)
-            prev[j][0] = 1;
-
-        for(int i = 1; i <= m; i++){
-            vector<vector<int>>curr(n+1, vector<int>(minProfit+1, 0));
-            for(int j = 0; j <= n; j++){
-                for(int k = 0; k <= minProfit; k++){
-                    // not take crime i-1
-                    curr[j][k] = prev[j][k];
-
-                    // take crime i-1 if enough members
-                    if(j >= group[i-1]){
-                        int prevProfit = max(0, k - profit[i-1]);
-                        curr[j][k] = (curr[j][k] + prev[j - group[i-1]][prevProfit]) % MOD;
-                    }
-                }
-            }
-            prev = curr;
-        }
-
-        return prev[n][minProfit];
+        memset(dp,-1,sizeof(dp)); 
+        return solve(group.size() -1 ,minProfit,group,profit, n ); 
     }
 };
