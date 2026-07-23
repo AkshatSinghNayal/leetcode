33a2179b44c3764631df2ee05bf3268b51033e46
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-24 01:49:45 +0530

    Time: 46 ms (80.84%), Space: 122 MB (33.82%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int dp[501][501];
-
-    int solve(vector<int>& cost, vector<int>& time, int i, int walls) {
-        // Base case: All walls are painted
-        if (walls <= 0) return 0;
-        if(i>=cost.size() ) return 1e9; 
-
-        if(dp[i][walls] != -1 ) return dp[i][walls]; 
-
-        int take = solve(cost,time,i+1 , walls-1-time[i]);
-        if(take != 1e9 ) take+=cost[i] ;
-
-        int notTake = solve(cost,time,i+1 , walls ); 
-
-        return dp[i][walls] = min(take, notTake);
-    }
-
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        memset(dp, -1, sizeof(dp)); 
         int n = cost.size();
-        return solve(cost, time, 0, n); 
+        vector<vector<int>>dp(n+1,vector<int>(n+1 , 1e9)); 
+
+        for( int i  = 0  ;i<= n ; i++ ){
+            dp[i][0] = 0 ;
+        }
+
+        for(int i  =n-1 ;i>=0 ;i-- ){
+            for(int walls = 1 ; walls<= n; walls++ ){
+                
+                int take =(walls-1-time[i] <0) ? 0 : dp[i+1][walls-1-time[i]];
+                if(take != 1e9 ) take+=cost[i] ;
+
+                int notTake = dp[i+1][walls];
+
+                dp[i][walls] = min(take, notTake);
+
+            }
+        }
+
+        return dp[0][n]; 
     }
 };
