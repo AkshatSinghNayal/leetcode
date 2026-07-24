51b0a7835763b36433bdb36304dd610e71d341b9
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-25 02:15:09 +0530

    Time: 90 ms (37.21%), Space: 21.9 MB (26.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(),wordList.end()); 
-        if(!st.count(endWord))return 0;
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>pq;
+        st.insert(beginWord); 
+        pq.push({1,beginWord}); 
 
-        queue<pair<string,int>> q; 
-        q.push({beginWord,1}); 
+        while(!pq.empty()){
+            auto [ith , curr] = pq.top();pq.pop();
 
-        while(!q.empty()){
-            auto [tempo , dist ]  = q.front(); q.pop(); 
+            if( curr == endWord) return ith;
 
-            
-            for(int i  = 0 ; i< tempo.size(); i++ ){
-                string temp = tempo;
+            for(int i = 0;i<curr.size();i++ ){
+                string temp = curr;
+
                 for(char ch = 'a' ;ch<='z' ;ch++ ){
-                    temp[i] = ch;
-                    if(temp == endWord ) return dist+1;
-                    if( st.count(temp)){
-                        st.erase(temp);
-                        q.push({temp, dist+1});
+                    temp[i] = ch; 
+                    string processing = temp;
+                    if(st.count(processing)){
+                        pq.push({ ith+1 , processing});
+                        st.erase(processing);
                     }
                 }
             }
+
+
         }
-
-        return 0 ;     
-
+        return 0;
     }
 };
