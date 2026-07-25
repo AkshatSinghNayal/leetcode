26ddba38388ed038440929b8100611c843a45ed6
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-07-26 03:38:01 +0530

    Time: 29 ms (37.87%), Space: 59.6 MB (61.66%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,56 +1,56 @@
 class Solution {
 public:
+    unordered_map<int, vector<pair<int, int>>> mp;
 
-        unordered_map<int,vector<pair<int,int>>> mp; 
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-        
+        mp[1] = {{0, -1}, {0, 1}};    // Left, Right
+        mp[2] = {{-1, 0}, {1, 0}};    // Up, Down
+        mp[3] = {{0, -1}, {1, 0}};    // Left, Down
+        mp[4] = {{0, 1}, {1, 0}};     // Right, Down
+        mp[5] = {{0, -1}, {-1, 0}};   // Left, Up
+        mp[6] = {{0, 1}, {-1, 0}};    // Right, Up
 
-    bool solve(vector<vector<int>>& grid , int n , int m , int i , int j,vector<vector<int>>&vis){
-        //base 
-        if( i == n-1 and j ==  m-1 ) return true;
-        if( i<0 or j<0 or i>=n or j>=m ) return false;
-        vis[i][j] = 1; 
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
 
-        for(auto& it : mp[grid[i][j]]){
-            auto [ nr , nc ] = it ;
-            nr+=i; nc+=j; 
-            if(nr>=0 and nc>=0 and nr<n and nc < m and vis[nr][nc] == -1){
-                for(auto& temp : mp[grid[nr][nc]]){
-                    auto [ nr2 , nc2 ] = temp; 
-                    if(nr2+nr == i and nc2+nc == j ){
-                        if(solve(grid, n, m, nr, nc, vis)){
-                            return true;
-                        } 
+        q.push({0, 0});
+        vis[0][0] = 1;
+
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+
+            if (i == n - 1 && j == m - 1)
+                return true;
+
+            // Try every direction from current street
+            for (auto [dr, dc] : mp[grid[i][j]]) {
+
+                int nr = i + dr;
+                int nc = j + dc;
+
+                if (nr < 0 || nc < 0 || nr >= n || nc >= m)
+                    continue;
+
+                if (vis[nr][nc])
+                    continue;
+
+                // Check whether neighbour connects back
+                for (auto [bdr, bdc] : mp[grid[nr][nc]]) {
+
+                    if (nr + bdr == i && nc + bdc == j) {
+
+                        vis[nr][nc] = 1;
+                        q.push({nr, nc});
+                        break;
                     }
                 }
             }
         }
 
-        return false; 
-    }
-
-
-    bool hasValidPath(vector<vector<int>>& grid) {
-        int n = grid.size(); 
-        int m = grid[0].size(); 
-        mp[1] = {{0, -1}, {0, 1}};   // Left, Right
-        mp[2] = {{-1, 0}, {1, 0}};   // Up, Down
-        mp[3] = {{0, -1}, {1, 0}};   // Left, Down
-        mp[4] = {{0, 1}, {1, 0}};    // Right, Down
-        mp[5] = {{0, -1}, {-1, 0}};  // Left, Up
-        mp[6] = {{0, 1}, {-1, 0}};   // Right, Up
-        vector<vector<int>>vis(n,vector<int>(m , -1)); 
-
-        
-
-        auto ans =  solve(grid,  n ,  m ,0,0,vis); 
-        for(auto& it : vis ){
-            for(int i = 0 ;i<it.size(); i++ ){
-                cout<<it[i]<<" "; 
-            } 
-            cout<<endl;
-        }
-
-        return ans; 
+        return false;
     }
 };
