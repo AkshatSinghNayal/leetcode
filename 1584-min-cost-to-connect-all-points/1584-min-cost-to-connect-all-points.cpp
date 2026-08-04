9b0dcaa801ae40584482b3348a0cc6673406b6fe
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-05 01:38:09 +0530

    Time: 477 ms (30.48%), Space: 63.8 MB (45.63%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -30,11 +30,11 @@ public:
 
             // Instead of adjacency list, check all other points
             for (int next = 0; next < n; next++) {
-                
+                if (!visited[next]) {
                     int distance = abs(points[node][0] - points[next][0]) +abs(points[node][1] - points[next][1]);
 
                     pq.push({distance, next, node});
-                
+                }
             }
         }
 
