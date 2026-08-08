1d99613e1002ca0c7a151b932fe52f76cbef555b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-08 23:30:47 +0530

    Time: 0 ms (100%), Space: 15.9 MB (72.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    int dp[5001][2]; 
+    int dp[5001][2][2]; 
 
-    int solve( vector<int>& prices , int i , int buy){
+    int solve( vector<int>& prices , int i , int buy , int cooldown ){
         //base 
         if( i >= prices.size() ) return 0;
 
-        if( dp[i][buy] != -1 ) return dp[i][buy]; 
+        if( dp[i][buy][cooldown] != -1 ) return dp[i][buy][cooldown]; 
 
         int maxi = INT_MIN; 
 
         if( buy ){
-            int notBought = solve(prices , i+1 , true ); 
-            int bought = -prices[i]+solve(prices , i+1 , false); 
+            int bought= INT_MIN ; 
+            int notBought = solve(prices , i+1 , true , false ); 
+            if(!cooldown)  bought = -prices[i]+solve(prices , i+1 , false , false ); 
             
 
             maxi = max({bought , notBought , maxi}); 
         }
         else{
-            int sold = prices[i]+solve(prices , i+2 , true ); 
-            int notSold = solve( prices , i+1 , false ); 
+            int sold = prices[i]+solve(prices , i+1 , true , true ); 
+            int notSold = solve( prices , i+1 , false , false); 
             maxi  = max({maxi, sold , notSold}); 
         }
 
-        return dp[i][buy]= maxi ;
+        return dp[i][buy][cooldown] = maxi ;
 
     }
 
     int maxProfit(vector<int>& prices) {
         memset(dp , -1, sizeof(dp)); 
-        return solve( prices , 0 , true ); 
+        return solve( prices , 0 , true , false ); 
     }
 };
