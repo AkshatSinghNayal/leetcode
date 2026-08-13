7abc052620129076af8da797a9fc9104ceeb8349
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-14 04:46:31 +0530

    Time: 3 ms (70.27%), Space: 32.1 MB (15.4%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -1,50 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class BSTIterator {
 public:
-    vector<TreeNode*> ans;
-    int i = 0;
+    TreeNode* root;
+    stack<TreeNode*> stk;
 
-    void inorder(TreeNode* root) {
-        if (!root) return;
-
-        inorder(root->left);
-        ans.push_back(root);
-        inorder(root->right);
-    }
-
-    BSTIterator(TreeNode* root) { 
-        inorder(root);
+    BSTIterator(TreeNode* root) {
+        this->root = root;
     }
 
     int next() {
-        if (i < ans.size()) {
-            return ans[i++]->val;
+        // Go as far left as possible
+        while (root) {
+            stk.push(root);
+            root = root->left;
         }
-        return -1;
+
+        // Top of stack is the next smallest element
+        root = stk.top();
+        stk.pop();
+
+        int val = root->val;
+
+        // Now process the right subtree next
+        root = root->right;
+
+        return val;
     }
 
     bool hasNext() {
-        if (i + 1 <= ans.size()) {
-            return true;
-        }
-        return false;
+        return root != nullptr || !stk.empty();
     }
 };
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
