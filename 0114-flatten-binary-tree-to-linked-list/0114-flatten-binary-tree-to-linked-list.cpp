b9f3e960de922743d456ce8726b7c235914b8c7b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-14 04:32:53 +0530

    Time: 0 ms (100%), Space: 17.6 MB (24.66%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,39 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    void dfs(TreeNode* root , vector<TreeNode*>& ans ){
-        if(!root) return ; 
-        ans.push_back(root); 
-        dfs(root->left , ans ); 
-        dfs(root->right , ans); 
-    }
-
     void flatten(TreeNode* root) {
-        vector<TreeNode*>ans; 
-        dfs( root ,ans ); 
+        TreeNode* curr = root;
 
-        TreeNode* home = root;
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        int i  = 1; 
-        while(!q.empty() and i < ans.size() ){
-            auto node = q.front(); q.pop(); 
-            node->right = ans[i++]; 
-            node->left = nullptr; 
-            q.push(node->right);
+        while (curr) {
+            if (curr->left) {
+                TreeNode* pred = curr->left;
+
+                while (pred->right)
+                    pred = pred->right;
+
+                pred->right = curr->right;
+                curr->right = curr->left;
+                curr->left = nullptr;
+            }
+
+            curr = curr->right;
         }
-        
     }
 };
