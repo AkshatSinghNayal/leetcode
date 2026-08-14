0e8e3e76dcda30500250ffcb057363c6479f43ed
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-14 22:17:19 +0530

    Time: 3 ms (81.68%), Space: 15.6 MB (30.59%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,67 +10,57 @@
 class Solution {
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int> ans;
-        if (!root) return ans;
+       unordered_map<TreeNode*,vector<TreeNode*>>mp; 
+       queue<TreeNode*>q; 
+       TreeNode* found= nullptr;
+       q.push(root);
 
-        unordered_map<TreeNode*, vector<TreeNode*>> mp;
-        int maxi = INT_MIN;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            maxi = max(maxi, node->val);
-
-            if (node->left) {
-                mp[node].push_back(node->left);
-                mp[node->left].push_back(node);
-                q.push(node->left);
-            }
-
-            if (node->right) {
-                mp[node].push_back(node->right);
-                mp[node->right].push_back(node);
-                q.push(node->right);
-            }
-        }
-
-        vector<int> vis(maxi + 1, -1);
-
-        queue<TreeNode*> q1;
-        q1.push(target);
-        vis[target->val] = 1;
-
-        while (!q1.empty()) {
-
-            if (k == 0) {
-                while (!q1.empty()) {
-                    ans.push_back(q1.front()->val);
-                    q1.pop();
+       while(!q.empty()){
+            int size = q.size(); 
+            for(int i = 0 ; i<size ; i++ ){
+                auto node = q.front(); q.pop(); 
+                if( node == target ){
+                    found = node;
                 }
-                return ans;
-            }
+                if( node->left){
+                    mp[node].push_back(node->left); 
+                    mp[node->left].push_back(node); 
+                    q.push(node->left);
+                }
 
-            int size = q1.size();
-
-            while (size--) {
-                TreeNode* node = q1.front();
-                q1.pop();
-
-                for (auto it : mp[node]) {
-                    if (vis[it->val] == -1) {
-                        q1.push(it);
-                    }
-                        vis[node->val] = 1;
+                if( node->right ){
+                    mp[node].push_back(node->right); 
+                    mp[node->right].push_back(node); 
+                    q.push(node->right);
                 }
             }
+       }
+        queue<pair<TreeNode* , TreeNode*>> q1;
+        q1.push({ nullptr , found}); 
+        int dist = 0; 
+        vector<int>ans; 
 
-            k--;
+        while(!q1.empty()){
+            int size = q1.size(); 
+            if( dist ==  k ){
+                while(!q1.empty()){
+                    // cout<<q1.front().second->val << " " ;
+                    ans.push_back(q1.front().second->val ); q1.pop(); 
+                }
+                break;
+            }
+            for(int i  = 0;i<size ; i++ ){
+                auto [parent , node] = q1.front() ; q1.pop(); 
+
+                for(auto& it : mp[node]){
+                    if( parent  == it ) continue;
+                    q1.push({ node , it }); 
+                }
+
+            }
+            
+            dist++; 
         }
-
-        return ans;
+        return ans; 
     }
 };
