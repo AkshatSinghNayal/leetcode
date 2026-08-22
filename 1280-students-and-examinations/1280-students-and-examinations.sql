4255c70223f9328b91c83dd35f7dacfab5e83a70
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-23 01:56:07 +0530

    Time: 1037 ms (69.29%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -4,5 +4,5 @@ from Students as s
 cross join Subjects as sub
 left join Examinations as e on
 s.student_id = e.student_id and sub.subject_name = e.subject_name 
-group by s.student_id , s.student_name , sub.subject_name
+group by s.student_id ,  sub.subject_name
 ORDER BY s.student_id, sub.subject_name
