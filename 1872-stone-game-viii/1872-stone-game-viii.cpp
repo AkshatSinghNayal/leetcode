35fba69bbe8192004d763a2b780fd1eba8fe81f4
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-26 06:11:53 +0530

    Time: 112 ms (28.09%), Space: 91.6 MB (58.99%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -5,20 +5,18 @@ public:
         int n =stones.size(); 
         vector<int>prefix(n);
         prefix[0] = stones[0]; 
-        vector<int>dp(n+1 ,0); 
         for(int i =1 ;i<n ;i++ ){
             prefix[i] = prefix[i-1]+stones[i]; 
         }
-        dp[n-1] = prefix[n-1]; 
-        for(int i  = n-1 ;i>=0 ;i-- ){
-            if(i == n-1 ) continue; 
+        int dp = prefix[n-1]; 
+        for(int i  = n-2 ;i>=1 ;i-- ){
 
-            int take = prefix[i]- dp[i+1]; 
-            int notTake = dp[i+1]; 
+            int take = prefix[i]- dp; 
+            int notTake = dp;
 
-            dp[i] = max(take , notTake ); 
+            dp = max(take , notTake ); 
 
         }
-        return dp[1]; 
+        return dp; 
     }
 };
