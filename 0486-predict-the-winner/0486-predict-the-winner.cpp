3c4cde97095994641ea98a88fd9ae475ad74d168
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-26 08:10:47 +0530

    Time: 0 ms (100%), Space: 10.6 MB (5.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    int dp[21][21][2];
+    int dp[21][21][2]; 
 
-    int solve(vector<int>& nums, int left, int right, int n, int p1) {
-        // base
-        if (left > right) return 0;
-        if (dp[left][right][p1] != -1) return dp[left][right][p1];
+    int solve( vector<int>& nums , int i , bool player , int n ){
+        //base 
+        if( i>=n ) return 0 ; 
 
-        int win = 0;
-        if (p1) {
-            int takeLeft = nums[left] + solve(nums, left + 1, right, n, false);
-            int takeRight =nums[right] + solve(nums, left, right - 1, n, false);
+        if( dp[i][n][player] !=-1 ) return dp[i][n][player]; 
 
-            win = max(takeLeft, takeRight);
-        } else {
-            int takeLeft = solve(nums, left + 1, right, n, true);
-            int takeRight = solve(nums, left, right - 1, n, true);
+        int maxi =  INT_MIN; 
 
-            win = min(takeLeft, takeRight);
+        if( player ){
+            int front = nums[i]+solve(nums, i+1 , 1-player , n ); 
+            int back = nums[n-1]+solve(nums , i , 1-player , n-1 ); 
+
+            maxi = max({  front , back }); 
+        } 
+        else{
+            int front = solve(nums, i+1 , 1-player , n ); 
+            int back = solve(nums , i , 1-player , n -1 ); 
+
+            maxi  = min({  front , back }) ; 
         }
-        return dp[left][right][p1] = win;
+
+        return dp[i][n][player] = maxi ; 
+
     }
 
+
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(nums, 0, n - 1, n, true);
-        return ans >= (double)accumulate(nums.begin(), nums.end(), 0)/2;
+        memset(dp,-1,sizeof(dp)); 
+        int n = nums.size(); 
+        long long ans  = solve(nums , 0, 1 , n ); 
+        long long total = accumulate( nums.begin() , nums.end() , 0LL ); 
+        cout<< ans ; 
+        return ( ans*2 >= total ); 
     }
 };
