851f05924338f020a53f1a03d6c77c14b388bb2e
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-28 00:14:43 +0530

    Time: 18 ms (20.38%), Space: 23.1 MB (10.83%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    string result ="";
+
+    bool solve(string s , string target , int n , vector<int>&freq , int i, bool found  ){
+        //base
+        if( i == n ){
+            if( result > target ){
+                found = true;
+                return true;
+            }
+            return false;
+        }
+
+        for(char ch = 'a' ; ch<='z' ; ch++ ){
+            if( freq[ch-'a'] == 0 ) continue;
+            if( !found and ch < target[i] ) continue;
+
+            result+=ch; 
+            freq[ch-'a']--;  
+            bool isFound = false;
+            if( found or ch > target[i]) isFound = true;
+            if(solve(s , target , n , freq, i+1 , isFound )) return true;
+            result.pop_back(); 
+            freq[ch-'a']++; 
+        }
+        return false;
+    }
+
+
+    string lexGreaterPermutation(string s, string target) {
+        vector<int> freq(26);
+        for(auto& it: s ){
+            int ele = it-'a'; 
+            freq[ele]++; 
+        }
+
+        solve( s , target , target.length(), freq , 0 , false ); 
+        return result;
+
+    }
+};
