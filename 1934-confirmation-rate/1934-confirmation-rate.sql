582d57e52720720a936235e1a03302b40ad1cf2b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-29 03:27:21 +0530

    Time: 596 ms (98.46%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,7 +1,5 @@
-SELECT
-    s.user_id,
-   ROUND(sum( case when action='confirmed' then 1 else 0 end )/count(*) ,2) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
-GROUP BY s.user_id;
+# Write your MySQL query statement below
+select s.user_id , round((sum(case when action = 'confirmed' then 1 else 0 end))*1.0/count(*),2) as confirmation_rate
+from Signups as s 
+left join Confirmations as c on s.user_id = c.user_id
+group by s.user_id
