05d91e3c3a9abe03e39b2f57b2c19f7bdd6848fd
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-08-29 16:03:03 +0530

    Time: 0 ms (100%), Space: 52 MB (90.66%) - LeetHub

--- 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.cpp
+++ 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
+
+    bool solve( long long d1 , long long d2 , long long r1 , long long r2 , long long mid ){
+        long long lcm = (r1 / gcd(r1 , r2))*r2; 
+
+        return mid-mid/r1 >= d1 and mid- mid/r2 >= d2 and mid - mid/lcm>=d1+d2; 
+    }
+
     long long minimumTime(vector<int>& d, vector<int>& r) {
-        long long d1 = d[0], d2 = d[1];
-        long long r1 = r[0], r2 = r[1];
-
-        long long g = gcd(r1, r2);
-        long long lcm = (r1 / g) * r2;
-
-        auto possible = [&](long long t) {
-            long long avail1 = t - t / r1;
-            long long avail2 = t - t / r2;
-
-            // Hours where at least one drone can make a delivery
-            long long availTotal = t - t / lcm;
-
-            return avail1 >= d1 &&
-                   avail2 >= d2 &&
-                   availTotal >= d1 + d2;
-        };
-
-        long long lo = 0, hi = 1;
-
-        // Find a valid upper bound
-        while (!possible(hi)) {
-            hi *= 2;
+        long long d1 = d[0] , d2 = d[1] , r1 = r[0] , r2 = r[1];
+        long long high = 1; 
+        while(!solve(d1 , d2 , r1 , r2 , high)){
+            high*=2;
         }
+        long long low = 1 ;
+        long long mid = 1;
+        while( low<=high ){
+            mid = low+(high-low)/2;
 
-        // Find minimum feasible time
-        while (lo <= hi) {
-            long long mid = lo + (hi - lo) / 2;
-
-            if (possible(mid))
-                hi = mid-1;
-            else
-                lo = mid + 1;
+            if(solve(d1,d2,r1,r2,mid)){
+                high =mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-
-        return lo;
+        return low;
     }
 };
