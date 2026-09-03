e3306b1c80ab8f176f5a9834af2187dde79b850b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-04 01:41:20 +0530

    Time: 991 ms (74.17%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,9 @@
-select r.contest_id , round(count(*)/(select count(*) from Users)*100,2) as percentage
-from Users as u
-left join Register as r
-on u.user_id = r.user_id
-group by r.contest_id
-having r.contest_id is not null
-order by percentage  desc , r.contest_id  asc
+SELECT
+    r.contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
