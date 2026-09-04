102faa7b0aeb5f3a8b97afd3d55390e23cfb5be1
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-04 15:32:09 +0530

    Time: 722 ms (44.73%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,7 +1,13 @@
-select round(sum( case when d.order_date = d.customer_pref_delivery_date then 1 else 0 end )/count(*)*100,2) as immediate_percentage
-from Delivery AS d 
-where d.order_date = (
-    select min(temp.order_date)
-    from Delivery as temp
-    where temp.customer_id = d.customer_id
-)
+SELECT ROUND(
+    AVG(order_date = customer_pref_delivery_date) * 100,
+    2
+) AS immediate_percentage
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date
+           ) AS rn
+    FROM Delivery
+) d
+WHERE rn = 1;
