06d149f23dc31294eea3e8d5aeb080e7617c5ba8
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-05 01:38:37 +0530

    Time: 617 ms (52.51%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,10 +1,18 @@
-select round(count(*)/(
-    select count( distinct player_id )
-    from Activity
-),2) as fraction 
-from Activity as a
-where datediff(a.event_date , (
-    select min(temp.event_date)
-    from Activity as temp 
-    where temp.player_id = a.player_id
-)) = 1 
+WITH first_login AS (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+)
+
+SELECT
+    ROUND(
+        COUNT(a.player_id) /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM first_login AS f
+JOIN Activity AS a
+    ON f.player_id = a.player_id
+   AND datediff( a.event_date , f.first_date ) =1 
