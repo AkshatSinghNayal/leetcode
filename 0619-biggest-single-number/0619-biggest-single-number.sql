1eb47f42f408cacb33f18964bd5355f29deeb080
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-05 04:56:20 +0530

    Time: 456 ms (63.09%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,8 @@
-with maxi as (
+select max(num) as num
+from MyNumbers
+where num in ( 
     select num
     from MyNumbers
     group by num
-    having count(num) = 1
+    having count(num)  =1 
 )
-
-select max(num) as num
-from maxi 
