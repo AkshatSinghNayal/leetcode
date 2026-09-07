cead2e9e788eee3b19eef763f445f96796368721
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-08 03:06:54 +0530

    Time: 607 ms (58.06%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,16 +1,10 @@
-with temp as (
-    select main.id , case when num = (
-    select l.num
-    from Logs as l
-    where l.id = main.id+1
-) and  num = (
-    select ll.num
-    from Logs as ll
-    where ll.id = main.id+2
-) then num else null end as found 
-from Logs as main
-)
-
-select distinct found as ConsecutiveNums
-from temp
-where found is not null
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+        LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
+    FROM Logs
+) t
+WHERE num = next_num
+  AND num = next_next_num;
