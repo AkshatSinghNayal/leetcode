ac1a80068b1a172b2abab749396033e838f8831b
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-08 01:02:40 +0530

    Time: 329 ms (44.32%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,4 +1,7 @@
-select x , y , z , 
-case when x+y>z and y+z > x and z+x > y then 
-'Yes' else 'No' end as triangle
-from Triangle
+SELECT x, y, z,
+       CASE
+           WHEN 2 * GREATEST(x, y, z) < x + y + z
+           THEN 'Yes'
+           ELSE 'No'
+       END AS triangle
+FROM Triangle;
