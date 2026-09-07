2958d5b92c8d2ce8269195db086687827d631b4c
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-08 02:05:41 +0530

    Time: 356 ms (75.82%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,16 @@
+with temp as(
+    select  id , student , 
+    case when id%2 !=0 and id+1 in ( select id from Seat)
+    then id+1
+    when id%2 = 0 and id-1 in( select id from Seat ) 
+    then id-1
+    else id
+    end as req
+    from Seat
+)
+
+select t.req as id, t.student as student 
+from temp as t 
+left join Seat as s 
+on t.req = s.id 
+order by id
