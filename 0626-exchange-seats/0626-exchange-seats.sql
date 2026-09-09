12eded205e74c489b524800dcd20291f69d33f7d
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-09 13:55:27 +0530

    Time: 401 ms (40.26%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,16 +1,16 @@
-with temp as(
-    select  id , student , 
-    case when id%2 !=0 and id+1 in ( select id from Seat)
+with temp as (
+    select 
+    id , student , 
+    case when id%2 != 0 and id+1 in ( select id from Seat )
     then id+1
-    when id%2 = 0 and id-1 in( select id from Seat ) 
+    when id % 2 = 0 and id-1 in (select id from Seat ) 
     then id-1
-    else id
-    end as req
+    else id 
+    end  as req
     from Seat
 )
 
-select t.req as id, t.student as student 
-from temp as t 
+select t.id ,  s.student
+from temp  as t
 left join Seat as s 
-on t.req = s.id 
-order by id
+on t.req = s.id
