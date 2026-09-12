12127d0019d05c6e4a74b24c748e539e03c5153a
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-12 16:50:24 +0530

    Time: 1521 ms (77.84%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,22 +1,15 @@
-with temp as(
-    select 
-    case when income < 20000 then 'Low Salary'
-    when income >= 20000 and income <= 50000 then 'Average Salary'
-    else 'High Salary' end as type
-    from Accounts 
-),
+SELECT 'Low Salary' AS category,
+       COUNT(CASE WHEN income < 20000 THEN 1 END) AS accounts_count
+FROM Accounts
 
-categories AS (
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL
-    SELECT 'High Salary'
-)
+UNION ALL
 
-select c.category, case when t.type is null then 0 else count(*) end as accounts_count
-from categories as c 
-left join temp as t 
-on c.category = t.type
-group by t.type
-order by accounts_count
+SELECT 'Average Salary' AS category,
+       COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
+FROM Accounts
+
+UNION ALL
+
+SELECT 'High Salary' AS category,
+       COUNT(CASE WHEN income > 50000 THEN 1 END) AS accounts_count
+FROM Accounts;
