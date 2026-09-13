b540c29c76a281bdb3899aa16ee0772263ec3184
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-13 14:09:38 +0530

    Time: 379 ms (0%), Space: 23.3 MB (0%) - LeetHub

--- 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -9,8 +9,8 @@ public:
 
         if( dp[n] != -1) return dp[n]; 
         int ans = INT_MAX; 
-        for(int i =1 ; ; i++ ){
-            long long sum = i*(i+1)/2;
+        for(int i =1 ; i<=n; i++ ){
+            long long sum = 1LL*i*(i+1)/2;
             if( sum > n ) break;
             if( sum ==  n) {
                 ans = min(ans , i);
