71cf9b48ad749d8206ff5cb28e107c55783f44a2
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-14 02:01:19 +0530

    Time: 9 ms (28%), Space: 20.1 MB (25.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& pre) {
-        vector<vector<int>>list(V); 
-        queue<int>q;
-        vector<int>indegree(V);
-        for(auto& it : pre){
-            list[it[1]].push_back(it[0]); 
-            indegree[it[0]]++; 
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        int n = numCourses; 
+        unordered_map<int,vector<int>>mp; 
+        vector<int>indegree(n); 
+        queue<int>q; 
+        int count = 0;
+
+        for(int i  = 0 ;i<pre.size(); i++ ){
+            int B = pre[i][1] , A = pre[i][0]; 
+            mp[B].push_back(A);
+            indegree[A]++; 
         }
-        for(int i  = 0 ;i<indegree.size() ;i++ ){
-            if(indegree[i] == 0  ) q.push(i);
+
+        for(int i  = 0 ;i<indegree.size(); i++ ){
+            if(indegree[i] == 0  ) q.push(i); 
         }
 
         while(!q.empty()){
-            int size = q.size(); 
-            for(int i  = 0;i<size; i++ ){
-                auto edge = q.front(); q.pop(); 
+            auto node = q.front(); q.pop(); 
+            count++; 
 
-                for(auto& it : list[edge]){
-                    indegree[it]--; 
-                    if(indegree[it] == 0  ) q.push(it);
-                }
+            for(auto& it : mp[node]){
+                indegree[it]--; 
+                if( indegree[it] == 0  ) q.push(it);
             }
         }
-        for(auto& it : indegree ){
-            if( it > 0 ) return false;
-        }
-        return true;
+
+        return count == n ; 
     }
 };
