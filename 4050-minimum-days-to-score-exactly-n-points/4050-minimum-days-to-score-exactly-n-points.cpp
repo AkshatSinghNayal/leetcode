864891ac3d7734f7a7b5c1dcc8f4ffc912724477
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-13 13:53:08 +0530

    Time: 354 ms (0%), Space: 23.1 MB (0%) - LeetHub

--- 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
 
-    int solve(int n, vector<int>& dp) {
+    int solve( int n , vector<int>& dp ){
+        //base 
 
-        if (n == 0)
-            return 0;
+        if(n == 0  ) return 0;
+       
 
-        if (dp[n] != -1)
-            return dp[n];
-
-        int ans = 1e9;
-
-        for (int i = 1; ; i++) {
-
-            // points earned in i continuous earning days
-            int sum = i * (i + 1) / 2;
-
-            if (sum > n)
-                break;
-
-            // exact score reached
-            if (sum == n) {
-                ans = min(ans, i);
+        if( dp[n] != -1) return dp[n]; 
+        int ans = INT_MAX; 
+        for(int i =1 ; ; i++ ){
+            long long sum = i*(i+1)/2;
+            if( sum > n ) break;
+            if( sum ==  n) {
+                ans = min(ans , i);
             }
-            else {
-                // i earning days
-                // + 1 skip day
-                // + solve remaining score
-                ans = min(
-                    ans,
-                    i + 1 + solve(n - sum, dp)
-                );
+            else{
+                ans = min(ans , i+1+solve( n-sum , dp)); 
             }
         }
-
         return dp[n] = ans;
     }
 
     int minDays(int n) {
-
-        vector<int> dp(n + 1, -1);
-
-        return solve(n, dp);
+        vector<int>dp(n+1, -1 ); 
+        return solve(n,dp);
     }
 };
