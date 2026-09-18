f9075a449887c161e00182d7cc4d8c30da023a22
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-18 11:09:11 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size(); 
-        if( n  == 1 ) return nums[0] ; 
-        int maxi =  nums[0]; 
-        int mini = nums[0] ; 
-        int ans =  nums[0]; 
-       
-       
+        //till this point i whats the max and min product of the subarray 
+        int n =  nums.size(); 
+        // vector<int>maxi(n),mini(n);
+        // maxi[0]= mini[0] = nums[0];
+        int maxi = nums[0] ;  int mini = nums[0];
+        int maxProduct = maxi;
 
-        for( int i  =1 ; i<n ; i++){
-            int x =  nums[i] ; 
-            maxi = max({x , x*maxi , x*mini}); 
-            mini = min({x, x*maxi , x*mini}) ; 
-            
-            // currentMax = maxi ; 
-            // currentMin = mini ; 
+        for(int i = 1  ;i<n; i++ ){
+            int x = nums[i]; 
 
-            ans = max(ans , maxi ); 
+            int currentMax = max({x , x*maxi , x*mini});
+            int currentMin = min({x , x*maxi, x*mini}); 
+            maxi = currentMax; mini= currentMin;
+            maxProduct = max(maxProduct , currentMax); 
+
         }
-        return ans ; 
+        return maxProduct;
     }
 };
