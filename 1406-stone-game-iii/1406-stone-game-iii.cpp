c309f39706338d9b3389cfdec57daa4f75ccfc76
Akshat Singh Nayal <139916352+AkshatSinghNayal@users.noreply.github.com>
2026-09-18 20:04:06 +0530

    Time: 305 ms (5%), Space: 242.2 MB (5.11%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
 
-    int solve(vector<int>& stones,  vector<vector<int>>&dp , int i , bool player , int n ){
-        if( i>=n ) return 0 ; 
-        if( dp[i][player] != INT_MIN ) return dp[i][player]; 
-        int maxi = INT_MIN; int mini = INT_MAX ; 
+    int solve(vector<int>&stone , vector<vector<int>>&dp , int i , int n , int player ){
+        //base
+        if(i>=n) return 0;
+
+        if(dp[i][player] != INT_MIN ) return dp[i][player]; 
+
+        int maxi = INT_MIN;
         if(player){
-            int total = 0;
-            for(int idx = 0 ;idx+i<n and idx < 3  ;idx++ ){
-                int index = idx+i; 
-                total+=stones[index]; 
-                int sum = total + solve( stones , dp , index+1 , 1-player , n ); 
-                maxi = max(maxi , sum ); 
+            int total = 0; 
+            for(int idx = 0 ; idx<3 and idx+i < n ; idx++ ){
+                int index = idx+i;
+                total+=stone[index]; 
+
+                int cost = total+solve(stone , dp , index+1 , n , 1-player);
+                maxi = max(maxi , cost);
             }
-            return dp[i][player] = maxi ; 
+            return dp[i][player] = maxi;
         }
         else{
-            for(int  idx = 0 ; idx+i < n and idx < 3 ; idx++ ){
-                int index  = idx+i; 
-                int sum =  solve( stones , dp , index+1 , 1-player , n ) ; 
-                mini = min(mini , sum); 
+            int mini = INT_MAX; 
+            for(int idx = 0; idx<3 and idx+i < n ; idx++ ){
+                int cost = solve(stone,dp,idx+i+1,n,1-player);
+                mini = min(mini,cost);
             }
-            return dp[i][player] = mini; 
+            return dp[i][player] = mini;
         }
-        return -1 ; 
+        return -1;
     }
 
-
-    string stoneGameIII(vector<int>& stones) {
-        int n = stones.size(); 
-        vector<vector<int>> dp(n, vector<int>(2, INT_MIN));
-        long long ans  =  solve( stones , dp , 0 , 1, stones.size() ); 
-        long long total = accumulate( stones.begin() , stones.end() , 0LL ); 
-        long long final = ans*2; 
-        if( final== total ){
-            return "Tie"; 
-        }
-        else if( final > total ){
-            return "Alice"; 
-        }
-        return "Bob"; 
+    string stoneGameIII(vector<int>& stone) {
+        int n  = stone.size();
+        vector<vector<int>>dp(n,vector<int>(2,INT_MIN)); 
+        double total = accumulate(stone.begin() , stone.end() , 0LL );
+        int ans = solve(stone,dp ,0,n,1); 
+        return ( total - ans < ans )  ? "Alice" : ( total - ans == ans ) ? "Tie" : "Bob";
     }
 };
